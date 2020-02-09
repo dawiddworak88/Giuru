90f287e490c202befcae5a003c6ca20a021fa974
Dawid Dworak <dawiddworak88@gmail.com>
2020-02-09 18:11:49 +0100

    Initial commit

---
+++ fe/server/bootstrap.js
@@ -0,0 +1,16 @@
+require('ignore-styles');
+
+require('@babel/register')({
+    plugins: [
+        [
+            "file-loader",
+            {
+				"name": "[name].[ext]",
+				"publicPath": "/dist/images",
+				"outputPath": "../../../server/public/dist/images"
+            }
+        ]
+	]
+});
+
+require('./index');
