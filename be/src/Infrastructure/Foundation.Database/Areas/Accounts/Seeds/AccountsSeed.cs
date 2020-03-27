9ba8adeff02a8cebb65cfde872137d6946c6ccc6
Dawid Dworak <dawiddworak88@gmail.com>
2020-03-27 21:17:37 +0100

    Diabled a rule temporarily

--- be/src/Infrastructure/Foundation.Database/Areas/Accounts/Seeds/AccountsSeed.cs
+++ be/src/Infrastructure/Foundation.Database/Areas/Accounts/Seeds/AccountsSeed.cs
@@ -6,7 +6,7 @@ namespace Foundation.Database.Areas.Accounts.Seeds
 {
     public static class AccountsSeed
     {
-        #pragma warning disable S2068
+        #pragma warning disable 2068
         public static void SeedAccounts(DatabaseContext context)
         {
             if (!context.Accounts.Any(x => x.Email == "dawid.dworak@giuru.com"))
@@ -55,6 +55,6 @@ namespace Foundation.Database.Areas.Accounts.Seeds
 
             context.SaveChanges();
         }
-        #pragma warning restore S2068
+        #pragma warning restore 2068
     }
 }
