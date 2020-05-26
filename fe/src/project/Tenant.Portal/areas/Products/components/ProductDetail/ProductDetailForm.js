8f3fee1a760847ad4c76f81f3838c90b709eec19
Dawid Dworak <dawiddworak88@gmail.com>
2020-05-26 23:34:57 +0200

    Implemented selects

--- fe/src/project/Tenant.Portal/areas/Products/components/ProductDetail/ProductDetailForm.js
+++ fe/src/project/Tenant.Portal/areas/Products/components/ProductDetail/ProductDetailForm.js
@@ -147,6 +147,7 @@ function ProductDetailForm(props) {
                             ]
                         }
                     },
+                    "type": "object",
                     "properties": {
                         "primaryColor": {
                             "type": "array",
--- fe/src/shared/components/DynamicForm/DynamicForm.js
+++ fe/src/shared/components/DynamicForm/DynamicForm.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import SchemaField from '../DynamicForm/fields/SchemaField';
+import { getDefaultRegistry } from "./utils/utils";
 
 function DynamicForm(props) {
 
+    function getRegistry() {
+        // For BC, accept passed SchemaField and TitleField props and pass them to
+        // the "fields" registry one.
+        const { fields, widgets } = getDefaultRegistry();
+
+        return {
+            fields: { ...fields },
+            widgets: { ...widgets },
+            definitions: props.schema.jsonSchema.definitions || {},
+            rootSchema: props.schema.jsonSchema,
+            formContext: props.formContext || {},
+        };
+    }
+
     return (
         <div>
-            <SchemaField 
-              schema={props.schema.jsonSchema} 
-              uiSchema={props.schema.uiSchema} 
-              formData={props.formData}
-              onChange={props.onChange} />
+            <SchemaField
+                schema={props.schema.jsonSchema}
+                uiSchema={props.schema.uiSchema}
+                formData={props.formData}
+                onChange={props.onChange}
+                registry={getRegistry()} />
         </div>
     );
 }
