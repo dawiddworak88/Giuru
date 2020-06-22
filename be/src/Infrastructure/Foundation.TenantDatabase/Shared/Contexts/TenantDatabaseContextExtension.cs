d9590029afb9ff7bed5b323fe24dd081faf0051e
Dawid Dworak <dawiddworak88@gmail.com>
2020-06-22 22:15:26 +0200

    Added seeds

--- be/src/Infrastructure/Foundation.TenantDatabase/Shared/Contexts/TenantDatabaseContextExtension.cs
+++ be/src/Infrastructure/Foundation.TenantDatabase/Shared/Contexts/TenantDatabaseContextExtension.cs
@@ -1,9 +1,4 @@
-﻿using Foundation.TenantDatabase.Areas.Translations.Definitions.Languages;
-using Foundation.TenantDatabase.Areas.Translations.Entities;
-using Foundation.TenantDatabase.Shared.Definitions.EntityTypes;
-using Foundation.TenantDatabase.Shared.Entities;
-using Foundation.TenantDatabase.Shared.Helpers;
-using System.Linq;
+﻿using Foundation.TenantDatabase.Shared.Seeds;
 
 namespace Foundation.TenantDatabase.Shared.Contexts
 {
@@ -11,23 +6,7 @@ namespace Foundation.TenantDatabase.Shared.Contexts
     {
         public static void EnsureSeeded(this TenantDatabaseContext context)
         {
-            // Adds entity types
-            if (!context.EntityTypes.Any(x => x.Name == EntityTypeConstants.Product))
-            {
-                var productItemEntityType = EntitySeedHelper.SeedEntity(new EntityType { Id = EntityTypeConstants.ProductId, Name = EntityTypeConstants.Product });
-
-                context.EntityTypes.Add(productItemEntityType);
-
-                var enTranslationProductEntityType = EntitySeedHelper.SeedEntity(new Translation { Key = productItemEntityType.Id.ToString(), Language = LanguageConstants.English, Value = "Product" });
-                var deTranslationProductEntityType = EntitySeedHelper.SeedEntity(new Translation { Key = productItemEntityType.Id.ToString(), Language = LanguageConstants.German, Value = "Produkt" });
-                var plTranslationProductEntityType = EntitySeedHelper.SeedEntity(new Translation { Key = productItemEntityType.Id.ToString(), Language = LanguageConstants.Polish, Value = "Produkt" });
-
-                context.Translations.Add(enTranslationProductEntityType);
-                context.Translations.Add(deTranslationProductEntityType);
-                context.Translations.Add(plTranslationProductEntityType);
-
-                context.SaveChanges();
-            }
+            context.EnsureEntityTypesSeeded();
         }
     }
 }
