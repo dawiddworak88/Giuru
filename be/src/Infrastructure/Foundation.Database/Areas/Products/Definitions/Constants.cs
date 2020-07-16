b22e46ed663612df1c2ae8eb37322e9aa3a6ebaa
Dawid Dworak <dawiddworak88@gmail.com>
2020-07-16 18:39:56 +0200

    Added root category seed

---
+++ be/src/Infrastructure/Foundation.Database/Areas/Products/Definitions/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Foundation.Database.Areas.Products.Definitions
+{
+    public static class Constants
+    {
+        public static class CategoryGuids
+        {
+            public static readonly Guid Furniture = Guid.Parse("e626a139-7068-4de0-abfe-1b970975f1d2");
+        }
+    }
+}
---
+++ be/src/Infrastructure/Foundation.Database/Areas/Products/Seeds/CategoriesSeed.cs
@@ -0,0 +1,55 @@
+﻿using Foundation.Database.Areas.Products.Definitions;
+using Foundation.Database.Areas.Products.Entities;
+using Foundation.Database.Areas.Translations.Definitions.Languages;
+using Foundation.Database.Areas.Translations.Entities;
+using Foundation.Database.Shared.Contexts;
+using Foundation.Database.Shared.Helpers;
+using System.Linq;
+
+namespace Foundation.Database.Areas.Products.Seeds
+{
+    public static class CategoriesSeed
+    {
+        public static void SeedCategories(DatabaseContext context)
+        {
+            if (!context.Categories.Any(x => x.Id == Constants.CategoryGuids.Furniture))
+            {
+                var furniture = new Category
+                {
+                    Id = Constants.CategoryGuids.Furniture,
+                    Level = 0
+                };
+
+                var enTranslation = new Translation
+                { 
+                    Key = Constants.CategoryGuids.Furniture.ToString(),
+                    Language = LanguageConstants.English,
+                    Value = "Furniture"
+                };
+
+                var plTranslation = new Translation
+                {
+                    Key = Constants.CategoryGuids.Furniture.ToString(),
+                    Language = LanguageConstants.Polish,
+                    Value = "Meble"
+                };
+
+                var deTranslation = new Translation
+                {
+                    Key = Constants.CategoryGuids.Furniture.ToString(),
+                    Language = LanguageConstants.German,
+                    Value = "Möbel"
+                };
+
+                context.Categories.Add(EntitySeedHelper.SeedEntity(furniture));
+                context.Translations.Add(enTranslation);
+                context.Translations.Add(plTranslation);
+                context.Translations.Add(deTranslation);
+
+                context.SaveChanges();
+            }
+
+            
+        }
+    }
+}
