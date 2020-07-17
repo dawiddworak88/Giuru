7c089ea82324bc50b61f49ff85bf07bae88bcf2f
Dawid Dworak <dawiddworak88@gmail.com>
2020-07-17 17:54:22 +0200

    Implemented first level of furniture categories

--- be/src/Infrastructure/Foundation.Database/Areas/Products/Definitions/Constants.cs
+++ be/src/Infrastructure/Foundation.Database/Areas/Products/Definitions/Constants.cs
@@ -6,7 +6,20 @@ namespace Foundation.Database.Areas.Products.Definitions
     {
         public static class CategoryGuids
         {
-            public static readonly Guid Furniture = Guid.Parse("e626a139-7068-4de0-abfe-1b970975f1d2");
+            public static readonly Guid FurnitureId = Guid.Parse("e626a139-7068-4de0-abfe-1b970975f1d2");
+
+            public static class Furniture
+            {
+                public static readonly Guid LivingRoomFurnitureId = Guid.Parse("5de3cf93-e4de-492e-98a0-5b65fb41cca5");
+                public static readonly Guid BedroomFurnitureId = Guid.Parse("e1ec2500-aff4-4c42-926b-f155ab5ceed3");
+                public static readonly Guid KichtenDiningFurnitureId = Guid.Parse("90400643-8976-44c8-b61f-f6c700ed45bf");
+                public static readonly Guid AccentFurnitureId = Guid.Parse("b72acb48-4653-4ed7-8766-d4aa0d6e29c7");
+                public static readonly Guid OfficeFurnitureId = Guid.Parse("9c9f3453-e00e-4532-88d8-e96db14d5982");
+                public static readonly Guid EntryFurnitureId = Guid.Parse("eb247a44-5513-40ee-84ef-5d15eec29545");
+                public static readonly Guid OutdoorFurnitureId = Guid.Parse("b65ed50e-8f51-4d63-be41-7c04af91602f");
+                public static readonly Guid BathroomFurnitureId = Guid.Parse("5853a764-8b27-4b34-84e4-23ee79451d0d");
+                public static readonly Guid KidsFurnitureId = Guid.Parse("21c97e53-e36a-4c35-b8c2-a50aaad517e7");
+            }
         }
     }
 }
--- be/src/Infrastructure/Foundation.Database/Areas/Products/Seeds/CategoriesSeed.cs
+++ be/src/Infrastructure/Foundation.Database/Areas/Products/Seeds/CategoriesSeed.cs
@@ -12,31 +12,32 @@ namespace Foundation.Database.Areas.Products.Seeds
     {
         public static void SeedCategories(DatabaseContext context)
         {
-            if (!context.Categories.Any(x => x.Id == Constants.CategoryGuids.Furniture))
+            // Level 0
+            if (!context.Categories.Any(x => x.Id == Constants.CategoryGuids.FurnitureId))
             {
                 var furniture = new Category
                 {
-                    Id = Constants.CategoryGuids.Furniture,
+                    Id = Constants.CategoryGuids.FurnitureId,
                     Level = 0
                 };
 
                 var enTranslation = new Translation
                 { 
-                    Key = Constants.CategoryGuids.Furniture.ToString(),
+                    Key = Constants.CategoryGuids.FurnitureId.ToString(),
                     Language = LanguageConstants.English,
                     Value = "Furniture"
                 };
 
                 var plTranslation = new Translation
                 {
-                    Key = Constants.CategoryGuids.Furniture.ToString(),
+                    Key = Constants.CategoryGuids.FurnitureId.ToString(),
                     Language = LanguageConstants.Polish,
                     Value = "Meble"
                 };
 
                 var deTranslation = new Translation
                 {
-                    Key = Constants.CategoryGuids.Furniture.ToString(),
+                    Key = Constants.CategoryGuids.FurnitureId.ToString(),
                     Language = LanguageConstants.German,
                     Value = "Möbel"
                 };
@@ -49,7 +50,357 @@ namespace Foundation.Database.Areas.Products.Seeds
                 context.SaveChanges();
             }
 
-            
+            // Level 1
+            if (!context.Categories.Any(x => x.Id == Constants.CategoryGuids.Furniture.LivingRoomFurnitureId))
+            {
+                var livingRoom = new Category
+                {
+                    Id = Constants.CategoryGuids.Furniture.LivingRoomFurnitureId,
+                    Level = 1,
+                    Parentid = Constants.CategoryGuids.FurnitureId,
+                    Order = 0
+                };
+
+                var enTranslation = new Translation
+                {
+                    Key = Constants.CategoryGuids.Furniture.LivingRoomFurnitureId.ToString(),
+                    Language = LanguageConstants.English,
+                    Value = "Living Room Furniture"
+                };
+
+                var plTranslation = new Translation
+                {
+                    Key = Constants.CategoryGuids.Furniture.LivingRoomFurnitureId.ToString(),
+                    Language = LanguageConstants.Polish,
+                    Value = "Meble do salonu"
+                };
+
+                var deTranslation = new Translation
+                {
+                    Key = Constants.CategoryGuids.Furniture.LivingRoomFurnitureId.ToString(),
+                    Language = LanguageConstants.German,
+                    Value = "Wohnzimmermöbel"
+                };
+
+                context.Categories.Add(EntitySeedHelper.SeedEntity(livingRoom));
+                context.Translations.Add(enTranslation);
+                context.Translations.Add(plTranslation);
+                context.Translations.Add(deTranslation);
+
+                context.SaveChanges();
+            }
+
+            if (!context.Categories.Any(x => x.Id == Constants.CategoryGuids.Furniture.BedroomFurnitureId))
+            {
+                var bedroom = new Category
+                {
+                    Id = Constants.CategoryGuids.Furniture.BedroomFurnitureId,
+                    Level = 1,
+                    Parentid = Constants.CategoryGuids.FurnitureId,
+                    Order = 10
+                };
+
+                var enTranslation = new Translation
+                {
+                    Key = Constants.CategoryGuids.Furniture.BedroomFurnitureId.ToString(),
+                    Language = LanguageConstants.English,
+                    Value = "Bedroom Furniture"
+                };
+
+                var plTranslation = new Translation
+                {
+                    Key = Constants.CategoryGuids.Furniture.BedroomFurnitureId.ToString(),
+                    Language = LanguageConstants.Polish,
+                    Value = "Meble do sypialni"
+                };
+
+                var deTranslation = new Translation
+                {
+                    Key = Constants.CategoryGuids.Furniture.BedroomFurnitureId.ToString(),
+                    Language = LanguageConstants.German,
+                    Value = "Schlafzimmermöbel"
+                };
+
+                context.Categories.Add(EntitySeedHelper.SeedEntity(bedroom));
+                context.Translations.Add(enTranslation);
+                context.Translations.Add(plTranslation);
+                context.Translations.Add(deTranslation);
+
+                context.SaveChanges();
+            }
+
+            if (!context.Categories.Any(x => x.Id == Constants.CategoryGuids.Furniture.KichtenDiningFurnitureId))
+            {
+                var kitchenDining = new Category
+                {
+                    Id = Constants.CategoryGuids.Furniture.KichtenDiningFurnitureId,
+                    Level = 1,
+                    Parentid = Constants.CategoryGuids.FurnitureId,
+                    Order = 20
+                };
+
+                var enTranslation = new Translation
+                {
+                    Key = Constants.CategoryGuids.Furniture.KichtenDiningFurnitureId.ToString(),
+                    Language = LanguageConstants.English,
+                    Value = "Kitchen & Dining Furniture"
+                };
+
+                var plTranslation = new Translation
+                {
+                    Key = Constants.CategoryGuids.Furniture.KichtenDiningFurnitureId.ToString(),
+                    Language = LanguageConstants.Polish,
+                    Value = "Meble do kuchni i jadalni"
+                };
+
+                var deTranslation = new Translation
+                {
+                    Key = Constants.CategoryGuids.Furniture.KichtenDiningFurnitureId.ToString(),
+                    Language = LanguageConstants.German,
+                    Value = "Esszimmer- & Küchenmöbel"
+                };
+
+                context.Categories.Add(EntitySeedHelper.SeedEntity(kitchenDining));
+                context.Translations.Add(enTranslation);
+                context.Translations.Add(plTranslation);
+                context.Translations.Add(deTranslation);
+
+                context.SaveChanges();
+            }
+
+            if (!context.Categories.Any(x => x.Id == Constants.CategoryGuids.Furniture.AccentFurnitureId))
+            {
+                var accentFurniture = new Category
+                {
+                    Id = Constants.CategoryGuids.Furniture.AccentFurnitureId,
+                    Level = 1,
+                    Parentid = Constants.CategoryGuids.FurnitureId,
+                    Order = 30
+                };
+
+                var enTranslation = new Translation
+                {
+                    Key = Constants.CategoryGuids.Furniture.AccentFurnitureId.ToString(),
+                    Language = LanguageConstants.English,
+                    Value = "Accent Furniture"
+                };
+
+                var plTranslation = new Translation
+                {
+                    Key = Constants.CategoryGuids.Furniture.AccentFurnitureId.ToString(),
+                    Language = LanguageConstants.Polish,
+                    Value = "Meble dekoracyjne"
+                };
+
+                var deTranslation = new Translation
+                {
+                    Key = Constants.CategoryGuids.Furniture.AccentFurnitureId.ToString(),
+                    Language = LanguageConstants.German,
+                    Value = "Kleinmöbel"
+                };
+
+                context.Categories.Add(EntitySeedHelper.SeedEntity(accentFurniture));
+                context.Translations.Add(enTranslation);
+                context.Translations.Add(plTranslation);
+                context.Translations.Add(deTranslation);
+
+                context.SaveChanges();
+            }
+
+            if (!context.Categories.Any(x => x.Id == Constants.CategoryGuids.Furniture.OfficeFurnitureId))
+            {
+                var officeFurniture = new Category
+                {
+                    Id = Constants.CategoryGuids.Furniture.OfficeFurnitureId,
+                    Level = 1,
+                    Parentid = Constants.CategoryGuids.FurnitureId,
+                    Order = 40
+                };
+
+                var enTranslation = new Translation
+                {
+                    Key = Constants.CategoryGuids.Furniture.OfficeFurnitureId.ToString(),
+                    Language = LanguageConstants.English,
+                    Value = "Office Furniture"
+                };
+
+                var plTranslation = new Translation
+                {
+                    Key = Constants.CategoryGuids.Furniture.OfficeFurnitureId.ToString(),
+                    Language = LanguageConstants.Polish,
+                    Value = "Meble do biura"
+                };
+
+                var deTranslation = new Translation
+                {
+                    Key = Constants.CategoryGuids.Furniture.OfficeFurnitureId.ToString(),
+                    Language = LanguageConstants.German,
+                    Value = "Büromöbel"
+                };
+
+                context.Categories.Add(EntitySeedHelper.SeedEntity(officeFurniture));
+                context.Translations.Add(enTranslation);
+                context.Translations.Add(plTranslation);
+                context.Translations.Add(deTranslation);
+
+                context.SaveChanges();
+            }
+
+            if (!context.Categories.Any(x => x.Id == Constants.CategoryGuids.Furniture.EntryFurnitureId))
+            {
+                var entryFurniture = new Category
+                {
+                    Id = Constants.CategoryGuids.Furniture.EntryFurnitureId,
+                    Level = 1,
+                    Parentid = Constants.CategoryGuids.FurnitureId,
+                    Order = 50
+                };
+
+                var enTranslation = new Translation
+                {
+                    Key = Constants.CategoryGuids.Furniture.EntryFurnitureId.ToString(),
+                    Language = LanguageConstants.English,
+                    Value = "Entry & Mudroom Furniture"
+                };
+
+                var plTranslation = new Translation
+                {
+                    Key = Constants.CategoryGuids.Furniture.EntryFurnitureId.ToString(),
+                    Language = LanguageConstants.Polish,
+                    Value = "Meble do przedpokoju"
+                };
+
+                var deTranslation = new Translation
+                {
+                    Key = Constants.CategoryGuids.Furniture.EntryFurnitureId.ToString(),
+                    Language = LanguageConstants.German,
+                    Value = "Flur- & Dielenmöbel"
+                };
+
+                context.Categories.Add(EntitySeedHelper.SeedEntity(entryFurniture));
+                context.Translations.Add(enTranslation);
+                context.Translations.Add(plTranslation);
+                context.Translations.Add(deTranslation);
+
+                context.SaveChanges();
+            }
+
+            if (!context.Categories.Any(x => x.Id == Constants.CategoryGuids.Furniture.OutdoorFurnitureId))
+            {
+                var outdoorFurniture = new Category
+                {
+                    Id = Constants.CategoryGuids.Furniture.OutdoorFurnitureId,
+                    Level = 1,
+                    Parentid = Constants.CategoryGuids.FurnitureId,
+                    Order = 60
+                };
+
+                var enTranslation = new Translation
+                {
+                    Key = Constants.CategoryGuids.Furniture.OutdoorFurnitureId.ToString(),
+                    Language = LanguageConstants.English,
+                    Value = "Outdoor & Patio Furniture"
+                };
+
+                var plTranslation = new Translation
+                {
+                    Key = Constants.CategoryGuids.Furniture.OutdoorFurnitureId.ToString(),
+                    Language = LanguageConstants.Polish,
+                    Value = "Meble do ogrodu"
+                };
+
+                var deTranslation = new Translation
+                {
+                    Key = Constants.CategoryGuids.Furniture.OutdoorFurnitureId.ToString(),
+                    Language = LanguageConstants.German,
+                    Value = "Gartenmöbel"
+                };
+
+                context.Categories.Add(EntitySeedHelper.SeedEntity(outdoorFurniture));
+                context.Translations.Add(enTranslation);
+                context.Translations.Add(plTranslation);
+                context.Translations.Add(deTranslation);
+
+                context.SaveChanges();
+            }
+
+            if (!context.Categories.Any(x => x.Id == Constants.CategoryGuids.Furniture.BathroomFurnitureId))
+            {
+                var bathroomFurniture = new Category
+                {
+                    Id = Constants.CategoryGuids.Furniture.BathroomFurnitureId,
+                    Level = 1,
+                    Parentid = Constants.CategoryGuids.FurnitureId,
+                    Order = 70
+                };
+
+                var enTranslation = new Translation
+                {
+                    Key = Constants.CategoryGuids.Furniture.BathroomFurnitureId.ToString(),
+                    Language = LanguageConstants.English,
+                    Value = "Bathroom Furniture"
+                };
+
+                var plTranslation = new Translation
+                {
+                    Key = Constants.CategoryGuids.Furniture.BathroomFurnitureId.ToString(),
+                    Language = LanguageConstants.Polish,
+                    Value = "Meble łazienkowe"
+                };
+
+                var deTranslation = new Translation
+                {
+                    Key = Constants.CategoryGuids.Furniture.BathroomFurnitureId.ToString(),
+                    Language = LanguageConstants.German,
+                    Value = "Badmöbel"
+                };
+
+                context.Categories.Add(EntitySeedHelper.SeedEntity(bathroomFurniture));
+                context.Translations.Add(enTranslation);
+                context.Translations.Add(plTranslation);
+                context.Translations.Add(deTranslation);
+
+                context.SaveChanges();
+            }
+
+            if (!context.Categories.Any(x => x.Id == Constants.CategoryGuids.Furniture.BathroomFurnitureId))
+            {
+                var kidsFurniture = new Category
+                {
+                    Id = Constants.CategoryGuids.Furniture.BathroomFurnitureId,
+                    Level = 1,
+                    Parentid = Constants.CategoryGuids.FurnitureId,
+                    Order = 80
+                };
+
+                var enTranslation = new Translation
+                {
+                    Key = Constants.CategoryGuids.Furniture.BathroomFurnitureId.ToString(),
+                    Language = LanguageConstants.English,
+                    Value = "Baby & Kids Furniture"
+                };
+
+                var plTranslation = new Translation
+                {
+                    Key = Constants.CategoryGuids.Furniture.BathroomFurnitureId.ToString(),
+                    Language = LanguageConstants.Polish,
+                    Value = "Meble do pokoju dziecka"
+                };
+
+                var deTranslation = new Translation
+                {
+                    Key = Constants.CategoryGuids.Furniture.BathroomFurnitureId.ToString(),
+                    Language = LanguageConstants.German,
+                    Value = "Kinderzimmermöbel"
+                };
+
+                context.Categories.Add(EntitySeedHelper.SeedEntity(kidsFurniture));
+                context.Translations.Add(enTranslation);
+                context.Translations.Add(plTranslation);
+                context.Translations.Add(deTranslation);
+
+                context.SaveChanges();
+            }
         }
     }
 }
