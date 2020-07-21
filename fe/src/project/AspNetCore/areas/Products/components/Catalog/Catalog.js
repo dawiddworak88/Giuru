e2f2dce74a19055ed12674436eeee934e56098c1
Dawid Dworak <dawiddworak88@gmail.com>
2020-07-21 21:53:16 +0200

    Implemented the category page

--- fe/src/project/AspNetCore/areas/Products/components/Catalog/Catalog.js
+++ fe/src/project/AspNetCore/areas/Products/components/Catalog/Catalog.js
@@ -1,15 +1,52 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function Catalog(props) {
 
     return (
 
-        <div>
-        </div>
+        <section className="section">
+            <h1 className="title is-3">{props.title}</h1>
+            {props.resultsCount &&
+                <p className="subtitle is-6">{props.resultsCount} {props.resultsLabel}</p>
+            }
+            {props.items ?
+            (
+                <div className="columns is-tablet is-multiline">
+                    {props.items.map((item, index) => 
+                        <div className="column is-3">
+                            <div className="card">
+                                <a key={item.id} href={item.url}>
+                                    <div className="card-image">
+                                        <figure className="image is-4by3">
+                                            <img src={item.imageUrl} alt={item.imageAlt} />
+                                        </figure>
+                                    </div>
+                                    <div className="media-content">
+                                        <p className="content-grid-card__title title is-5 has-text-centered">{item.title}</p>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    )}
+                </div>
+            ) :
+            (
+                <section className="section is-flex-centered">
+                    <span className="is-title is-5">{props.noResultsLabel}</span>
+                </section>
+            )}
+            
+        </section>
     );
 }
 
 Catalog.propTypes = {
+    title: PropTypes.string.isRequired,
+    noResultsLabel: PropTypes.string.isRequired,
+    resultsCount: PropTypes.number.isRequired,
+    resultsLabel: PropTypes.string.isRequired,
+    items: PropTypes.array
 }
 
 export default Catalog;
--- fe/src/stories/1-Category.stories.js
+++ fe/src/stories/1-Category.stories.js
@@ -3,7 +3,63 @@ import '../project/AspNetCore/areas/Products/pages/CategoryPage/CategoryPage.scs
 import CategoryPage from '../project/AspNetCore/areas/Products/pages/CategoryPage/CategoryPage';
 import { header, mainNavigation, footer } from './Shared/AspNetCoreProps';
 
-export const CategoryPageStory = () => <CategoryPage header={header} mainNavigation={mainNavigation} footer={footer} />
+var catalog = {
+  title: 'Sofas',
+  resultsCount: 2823,
+  resultsLabel: 'results',
+  items: [
+    {
+      id: 1,
+      title: 'Alessia',
+      imageUrl: 'https://eltap.pl/upload/gallery/190/cay02jpg163.jpg',
+      imageAlt: 'Alessia'
+    },
+    {
+      id: 2,
+      title: 'Alessia',
+      imageUrl: 'https://eltap.pl/upload/gallery/190/cay02jpg163.jpg',
+      imageAlt: 'Alessia'
+    },
+    {
+      id: 3,
+      title: 'Alessia',
+      imageUrl: 'https://eltap.pl/upload/gallery/190/cay02jpg163.jpg',
+      imageAlt: 'Alessia'
+    },
+    {
+      id: 4,
+      title: 'Alessia',
+      imageUrl: 'https://eltap.pl/upload/gallery/190/cay02jpg163.jpg',
+      imageAlt: 'Alessia'
+    },
+    {
+      id: 5,
+      title: 'Alessia',
+      imageUrl: 'https://eltap.pl/upload/gallery/190/cay02jpg163.jpg',
+      imageAlt: 'Alessia'
+    },
+    {
+      id: 6,
+      title: 'Alessia',
+      imageUrl: 'https://eltap.pl/upload/gallery/190/cay02jpg163.jpg',
+      imageAlt: 'Alessia'
+    },
+    {
+      id: 7,
+      title: 'Alessia',
+      imageUrl: 'https://eltap.pl/upload/gallery/190/cay02jpg163.jpg',
+      imageAlt: 'Alessia'
+    },
+    {
+      id: 8,
+      title: 'Alessia',
+      imageUrl: 'https://eltap.pl/upload/gallery/190/cay02jpg163.jpg',
+      imageAlt: 'Alessia'
+    }
+  ]
+};
+
+export const CategoryPageStory = () => <CategoryPage header={header} mainNavigation={mainNavigation} catalog={catalog} footer={footer} />
 
 CategoryPageStory.story = {
   name: 'Category Page',
