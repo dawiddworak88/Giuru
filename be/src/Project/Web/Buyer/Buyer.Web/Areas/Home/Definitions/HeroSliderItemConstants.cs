f6cc86e2bc2472e41f800482a400599856313640
Dawid Dworak <dawiddworak88@gmail.com>
2020-09-01 18:30:53 +0200

    Fixed Hero Slider Item

--- be/src/Project/Web/Buyer/Buyer.Web/Areas/Home/Definitions/HeroSliderItemConstants.cs
+++ be/src/Project/Web/Buyer/Buyer.Web/Areas/Home/Definitions/HeroSliderItemConstants.cs
@@ -6,9 +6,9 @@ namespace Buyer.Web.Areas.Home.Definitions
     {
         public struct Media
         {
-            public static Guid LivingRoomMediaId = Guid.Parse("c6c96ab8-a81f-446f-adc2-d375479ece98");
-            public static Guid BedroomMediaId = Guid.Parse("01bfe732-cfca-4cdf-a740-9f8e1ba0a537");
-            public static Guid KidsRoomMediaId = Guid.Parse("3c57aa8e-c54a-4571-8112-936d75331657");
+            public static readonly Guid LivingRoomMediaId = Guid.Parse("c6c96ab8-a81f-446f-adc2-d375479ece98");
+            public static readonly Guid BedroomMediaId = Guid.Parse("01bfe732-cfca-4cdf-a740-9f8e1ba0a537");
+            public static readonly Guid KidsRoomMediaId = Guid.Parse("3c57aa8e-c54a-4571-8112-936d75331657");
         }
     }
 }
