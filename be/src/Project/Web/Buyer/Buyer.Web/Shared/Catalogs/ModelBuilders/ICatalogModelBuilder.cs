93a43d4cb7050b072e67b9178e470344dbb88ee2
Dawid Dworak <dawiddworak88@gmail.com>
2020-10-12 19:35:44 +0200

    Fixed codacy remarks

--- be/src/Project/Web/Buyer/Buyer.Web/Shared/Catalogs/ModelBuilders/ICatalogModelBuilder.cs
+++ be/src/Project/Web/Buyer/Buyer.Web/Shared/Catalogs/ModelBuilders/ICatalogModelBuilder.cs
@@ -3,7 +3,7 @@ using Foundation.PageContent.ComponentModels;
 
 namespace Buyer.Web.Shared.Catalogs.ModelBuilders
 {
-    public interface ICatalogModelBuilder<in S, T> where S : ComponentModelBase where T : CatalogViewModel, new()
+    public interface ICatalogModelBuilder<in S, out T> where S : ComponentModelBase where T : CatalogViewModel, new()
     {
         T BuildModel(S componentModel);
     }
