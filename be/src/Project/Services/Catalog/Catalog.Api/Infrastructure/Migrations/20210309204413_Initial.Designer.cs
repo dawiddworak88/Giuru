304a200f446b384370adda461964731c2c55ba4b
Dawid Dworak <dawiddworak88@gmail.com>
2021-03-10 22:20:22 +0100

    Updated .NET Core versions to the latest one

--- be/src/Project/Services/Catalog/Catalog.Api/Infrastructure/Migrations/20210309204413_Initial.Designer.cs
+++ be/src/Project/Services/Catalog/Catalog.Api/Infrastructure/Migrations/20210309204413_Initial.Designer.cs
@@ -17,7 +17,7 @@ namespace Catalog.Api.Infrastructure.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "3.1.12")
+                .HasAnnotation("ProductVersion", "3.1.13")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
