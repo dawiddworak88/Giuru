d56df85c0fda9ef762bdcaaa37f2e309608a6cfb
Dawid Dworak <dawiddworak88@gmail.com>
2021-05-08 17:57:27 +0200

    Implemented logout

--- be/src/Infrastructure/PageContent/Foundation.PageContent/Components/Headers/ViewModels/HeaderViewModel.cs
+++ be/src/Infrastructure/PageContent/Foundation.PageContent/Components/Headers/ViewModels/HeaderViewModel.cs
@@ -7,10 +7,11 @@ namespace Foundation.PageContent.Components.Headers.ViewModels
 {
     public class HeaderViewModel
     {
+        public bool IsLoggedIn { get; set; }
+        public LinkViewModel SignOutLink { get; set; }
         public string DrawerBackLabel { get; set; }
         public string DrawerBackIcon { get; set; }
         public LogoViewModel Logo { get; set; }
-        public LinkViewModel LoginLink { get; set; }
         public IEnumerable<DrawerMenuViewModel> DrawerMenuCategories { get; set; }
         public LanguageSwitcherViewModel LanguageSwitcher { get; set; }
         public IEnumerable<LinkViewModel> Links { get; set; }
---
+++ be/src/Project/Web/Seller/Seller.Web/Areas/Accounts/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using Foundation.Extensions.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Seller.Web.Areas.Accounts.Controllers
+{
+    [Area("Accounts")]
+    public class AccountController : BaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> SignOut()
+        {
+            return this.SignOut("Cookies", "oidc");
+        }
+    }
+}
