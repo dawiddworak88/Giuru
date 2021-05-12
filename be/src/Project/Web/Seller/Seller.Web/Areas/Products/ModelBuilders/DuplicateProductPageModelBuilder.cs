34e864e38fbd9f49f7fbec84eaf2e1c9d24eda61
Dawid Dworak <dawiddworak88@gmail.com>
2021-05-12 20:44:28 +0200

    Implemented the duplicate product functionality (#50)

--- be/src/Project/Web/Seller/Seller.Web/Areas/Products/ModelBuilders/DuplicateProductPageModelBuilder.cs
+++ be/src/Project/Web/Seller/Seller.Web/Areas/Products/ModelBuilders/DuplicateProductPageModelBuilder.cs
@@ -5,35 +5,36 @@ using Foundation.Extensions.ModelBuilders;
 using System.Threading.Tasks;
 using Seller.Web.Areas.Products.ViewModels;
 using Foundation.PageContent.ComponentModels;
+using Seller.Web.Areas.Products.ComponentModels;
 
 namespace Seller.Web.Areas.ModelBuilders.Products
 {
-    public class ProductPageModelBuilder : IAsyncComponentModelBuilder<ComponentModelBase, ProductPageViewModel>
+    public class DuplicateProductPageModelBuilder : IAsyncComponentModelBuilder<DuplicateProductComponentModel, ProductPageViewModel>
     {
         private readonly IModelBuilder<HeaderViewModel> headerModelBuilder;
         private readonly IModelBuilder<MenuTilesViewModel> menuTilesModelBuilder;
-        private readonly IAsyncComponentModelBuilder<ComponentModelBase, ProductFormViewModel> productFormModelBuilder;
+        private readonly IAsyncComponentModelBuilder<DuplicateProductComponentModel, ProductFormViewModel> duplicateProductFormModelBuilder;
         private readonly IModelBuilder<FooterViewModel> footerModelBuilder;
 
-        public ProductPageModelBuilder(
+        public DuplicateProductPageModelBuilder(
             IModelBuilder<HeaderViewModel> headerModelBuilder,
             IModelBuilder<MenuTilesViewModel> menuTilesModelBuilder,
-            IAsyncComponentModelBuilder<ComponentModelBase, ProductFormViewModel> productFormModelBuilder,
+            IAsyncComponentModelBuilder<DuplicateProductComponentModel, ProductFormViewModel> productFormModelBuilder,
             IModelBuilder<FooterViewModel> footerModelBuilder)
         {
             this.headerModelBuilder = headerModelBuilder;
             this.menuTilesModelBuilder = menuTilesModelBuilder;
-            this.productFormModelBuilder = productFormModelBuilder;
+            this.duplicateProductFormModelBuilder = productFormModelBuilder;
             this.footerModelBuilder = footerModelBuilder;
         }
 
-        public async Task<ProductPageViewModel> BuildModelAsync(ComponentModelBase componentModel)
+        public async Task<ProductPageViewModel> BuildModelAsync(DuplicateProductComponentModel componentModel)
         {
             var viewModel = new ProductPageViewModel
             {
                 Header = this.headerModelBuilder.BuildModel(),
                 MenuTiles = this.menuTilesModelBuilder.BuildModel(),
-                ProductForm = await this.productFormModelBuilder.BuildModelAsync(componentModel),
+                ProductForm = await this.duplicateProductFormModelBuilder.BuildModelAsync(componentModel),
                 Footer = this.footerModelBuilder.BuildModel()
             };
 
--- be/src/Project/Web/Seller/Seller.Web/Shared/ViewModels/CatalogActionViewModel.cs
+++ be/src/Project/Web/Seller/Seller.Web/Shared/ViewModels/CatalogActionViewModel.cs
@@ -4,5 +4,6 @@
     {
         public bool IsEdit { get; set; }
         public bool IsDelete { get; set; }
+        public bool IsDuplicate { get; set; }
     }
 }
