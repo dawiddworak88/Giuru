31f0ec260fde1ff7a57e7d74593286d2a4193476
Dawid Dworak <dawiddworak88@gmail.com>
2021-05-13 21:59:12 +0200

    Implemented uploading multiple pictures (#51)

--- be/src/Project/Web/Seller/Seller.Web/Areas/Media/ApiControllers/FilesApiController.cs
+++ be/src/Project/Web/Seller/Seller.Web/Areas/Media/ApiControllers/FilesApiController.cs
@@ -3,14 +3,18 @@ using Foundation.ApiExtensions.Definitions;
 using Foundation.ApiExtensions.Shared.Definitions;
 using Foundation.Extensions.Services.MediaServices;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using Seller.Web.Areas.Media.ApiRequestModels;
 using Seller.Web.Areas.Media.Repositories;
+using Seller.Web.Areas.Products.DomainModels;
 using Seller.Web.Areas.Products.Repositories;
 using Seller.Web.Shared.Configurations;
+using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 
@@ -37,46 +41,91 @@ namespace Seller.Web.Areas.Media.ApiControllers
         }
 
         [HttpPost]
-        [RequestSizeLimit(ApiConstants.Request.RequestSizeLimit)]
-        public async Task<IActionResult> Post([FromForm] UploadMediaRequestModel model)
+        [DisableRequestSizeLimit]
+        public async Task<IActionResult> Post([FromForm] IFormFile file, List<IFormFile> files)
         {
-            if (model.File == null)
+            if (file == null && (files == null || !files.Any()))
             {
                 return this.StatusCode((int)HttpStatusCode.UnprocessableEntity);
             }
 
-            using (var ms = new MemoryStream())
+            if (file != null)
             {
-                await model.File.CopyToAsync(ms);
-
-                var fileId = await this.filesRepository.SaveAsync(
-                    await HttpContext.GetTokenAsync(ApiExtensionsConstants.TokenName),
-                    CultureInfo.CurrentUICulture.Name,
-                    ms.ToArray(),
-                    model.File.FileName);
-
-                var mediaItem = await this.mediaItemsRepository.GetMediaItemAsync(
-                    await HttpContext.GetTokenAsync(ApiExtensionsConstants.TokenName),
-                    CultureInfo.CurrentUICulture.Name,
-                    fileId);
-
-                if (mediaItem != null)
+                using (var ms = new MemoryStream())
                 {
-                    return this.StatusCode(
-                        (int)HttpStatusCode.OK, 
-                        new 
-                        { 
-                            Id = mediaItem.Id,
-                            Url = this.mediaHelperService.GetFileUrl(this.settings.CurrentValue.MediaUrl, mediaItem.Id, true),
-                            Name = mediaItem.Name,
-                            MimeType = mediaItem.MimeType,
-                            Filename = mediaItem.Filename,
-                            Extension = mediaItem.Extension
-                        });
+                    await file.CopyToAsync(ms);
+
+                    var fileId = await this.filesRepository.SaveAsync(
+                        await HttpContext.GetTokenAsync(ApiExtensionsConstants.TokenName),
+                        CultureInfo.CurrentUICulture.Name,
+                        ms.ToArray(),
+                        file.FileName);
+
+                    var mediaItem = await this.mediaItemsRepository.GetMediaItemAsync(
+                        await HttpContext.GetTokenAsync(ApiExtensionsConstants.TokenName),
+                        CultureInfo.CurrentUICulture.Name,
+                        fileId);
+
+                    if (mediaItem != null)
+                    {
+                        return this.StatusCode(
+                            (int)HttpStatusCode.OK,
+                            new
+                            {
+                                Id = mediaItem.Id,
+                                Url = this.mediaHelperService.GetFileUrl(this.settings.CurrentValue.MediaUrl, mediaItem.Id, true),
+                                Name = mediaItem.Name,
+                                MimeType = mediaItem.MimeType,
+                                Filename = mediaItem.Filename,
+                                Extension = mediaItem.Extension
+                            });
+                    }
+
+                    return this.StatusCode((int)HttpStatusCode.BadRequest);
+                }
+            }
+            else
+            {
+                var media = new List<MediaItem>();
+
+                foreach (var fileItem in files)
+                {
+                    using (var ms = new MemoryStream())
+                    {
+                        await fileItem.CopyToAsync(ms);
+
+                        var fileId = await this.filesRepository.SaveAsync(
+                            await HttpContext.GetTokenAsync(ApiExtensionsConstants.TokenName),
+                            CultureInfo.CurrentUICulture.Name,
+                            ms.ToArray(),
+                            fileItem.FileName);
+
+                        var mediaItem = await this.mediaItemsRepository.GetMediaItemAsync(
+                            await HttpContext.GetTokenAsync(ApiExtensionsConstants.TokenName),
+                            CultureInfo.CurrentUICulture.Name,
+                            fileId);
+
+                        if (mediaItem != null)
+                        {
+                            media.Add(mediaItem);
+                        }
+                    }
                 }
 
-                return this.StatusCode((int)HttpStatusCode.BadRequest);
+                return this.StatusCode(
+                            (int)HttpStatusCode.OK,
+                            media.Select(mediaItem => new
+                            {
+                                Id = mediaItem.Id,
+                                Url = this.mediaHelperService.GetFileUrl(this.settings.CurrentValue.MediaUrl, mediaItem.Id, true),
+                                Name = mediaItem.Name,
+                                MimeType = mediaItem.MimeType,
+                                Filename = mediaItem.Filename,
+                                Extension = mediaItem.Extension
+                            }));
             }
+
+            return this.StatusCode((int)HttpStatusCode.BadRequest);
         }
     }
 }
