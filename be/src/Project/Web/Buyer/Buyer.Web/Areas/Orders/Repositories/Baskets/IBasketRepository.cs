eeecde8be40634ea0333bbd31845b8867e11139f
Dawid Dworak <dawiddworak88@gmail.com>
2022-01-15 18:24:48 +0100

    Removed redundant Basket objects

--- be/src/Project/Web/Buyer/Buyer.Web/Areas/Orders/Repositories/Baskets/IBasketRepository.cs
+++ be/src/Project/Web/Buyer/Buyer.Web/Areas/Orders/Repositories/Baskets/IBasketRepository.cs
@@ -1,5 +1,4 @@
 ﻿using Buyer.Web.Areas.Orders.DomainModels;
-using Buyer.Web.Areas.Orders.DomainModels.BasketOrder;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -8,8 +7,8 @@ namespace Buyer.Web.Areas.Orders.Repositories.Baskets
 {
     public interface IBasketRepository
     {
-        Task<BasketOrder> GetBasketByOrganisation(string token, string language);
-        Task<BasketOrder> DeleteItemAsync(string token, string language, Guid? id);
+        Task<Basket> GetBasketByOrganisation(string token, string language);
+        Task<Basket> DeleteItemAsync(string token, string language, Guid? id);
         Task DeleteAsync(string token, string language);
         Task<Basket> SaveAsync(string token, string language, Guid? id, IEnumerable<BasketItem> items);
         Task CheckoutBasketAsync(string token, string language, Guid? clientId, string clientName, Guid? basketId, DateTime? expectedDelivery, string moreInfo);
