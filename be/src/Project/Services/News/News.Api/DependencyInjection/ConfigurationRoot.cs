57b0cb65b20116f07451f42caea2946d49df0476
SebaKas21 <sebastian.kasperek@eltap.com>
2022-02-23 12:56:36 +0100

    Added initial api

---
+++ be/src/Project/Services/News/News.Api/DependencyInjection/ConfigurationRoot.cs
@@ -0,0 +1,14 @@
+﻿using Foundation.Localization.Definitions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace News.Api.DependencyInjection
+{
+    public static class ConfigurationRoot
+    {
+        public static void ConfigureSettings(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<LocalizationSettings>(configuration);
+        }
+    }
+}
