9466905d6a3721f6c1f47743930d98c772fe70c1
SebaKas21 <sebastian.kasperek@eltap.com>
2022-02-24 13:53:45 +0100

    Added categries seeds

--- be/src/Project/Services/News/News.Api/Infrastructure/Seeds/CategoriesSeed.cs
+++ be/src/Project/Services/News/News.Api/Infrastructure/Seeds/CategoriesSeed.cs
@@ -12,6 +12,13 @@ namespace News.Api.Infrastructure.Seeds
         public static void SeedCategories(NewsContext context)
         {
             SeedCategory(context, NewsConstants.Categories.InformationCategory, null, "Information", "Informacje", "Information");
+            SeedCategory(context, NewsConstants.Categories.EventsCategory, null, "Events", "Wydarzenia", "Veranstaltungen");
+            SeedCategory(context, NewsConstants.Categories.BusinessCategory, null, "Business", "Biznes", "Unternehmen");
+
+            SeedCategory(context, NewsConstants.Categories.CooperationCategory, NewsConstants.Categories.BusinessCategory, "B2B", "B2B", "B2B");
+            SeedCategory(context, NewsConstants.Categories.FairCategory, NewsConstants.Categories.EventsCategory, "Fairs", "Targi", "Messen");
+            SeedCategory(context, NewsConstants.Categories.ExhibitionCategory, NewsConstants.Categories.EventsCategory, "Exhibition", "Wystawa", "Auslage");
+            SeedCategory(context, NewsConstants.Categories.SalesCategory, NewsConstants.Categories.InformationCategory, "Sales", "Sprzedaż", "Verkauf");
         }
 
         private static void SeedCategory(NewsContext context, Guid id, Guid? parentCategoryId, string englishName, string polishName, string germanName)
