8447a38f758356a6cb94c2d193d364a29a844c06
Dawid Dworak <dawiddworak88@gmail.com>
2022-04-01 23:12:21 +0200

    Fixed the default db name

--- be/src/Project/Services/Content/Content.Api/config/database.js
+++ be/src/Project/Services/Content/Content.Api/config/database.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
     connection: {
       host: env('DATABASE_HOST', 'host.docker.internal'),
       port: env.int('DATABASE_PORT', 5438),
-      database: env('DATABASE_NAME', 'ContentDb'),
+      database: env('DATABASE_NAME', 'postgres'),
       user: env('DATABASE_USERNAME', 'postgres'),
       password: env('DATABASE_PASSWORD', 'postgres'),
       ssl: env.bool('DATABASE_SSL', false),
