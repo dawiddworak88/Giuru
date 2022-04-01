c6301dc23d0d46381f21ac60a3fd2a31eca03539
SebaKas21 <sebastian.kasperek@eltap.com>
2022-04-01 13:15:20 +0200

    Added code improvements

--- fe/src/project/AspNetCore/shared/components/Sidebar/Sidebar.js
+++ fe/src/project/AspNetCore/shared/components/Sidebar/Sidebar.js
@@ -17,6 +17,7 @@ const Sidebar = (props) => {
     const [productVariants, setProductVariants] = useState([]);
     const [quantities, setQuantities] = useState([]);
     const {productId, isOpen, manyUses, setIsOpen, handleOrder, labels} = props;
+    const [t, st] = useState(false);
 
     const toggleDrawer = (open) => (e) => {
         if (e && e.type === 'keydown' && 
@@ -26,6 +27,7 @@ const Sidebar = (props) => {
 
         if (!isOpen && manyUses){
             setProductVariants([])
+            setQuantities([]);
         }
 
         setIsOpen(open)
@@ -37,6 +39,8 @@ const Sidebar = (props) => {
     }
 
     const fetchProductVariants = () => {
+        dispatch({ type: "SET_IS_LOADING", payload: true });
+
         if (productVariants.length === 0) {
             const requestOptions = {
                 method: "GET",
@@ -69,6 +73,7 @@ const Sidebar = (props) => {
                             });
 
                             setQuantities(quantities)
+                            st(true)
                         }   
                     });
                 }).catch(() => {
@@ -104,10 +109,7 @@ const Sidebar = (props) => {
         if (isOpen){
             fetchProductVariants();
         }
-        else {
-            setProductVariants(() => []);
-            setQuantities(() => []);
-        }
+      
     }, [isOpen, productId])
 
     return (
@@ -128,75 +130,71 @@ const Sidebar = (props) => {
                     <h2 className="title">{labels.sidebarTitle}</h2>
                     <a href={labels.basketUrl} className="link">{labels.toBasketLabel}</a>
                 </div>
-                {productVariants.length === 0 ? (
-                    <div className="not-found">{labels.notFound}</div>
-                ) : (
+                {productVariants && productVariants.length > 0 ? (
                     productVariants.map((item) => 
                         item.carouselItems.map((carouselItem) => {
-                                let fabrics = labels.lackInformation;
-                                if (carouselItem.attributes.length > 0) {
-                                    fabrics = carouselItem.attributes.find(x => x.key === "primaryFabrics") ? carouselItem.attributes.find(x => x.key === "primaryFabrics").value : "";
-                                    var secondaryfabrics = carouselItem.attributes.find(x => x.key === "secondaryFabrics") ? carouselItem.attributes.find(x => x.key === "secondaryFabrics").value : "";
-                                    if (secondaryfabrics) {
-                                        fabrics += ", " + secondaryfabrics;
-                                    }
+                            let fabrics = labels.lackInformation;
+                            if (carouselItem.attributes.length > 0) {
+                                fabrics = carouselItem.attributes.find(x => x.key === "primaryFabrics") ? carouselItem.attributes.find(x => x.key === "primaryFabrics").value : "";
+                                var secondaryfabrics = carouselItem.attributes.find(x => x.key === "secondaryFabrics") ? carouselItem.attributes.find(x => x.key === "secondaryFabrics").value : "";
+                                if (secondaryfabrics) {
+                                    fabrics += ", " + secondaryfabrics;
                                 }
-
-                                let quantity = 1;
-                                if (quantities.length !== 0){
-                                    quantity = quantities.find(x => x.id === carouselItem.id).quantity;
-                                }
-
-                                return (
-                                    <ListItem className="sidebar-item">
-                                        <div className="sidebar-item__row">
-                                            <figure className="sidebar-item__image">
-                                                <ResponsiveImage sources={carouselItem.sources} imageSrc={carouselItem.imageUrl} imageAlt={carouselItem.imageAlt} />
-                                            </figure>
-                                            <div className="sidebar-item__details">
-                                                <h1 className="title">{carouselItem.title}</h1>
-                                                <span className="sku">{labels.skuLabel} {carouselItem.subtitle}</span>
-                                                <div className="stock-details">
-                                                    {carouselItem.availableQuantity && carouselItem.availableQuantity > 0 &&
-                                                        <div className="stock">
-                                                            {labels.inStockLabel} {carouselItem.availableQuantity}
-                                                        </div>
-                                                    }
-                                                    {carouselItem.expectedDelivery &&
-                                                        <div className="expected-delivery">
-                                                            {labels.expectedDeliveryLabel} {moment(carouselItem.expectedDelivery).format("DD/MM/YYYY")}
-                                                        </div>
-                                                    }
-                                                </div>
-                                                <div className="fabrics">
-                                                    <span>{labels.fabricsLabel}</span>
-                                                    <p>{fabrics}</p>
-                                                </div>
+                            }
+                            let quantity = 1;
+                            if (quantities.length !== 0){
+                                quantity = quantities.find(x => x.id === carouselItem.id).quantity;
+                            }
+                            return (
+                                <ListItem className="sidebar-item">
+                                    <div className="sidebar-item__row">
+                                        <figure className="sidebar-item__image">
+                                            <ResponsiveImage sources={carouselItem.sources} imageSrc={carouselItem.imageUrl} imageAlt={carouselItem.imageAlt} />
+                                        </figure>
+                                        <div className="sidebar-item__details">
+                                            <h1 className="title">{carouselItem.title}</h1>
+                                            <span className="sku">{labels.skuLabel} {carouselItem.subtitle}</span>
+                                            <div className="stock-details">
+                                                {carouselItem.availableQuantity && carouselItem.availableQuantity > 0 &&
+                                                    <div className="stock">
+                                                        {labels.inStockLabel} {carouselItem.availableQuantity}
+                                                    </div>
+                                                }
+                                                {carouselItem.expectedDelivery &&
+                                                    <div className="expected-delivery">
+                                                        {labels.expectedDeliveryLabel} {moment(carouselItem.expectedDelivery).format("DD/MM/YYYY")}
+                                                    </div>
+                                                }
+                                            </div>
+                                            <div className="fabrics">
+                                                <span>{labels.fabricsLabel}</span>
+                                                <p>{fabrics}</p>
                                             </div>
                                         </div>
-                                        <div className="sidebar-item__buttons">
-                                            <TextField 
-                                                id={carouselItem.id} 
-                                                name="quantity" 
-                                                type="number" 
-                                                inputProps={{ 
-                                                    min: 1, 
-                                                    step: 1,
-                                                    style: { textAlign: 'center' }
-                                                }}
-                                                value={quantity} 
-                                                onChange={onQuantityChange(carouselItem.id)}
-                                                className="quantity-input" />
-                                            <Button title={props.labels.addToCartLabel} aria-label={props.labels.addToCartLabel} type="text" color="primary" variant="contained" className="cart-button" onClick={() => handleAddOrderItemClick(carouselItem)}><AddShoppingCart /></Button>
-                                            <Button title={props.labels.goToDetailsLabel} aria-label={props.labels.goToDetailsLabel} type="text" color="primary" variant="contained" className="cart-button" onClick={variantDetails(carouselItem)}><ArrowRight /></Button>
-                                        </div>
-                                        <hr className="divider"></hr>
-                                    </ListItem>
-                                )
-                            }
-                        ),
-                        state.isLoading && <CircularProgress className="progressBar" />
-                    )
+                                    </div>
+                                    <div className="sidebar-item__buttons">
+                                        <TextField 
+                                            id={carouselItem.id} 
+                                            name="quantity" 
+                                            type="number" 
+                                            inputProps={{ 
+                                                min: 1, 
+                                                step: 1,
+                                                style: { textAlign: 'center' }
+                                            }}
+                                            value={quantity} 
+                                            onChange={onQuantityChange(carouselItem.id)}
+                                            className="quantity-input" />
+                                        <Button title={props.labels.addToCartLabel} aria-label={props.labels.addToCartLabel} type="text" color="primary" variant="contained" className="cart-button" onClick={() => handleAddOrderItemClick(carouselItem)}><AddShoppingCart /></Button>
+                                        <Button title={props.labels.goToDetailsLabel} aria-label={props.labels.goToDetailsLabel} type="text" color="primary" variant="contained" className="cart-button" onClick={variantDetails(carouselItem)}><ArrowRight /></Button>
+                                    </div>
+                                    <hr className="divider"></hr>
+                                </ListItem>
+                            )
+                        }
+                    ))
+                ) : (
+                    <div className="not-found">{labels.notFound}</div>
                 )}
             </List>
         </SwipeableDrawer>
