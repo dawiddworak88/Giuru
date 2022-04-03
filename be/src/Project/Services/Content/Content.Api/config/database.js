4ca35c611e802736264a360cba481faa1261cf44
Dawid Dworak <dawiddworak88@gmail.com>
2022-04-03 23:08:23 +0200

    Fixed Dockerfile env variables

--- be/src/Project/Services/Content/Content.Api/config/database.js
+++ be/src/Project/Services/Content/Content.Api/config/database.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: process.env.DATABASE_CLIENT,
     connection: {
-      host: env('DATABASE_HOST', 'host.docker.internal'),
-      port: env.int('DATABASE_PORT', 5438),
-      database: env('DATABASE_NAME', 'postgres'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'postgres'),
-      ssl: env.bool('DATABASE_SSL', false),
+      host: env('DATABASE_HOST', process.env.DATABASE_HOST),
+      port: env.int('DATABASE_PORT', process.env.DATABASE_PORT),
+      database: env('DATABASE_NAME', process.env.DATABASE_NAME),
+      user: env('DATABASE_USERNAME', process.env.DATABASE_USERNAME),
+      password: env('DATABASE_PASSWORD', process.env.DATABASE_PASSWORD),
+      ssl: env.bool('DATABASE_SSL', process.env.DATABASE_SSL),
     },
   },
 });
