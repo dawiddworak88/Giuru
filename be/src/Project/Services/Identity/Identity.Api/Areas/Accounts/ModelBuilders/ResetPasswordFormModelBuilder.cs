35cfb145a520c5c3489e3b4fac6d7b69d4dbeaee
SebaKas21 <sebastian.kasperek@eltap.com>
2022-04-05 14:08:04 +0200

    Added integration fe with ssr

---
+++ be/src/Project/Services/Identity/Identity.Api/Areas/Accounts/ModelBuilders/ResetPasswordFormModelBuilder.cs
@@ -0,0 +1,34 @@
+﻿using Feature.Account;
+using Foundation.Extensions.ModelBuilders;
+using Identity.Api.Areas.Accounts.ComponentModels;
+using Identity.Api.Areas.Accounts.ViewModels;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Localization;
+using System.Threading.Tasks;
+
+namespace Identity.Api.Areas.Accounts.ModelBuilders
+{
+    public class ResetPasswordFormModelBuilder : IAsyncComponentModelBuilder<ResetPasswordComponentModel, ResetPasswordFormViewModel>
+    {
+        private readonly IStringLocalizer<AccountResources> accountLocalizer;
+        private readonly LinkGenerator linkGenerator;
+
+        public ResetPasswordFormModelBuilder(
+            IStringLocalizer<AccountResources> accountLocalizer,
+            LinkGenerator linkGenerator)
+        {
+            this.accountLocalizer = accountLocalizer;
+            this.linkGenerator = linkGenerator;
+        }
+
+        public async Task<ResetPasswordFormViewModel> BuildModelAsync(ResetPasswordComponentModel componentModel)
+        {
+            var viewModel = new ResetPasswordFormViewModel
+            {
+               
+            };
+
+            return viewModel;
+        }
+    }
+}
---
+++ be/src/Project/Services/Identity/Identity.Api/Areas/Accounts/ViewModels/ResetPasswordFormViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Identity.Api.Areas.Accounts.ViewModels
+{
+    public class ResetPasswordFormViewModel
+    {
+    }
+}
