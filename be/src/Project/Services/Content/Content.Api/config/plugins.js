ffa833ba7bb4e711c31842c35af24997eb3cbee8
SebaKas21 <sebastian.kasperek@eltap.com>
2022-04-14 15:52:46 +0200

    Renamed media provider

--- be/src/Project/Services/Content/Content.Api/config/plugins.js
+++ be/src/Project/Services/Content/Content.Api/config/plugins.js
@@ -9,7 +9,7 @@ module.exports = ({ env }) => ({
     "upload": {
         enabled: true,
         config: {
-            provider: "eltap",
+            provider: "giuru",
             providerOptions: {}
         }
     }
