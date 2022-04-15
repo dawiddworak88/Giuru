22e14d301202a2031a83c66862188eb1babfb2d7
SebaKas21 <sebastian.kasperek@eltap.com>
2022-04-15 13:32:47 +0200

    Added from fields

--- be/src/Project/Web/Seller/Seller.Web/Areas/Clients/ViewModels/GroupFormViewModel.cs
+++ be/src/Project/Web/Seller/Seller.Web/Areas/Clients/ViewModels/GroupFormViewModel.cs
@@ -2,5 +2,15 @@
 {
     public class GroupFormViewModel
     {
+        public string Title { get; set; }
+        public string IdLabel { get; set; }
+        public string NameLabel { get; set; }
+        public string Name { get; set; }
+        public string GroupsUrl { get; set; }
+        public string NavigateToGroupsText { get; set; }
+        public string SaveText { get; set; }
+        public string SaveUrl { get; set; }
+        public string GeneralErrorMessage { get; set; }
+        public string NameRequiredErrorMessage { get; set; }
     }
 }
