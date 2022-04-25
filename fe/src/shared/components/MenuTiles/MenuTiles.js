3ed1e70dd3733ecf8f633aedddad8b0592fbf162
SebaKas21 <sebastian.kasperek@eltap.com>
2022-04-25 14:27:06 +0200

    Added smooth tiles scrolling

--- fe/src/shared/components/MenuTiles/MenuTiles.js
+++ fe/src/shared/components/MenuTiles/MenuTiles.js
@@ -1,16 +1,45 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 import Tile from "./Tile";
+import {
+    ArrowBackIos, ArrowForwardIos
+} from "@material-ui/icons";
 
 function MenuTiles(props) {
+    if (typeof window !== "undefined") {
+        const tilesWrapper = document.querySelector(".menu-tiles__wrapper");
+        const tiles = document.getElementsByClassName("tile");
+
+        const prevButton = document.getElementById("prev");
+        const nextButton = document.getElementById("next");
+
+        const toRight = () => {
+            const lastTile = tiles[tiles.length - 1];
+
+            tilesWrapper.scrollLeft = lastTile.offsetLeft;
+        }
+
+        const toLeft = () => {
+            tilesWrapper.scrollLeft = 0;
+        }
+
+        useEffect(() => {
+            nextButton.addEventListener("click", toRight);
+            prevButton.addEventListener("click", toLeft);
+        }, []);
+    }
 
     return (
         <nav className="section menu-tiles is-hidden-touch">
-            {props.tiles.map((tile, index) => {
-                return(
-                    <Tile key={index} icon={tile.icon} title={tile.title} url={tile.url} />
-                );
-            })}
+            <div className="menu-tiles__prev" id="prev"><ArrowBackIos /></div>
+            <div className="menu-tiles__next" id="next"><ArrowForwardIos /></div>
+            <div className="menu-tiles__wrapper">
+                {props.tiles.map((tile, index) => {
+                    return(
+                        <Tile key={index} icon={tile.icon} title={tile.title} url={tile.url} />
+                    );
+                })}
+            </div>
         </nav>
     );
 }
