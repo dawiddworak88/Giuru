3a65dbfde39f002454f8d669dddaaec522d0b15a
SebaKas21 <sebastian.kasperek@eltap.com>
2022-05-04 13:16:02 +0200

    Added arrows

--- fe/src/shared/components/MenuTiles/MenuTiles.js
+++ fe/src/shared/components/MenuTiles/MenuTiles.js
@@ -2,14 +2,21 @@ import React from "react";
 import PropTypes from "prop-types";
 import Tile from "./Tile";
 import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Navigation } from "swiper";
 import MenuTilesConstants from "./MenuTilesConstants";
 
 function MenuTiles(props) {
+    
+    SwiperCore.use([Navigation]);
 
     return (
         <nav className="section menu-tiles is-hidden-touch">
             <Swiper
                 slidesPerView={MenuTilesConstants.tilesCountPerSlide()}
+                navigation={{
+                    prevEl: ".menu-tiles__prev",
+                    nextEl: ".menu-tiles__next"
+                }}
             >
                 {props.tiles.map((tile, index) => {
                     return(
@@ -19,6 +26,8 @@ function MenuTiles(props) {
                     );
                 })}
             </Swiper>
+            <div className="swiper-button-prev menu-tiles__prev"></div>
+            <div className="swiper-button-next menu-tiles__next"></div>
         </nav>
     );
 }
