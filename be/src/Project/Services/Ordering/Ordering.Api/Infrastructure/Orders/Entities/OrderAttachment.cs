b79584c883cd816196563bf1cdb83c335e80587f
SebaKas21 <sebastian.kasperek@eltap.com>
2022-05-04 14:21:26 +0200

    Added order attachment migration

---
+++ be/src/Project/Services/Ordering/Ordering.Api/Infrastructure/Orders/Entities/OrderAttachment.cs
@@ -0,0 +1,12 @@
+﻿using Foundation.GenericRepository.Entities;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ordering.Api.Infrastructure.Orders.Entities
+{
+    public class OrderAttachment : EntityMedia
+    {
+        [Required]
+        public Guid OrderId { get; set; }
+    }
+}
