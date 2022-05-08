8f67f337b14f86b6ffb608606e006787320c7ac2
Dawid Dworak <dawiddworak88@gmail.com>
2022-05-08 21:01:17 +0200

    Implemented getting content from Strapi's GraphQL

--- be/src/Project/Web/Buyer/Buyer.Web/Shared/Configurations/AppSettings.cs
+++ be/src/Project/Web/Buyer/Buyer.Web/Shared/Configurations/AppSettings.cs
@@ -12,6 +12,7 @@ namespace Buyer.Web.Shared.Configurations
         public string OrderUrl { get; set; }
         public string BasketUrl { get; set; }
         public string NewsUrl { get; set; }
+        public string ContentGraphQlUrl { get; set; }
         public string CdnUrl { get; set; }
         public bool IsMarketplace { get; set; }
         public Guid? OrganisationId { get; set; }
--- be/src/Project/Web/Buyer/Buyer.Web/Shared/ModelBuilders/Seo/SeoModelBuilder.cs
+++ be/src/Project/Web/Buyer/Buyer.Web/Shared/ModelBuilders/Seo/SeoModelBuilder.cs
@@ -1,30 +1,46 @@
-﻿using Buyer.Web.Shared.DomainModels.Metadata;
+﻿using Buyer.Web.Shared.Configurations;
+using Buyer.Web.Shared.DomainModels.Metadata;
 using Foundation.Extensions.ModelBuilders;
 using Foundation.PageContent.ComponentModels;
 using Foundation.PageContent.Components.Seo.ViewModels;
 using GraphQL;
 using GraphQL.Client.Http;
 using GraphQL.Client.Serializer.Newtonsoft;
+using Microsoft.Extensions.Options;
 using System.Threading.Tasks;
 
 namespace Buyer.Web.Shared.ModelBuilders.Seo
 {
     public class SeoModelBuilder : IAsyncComponentModelBuilder<ComponentModelBase, SeoViewModel>
     {
+        private readonly IOptionsMonitor<AppSettings> options;
+
+        public SeoModelBuilder(IOptionsMonitor<AppSettings> options)
+        {
+            this.options = options;
+        }
+
         public async Task<SeoViewModel> BuildModelAsync(ComponentModelBase componentModel)
         {
-            var graphQLClient = new GraphQLHttpClient("http://content-api:5111/graphql", new NewtonsoftJsonSerializer());
+            var graphQLClient = new GraphQLHttpClient(this.options.CurrentValue.ContentGraphQlUrl, new NewtonsoftJsonSerializer());
+            graphQLClient.HttpClient.DefaultRequestHeaders.Add("Authorization", "Bearer 2c994cdbc32d6257d3529b5b767359364672c53548a6f324ff5cf3a76b3826c52ff159272732cd4627b60cd002005b785e8d98d29a90acdc527d11fba1e1c7586166ee785c3697b039fb29d84e7ea11c43bbed7c1101c7bf50f772c340a31356bea2dfbec174bcce34505bc8d43897eb2906cdcaf0e4a9dbc74fa6b2af1c2e25");
+
 
             var seoRequest = new GraphQLRequest
             {
                 Query = @"
-                {
-                    home-page {
+                 query {
+                  homePage {
+		            data {
+                      id,
+                      attributes {
                         seo {
-                            metaTitle,
-                            metaDescription
+                          metaTitle,
+                          metaDescription
                         }
-                    }
+                      }
+	                }
+                  }
                 }"
             };
 
