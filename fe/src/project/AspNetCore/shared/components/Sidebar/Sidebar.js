f5c9787c664910ebc4e3fb170da022e6b4450df1
SebaKas21 <93117743+SebaKas21@users.noreply.github.com>
2022-05-19 17:24:18 +0200

    Redirection to product details at the sidebar (#76)
    * Added handle for image
    
    * Added handle for sidebar details
    
    Co-authored-by: Dawid Dworak <dawiddworak88@gmail.com>

--- fe/src/project/AspNetCore/shared/components/Sidebar/Sidebar.js
+++ fe/src/project/AspNetCore/shared/components/Sidebar/Sidebar.js
@@ -125,15 +125,19 @@ const Sidebar = (props) => {
                                     return (
                                         <ListItem className="sidebar-item">
                                             <div className="sidebar-item__row">
-                                                <figure className="sidebar-item__image">
-                                                    <ResponsiveImage sources={carouselItem.sources} imageSrc={carouselItem.imageUrl} imageAlt={carouselItem.imageAlt} />
-                                                </figure>
+                                                <a href={carouselItem.url}>
+                                                    <figure className="sidebar-item__image">
+                                                        <ResponsiveImage sources={carouselItem.sources} imageSrc={carouselItem.imageUrl} imageAlt={carouselItem.imageAlt} />
+                                                    </figure>
+                                                </a>
                                                 <div className="sidebar-item__details">
-                                                    <h1 className="title">{carouselItem.title}</h1>
-                                                    <span className="attribute">{labels.skuLabel} {carouselItem.subtitle}</span>
-                                                    {carouselItem.ean &&
-                                                        <span className="attribute">{labels.eanLabel} {carouselItem.ean}</span>
-                                                    }
+                                                    <a href={carouselItem.url}>
+                                                        <h1 className="title">{carouselItem.title}</h1>
+                                                        <span className="attribute">{labels.skuLabel} {carouselItem.subtitle}</span>
+                                                        {carouselItem.ean &&
+                                                            <span className="attribute">{labels.eanLabel} {carouselItem.ean}</span>
+                                                        }
+                                                    </a>
                                                     <div className="stock-details">
                                                         {carouselItem.availableQuantity && carouselItem.availableQuantity > 0 &&
                                                             <div className="stock">
