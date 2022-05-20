c304ee7490605ee49c8cc6bb628709963621d174
SebaKas21 <sebastian.kasperek@eltap.com>
2022-05-20 11:45:25 +0200

    Fixed inputs styling and removed redundant section

--- be/src/Project/Services/Identity/Identity.Api/ServicesModels/Users/RegisterServiceModel.cs
+++ be/src/Project/Services/Identity/Identity.Api/ServicesModels/Users/RegisterServiceModel.cs
@@ -15,8 +15,5 @@ namespace Identity.Api.ServicesModels.Users
         public string CompanyCity { get; set; }
         public string CompanyRegion { get; set; }
         public string CompanyPostalCode { get; set; }
-        public string AcceptReturns { get; set; }
-        public string DirectlyShip { get; set; }
-        public string OnlineRetailers { get; set; }
     }
 }
