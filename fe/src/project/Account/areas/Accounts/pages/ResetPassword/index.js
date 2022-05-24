ad9e4bea61e638bd4b9743b8f5bfebdadd273a1e
SebaKas21 <sebastian.kasperek@eltap.com>
2022-05-24 15:12:35 +0200

    Added hydrate to accounts

--- fe/src/project/Account/areas/Accounts/pages/ResetPassword/index.js
+++ fe/src/project/Account/areas/Accounts/pages/ResetPassword/index.js
@@ -1,8 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { hydrateRoot } from 'react-dom/client';
 import ResetPasswordPage from "./ResetPasswordPage";
 import CssSsrRemovalHelper from "../../../../../../shared/helpers/globals/CssSsrRemovalHelper";
 
 CssSsrRemovalHelper.remove();
 
-ReactDOM.hydrate(<ResetPasswordPage {...window.data} />, document.getElementById("root"));
+hydrateRoot(document.getElementById("root"), <ResetPasswordPage {...window.data} />)
