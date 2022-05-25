6ffbf7ebb3f78c16837b7e1d4a771c686cfa134e
SebaKas21 <sebastian.kasperek@eltap.com>
2022-05-25 07:40:50 +0200

    Added hydrate to dashboard page

--- fe/src/project/AspNetCore/areas/Dashboard/pages/SettingsPage/index.js
+++ fe/src/project/AspNetCore/areas/Dashboard/pages/SettingsPage/index.js
@@ -1,8 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { hydrateRoot } from 'react-dom/client';
 import SettingsPage from "./SettingsPage";
 import CssSsrRemovalHelper from "../../../../../../shared/helpers/globals/CssSsrRemovalHelper";
 
 CssSsrRemovalHelper.remove();
 
-ReactDOM.hydrate(<SettingsPage {...window.data} />, document.getElementById("root"));
+hydrateRoot(document.getElementById("root"), <SettingsPage {...window.data} />)
