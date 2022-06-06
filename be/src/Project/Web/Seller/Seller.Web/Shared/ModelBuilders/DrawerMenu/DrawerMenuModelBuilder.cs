25ef21ce11c2daf927819e86f315a766621a77ba
Sebastian Kasperek <sebastian.kasperek@eltap.com>
2022-06-06 11:03:26 +0200

    Added roles to drawer menu

--- be/src/Project/Web/Seller/Seller.Web/Shared/ModelBuilders/DrawerMenu/DrawerMenuModelBuilder.cs
+++ be/src/Project/Web/Seller/Seller.Web/Shared/ModelBuilders/DrawerMenu/DrawerMenuModelBuilder.cs
@@ -116,6 +116,12 @@ namespace Seller.Web.Shared.ModelBuilders.DrawerMenu
                         },
                         new DrawerMenuItemViewModel
                         {
+                            Icon = IconsConstants.Key,
+                            Title = this.globalLocalizer.GetString("ClientsRoles"),
+                            Url = this.linkGenerator.GetPathByAction("Index", "ClientRoles", new { Area = "Clients", culture = CultureInfo.CurrentUICulture.Name })
+                        },
+                        new DrawerMenuItemViewModel
+                        {
                             Icon = IconsConstants.Users,
                             Title = this.globalLocalizer.GetString("ClientsGroups"),
                             Url = this.linkGenerator.GetPathByAction("Index", "ClientGroups", new { Area = "Clients", culture = CultureInfo.CurrentUICulture.Name })
