b1aa8ecf61210a6c8e1bdc5b53c8358e439e2d04
Sebastian Kasperek <sebastian.kasperek@eltap.com>
2022-06-07 10:30:47 +0200

    Added tiles and drawer

--- be/src/Project/Web/Seller/Seller.Web/Shared/ModelBuilders/MenuTiles/MenuTilesModelBuilder.cs
+++ be/src/Project/Web/Seller/Seller.Web/Shared/ModelBuilders/MenuTiles/MenuTilesModelBuilder.cs
@@ -90,6 +90,12 @@ namespace Seller.Web.Shared.ModelBuilders.MenuTiles
                     },
                     new MenuTileViewModel
                     {
+                        Icon = IconsConstants.Briefcase,
+                        Title = this.globalLocalizer.GetString("ClientManagers"),
+                        Url = this.linkGenerator.GetPathByAction("Index", "ClientManagers", new { Area = "Clients", culture = CultureInfo.CurrentUICulture.Name })
+                    },
+                    new MenuTileViewModel
+                    {
                         Icon = IconsConstants.Grid,
                         Title = this.globalLocalizer.GetString("ClientsGroups"),
                         Url = this.linkGenerator.GetPathByAction("Index", "ClientGroups", new { Area = "Clients", culture = CultureInfo.CurrentUICulture.Name })
