226893ee8f0cff6d14a3748d46d0c4cb96124d99
Sebastian Kasperek <sebastian.kasperek@eltap.com>
2022-06-21 13:40:45 +0200

    Moved constant to docker

--- be/src/Project/Services/Media/Media.Api/Infrastructure/Media/Seeds/MediaSeed.cs
+++ be/src/Project/Services/Media/Media.Api/Infrastructure/Media/Seeds/MediaSeed.cs
@@ -12,45 +12,45 @@ namespace Media.Api.Infrastructure.Media.Seeds
 {
     public static class MediaSeed
     {
-        public static void SeedHeaders(MediaContext context, string storageConnectionString, IChecksumService checksumService)
+        public static void SeedHeaders(MediaContext context, string storageConnectionString, Guid? organisationId, IChecksumService checksumService)
         {
-            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.Headers.LogoMediaId, MediaConstants.Headers.LogoMediaVersionId, MediaConstants.General.DefaultOrganisationId, MediaConstants.Headers.LogoMediaUrl);
-            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.Headers.FaviconMediaId, MediaConstants.Headers.FaviconMediaVersionId, MediaConstants.General.DefaultOrganisationId, MediaConstants.Headers.FaviconMediaUrl);
+            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.Headers.LogoMediaId, MediaConstants.Headers.LogoMediaVersionId, organisationId, MediaConstants.Headers.LogoMediaUrl);
+            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.Headers.FaviconMediaId, MediaConstants.Headers.FaviconMediaVersionId, organisationId, MediaConstants.Headers.FaviconMediaUrl);
         }
 
-        public static void SeedHeroSliderItems(MediaContext context, string storageConnectionString, IChecksumService checksumService)
+        public static void SeedHeroSliderItems(MediaContext context, string storageConnectionString, Guid? organisationId, IChecksumService checksumService)
         {
-            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.HeroSliderItems.BoxspringsMediaId, MediaConstants.HeroSliderItems.BoxspringsMediaVersionId, MediaConstants.General.DefaultOrganisationId, MediaConstants.HeroSliderItems.BoxspringsMediaUrl);
-            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.HeroSliderItems.Boxsprings1600x400MediaId, MediaConstants.HeroSliderItems.Boxsprings1600x400MediaVersionId, MediaConstants.General.DefaultOrganisationId, MediaConstants.HeroSliderItems.Boxsprings1600x400MediaUrl);
-            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.HeroSliderItems.Boxsprings1024x400MediaId, MediaConstants.HeroSliderItems.Boxsprings1024x400MediaVersionId, MediaConstants.General.DefaultOrganisationId, MediaConstants.HeroSliderItems.Boxsprings1024x400MediaUrl);
-            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.HeroSliderItems.Boxsprings414x286MediaId, MediaConstants.HeroSliderItems.Boxsprings414x286MediaVersionId, MediaConstants.General.DefaultOrganisationId, MediaConstants.HeroSliderItems.Boxsprings414x286MediaUrl);
+            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.HeroSliderItems.BoxspringsMediaId, MediaConstants.HeroSliderItems.BoxspringsMediaVersionId, organisationId, MediaConstants.HeroSliderItems.BoxspringsMediaUrl);
+            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.HeroSliderItems.Boxsprings1600x400MediaId, MediaConstants.HeroSliderItems.Boxsprings1600x400MediaVersionId, organisationId, MediaConstants.HeroSliderItems.Boxsprings1600x400MediaUrl);
+            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.HeroSliderItems.Boxsprings1024x400MediaId, MediaConstants.HeroSliderItems.Boxsprings1024x400MediaVersionId, organisationId, MediaConstants.HeroSliderItems.Boxsprings1024x400MediaUrl);
+            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.HeroSliderItems.Boxsprings414x286MediaId, MediaConstants.HeroSliderItems.Boxsprings414x286MediaVersionId, organisationId, MediaConstants.HeroSliderItems.Boxsprings414x286MediaUrl);
 
-            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.HeroSliderItems.ChairsMediaId, MediaConstants.HeroSliderItems.ChairsMediaVersionId, MediaConstants.General.DefaultOrganisationId, MediaConstants.HeroSliderItems.ChairsMediaUrl);
-            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.HeroSliderItems.Chairs1600x400MediaId, MediaConstants.HeroSliderItems.Chairs1600x400MediaVersionId, MediaConstants.General.DefaultOrganisationId, MediaConstants.HeroSliderItems.Chairs1600x400MediaUrl);
-            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.HeroSliderItems.Chairs1024x400MediaId, MediaConstants.HeroSliderItems.Chairs1024x400MediaVersionId, MediaConstants.General.DefaultOrganisationId, MediaConstants.HeroSliderItems.Chairs1024x400MediaUrl);
-            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.HeroSliderItems.Chairs414x286MediaId, MediaConstants.HeroSliderItems.Chairs414x286MediaVersionId, MediaConstants.General.DefaultOrganisationId, MediaConstants.HeroSliderItems.Chairs414x286MediaUrl);
+            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.HeroSliderItems.ChairsMediaId, MediaConstants.HeroSliderItems.ChairsMediaVersionId, organisationId, MediaConstants.HeroSliderItems.ChairsMediaUrl);
+            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.HeroSliderItems.Chairs1600x400MediaId, MediaConstants.HeroSliderItems.Chairs1600x400MediaVersionId, organisationId, MediaConstants.HeroSliderItems.Chairs1600x400MediaUrl);
+            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.HeroSliderItems.Chairs1024x400MediaId, MediaConstants.HeroSliderItems.Chairs1024x400MediaVersionId, organisationId, MediaConstants.HeroSliderItems.Chairs1024x400MediaUrl);
+            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.HeroSliderItems.Chairs414x286MediaId, MediaConstants.HeroSliderItems.Chairs414x286MediaVersionId, organisationId, MediaConstants.HeroSliderItems.Chairs414x286MediaUrl);
 
-            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.HeroSliderItems.CornersMediaId, MediaConstants.HeroSliderItems.CornersMediaVersionId, MediaConstants.General.DefaultOrganisationId, MediaConstants.HeroSliderItems.CornersMediaUrl);
-            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.HeroSliderItems.Corners1600x400MediaId, MediaConstants.HeroSliderItems.Corners1600x400MediaVersionId, MediaConstants.General.DefaultOrganisationId, MediaConstants.HeroSliderItems.Corners1600x400MediaUrl);
-            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.HeroSliderItems.Corners1024x400MediaId, MediaConstants.HeroSliderItems.Corners1024x400MediaVersionId, MediaConstants.General.DefaultOrganisationId, MediaConstants.HeroSliderItems.Corners1024x400MediaUrl);
-            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.HeroSliderItems.Corners414x286MediaId, MediaConstants.HeroSliderItems.Corners414x286MediaVersionId, MediaConstants.General.DefaultOrganisationId, MediaConstants.HeroSliderItems.Corners414x286MediaUrl);
+            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.HeroSliderItems.CornersMediaId, MediaConstants.HeroSliderItems.CornersMediaVersionId, organisationId, MediaConstants.HeroSliderItems.CornersMediaUrl);
+            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.HeroSliderItems.Corners1600x400MediaId, MediaConstants.HeroSliderItems.Corners1600x400MediaVersionId, organisationId, MediaConstants.HeroSliderItems.Corners1600x400MediaUrl);
+            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.HeroSliderItems.Corners1024x400MediaId, MediaConstants.HeroSliderItems.Corners1024x400MediaVersionId, organisationId, MediaConstants.HeroSliderItems.Corners1024x400MediaUrl);
+            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.HeroSliderItems.Corners414x286MediaId, MediaConstants.HeroSliderItems.Corners414x286MediaVersionId, organisationId, MediaConstants.HeroSliderItems.Corners414x286MediaUrl);
 
-            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.HeroSliderItems.SetsMediaId, MediaConstants.HeroSliderItems.SetsMediaVersionId, MediaConstants.General.DefaultOrganisationId, MediaConstants.HeroSliderItems.SetsMediaUrl);
-            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.HeroSliderItems.Sets1600x400MediaId, MediaConstants.HeroSliderItems.Sets1600x400MediaVersionId, MediaConstants.General.DefaultOrganisationId, MediaConstants.HeroSliderItems.Sets1600x400MediaUrl);
-            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.HeroSliderItems.Sets1024x400MediaId, MediaConstants.HeroSliderItems.Sets1024x400MediaVersionId, MediaConstants.General.DefaultOrganisationId, MediaConstants.HeroSliderItems.Sets1024x400MediaUrl);
-            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.HeroSliderItems.Sets414x286MediaId, MediaConstants.HeroSliderItems.Sets414x286MediaVersionId, MediaConstants.General.DefaultOrganisationId, MediaConstants.HeroSliderItems.Sets414x286MediaUrl);
+            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.HeroSliderItems.SetsMediaId, MediaConstants.HeroSliderItems.SetsMediaVersionId, organisationId, MediaConstants.HeroSliderItems.SetsMediaUrl);
+            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.HeroSliderItems.Sets1600x400MediaId, MediaConstants.HeroSliderItems.Sets1600x400MediaVersionId, organisationId, MediaConstants.HeroSliderItems.Sets1600x400MediaUrl);
+            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.HeroSliderItems.Sets1024x400MediaId, MediaConstants.HeroSliderItems.Sets1024x400MediaVersionId, organisationId, MediaConstants.HeroSliderItems.Sets1024x400MediaUrl);
+            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.HeroSliderItems.Sets414x286MediaId, MediaConstants.HeroSliderItems.Sets414x286MediaVersionId, organisationId, MediaConstants.HeroSliderItems.Sets414x286MediaUrl);
         }
 
-        public static void SeedCategories(MediaContext context, string storageConnectionString, IChecksumService checksumService)
+        public static void SeedCategories(MediaContext context, string storageConnectionString, Guid? organisationId, IChecksumService checksumService)
         {
-            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.Categories.CouchesMediaId, MediaConstants.Categories.CouchesMediaVersionId, MediaConstants.General.DefaultOrganisationId, MediaConstants.Categories.CouchesMediaUrl);
-            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.Categories.SectionalsMediaId, MediaConstants.Categories.SectionalsMediaVersionId, MediaConstants.General.DefaultOrganisationId, MediaConstants.Categories.SectionalsMediaUrl);
-            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.Categories.CoffeeTablesMediaId, MediaConstants.Categories.CoffeeTablesMediaVersionId, MediaConstants.General.DefaultOrganisationId, MediaConstants.Categories.CoffeeTablesMediaUrl);
-            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.Categories.ChairsMediaId, MediaConstants.Categories.ChairsMediaVersionId, MediaConstants.General.DefaultOrganisationId, MediaConstants.Categories.ChairsMediaUrl);
-            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.Categories.PoufsMediaId, MediaConstants.Categories.PoufsMediaVersionId, MediaConstants.General.DefaultOrganisationId, MediaConstants.Categories.PoufsMediaUrl);
-            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.Categories.SetsMediaId, MediaConstants.Categories.SetsMediaVersionId, MediaConstants.General.DefaultOrganisationId, MediaConstants.Categories.SetsMediaUrl);
-            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.Categories.BedsMediaId, MediaConstants.Categories.BedsMediaVersionId, MediaConstants.General.DefaultOrganisationId, MediaConstants.Categories.BedsMediaUrl);
-            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.Categories.MattressesMediaId, MediaConstants.Categories.MattressesMediaVersionId, MediaConstants.General.DefaultOrganisationId, MediaConstants.Categories.MattressesMediaUrl);
+            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.Categories.CouchesMediaId, MediaConstants.Categories.CouchesMediaVersionId, organisationId, MediaConstants.Categories.CouchesMediaUrl);
+            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.Categories.SectionalsMediaId, MediaConstants.Categories.SectionalsMediaVersionId, organisationId, MediaConstants.Categories.SectionalsMediaUrl);
+            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.Categories.CoffeeTablesMediaId, MediaConstants.Categories.CoffeeTablesMediaVersionId, organisationId, MediaConstants.Categories.CoffeeTablesMediaUrl);
+            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.Categories.ChairsMediaId, MediaConstants.Categories.ChairsMediaVersionId, organisationId, MediaConstants.Categories.ChairsMediaUrl);
+            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.Categories.PoufsMediaId, MediaConstants.Categories.PoufsMediaVersionId, organisationId, MediaConstants.Categories.PoufsMediaUrl);
+            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.Categories.SetsMediaId, MediaConstants.Categories.SetsMediaVersionId, organisationId, MediaConstants.Categories.SetsMediaUrl);
+            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.Categories.BedsMediaId, MediaConstants.Categories.BedsMediaVersionId, organisationId, MediaConstants.Categories.BedsMediaUrl);
+            SeedMedia(context, checksumService, storageConnectionString, MediaConstants.Categories.MattressesMediaId, MediaConstants.Categories.MattressesMediaVersionId, organisationId, MediaConstants.Categories.MattressesMediaUrl);
         }
 
         private static void SeedMedia(MediaContext context, IChecksumService checksumService, string storageConnectionString, Guid mediaId, Guid mediaVersionId, Guid? organisationId, string mediaUrl)
