2dc231611df4d9f81f96f9d8d3866013896cccb0
Sebastian Kasperek <sebastian.kasperek@eltap.com>
2022-06-30 11:51:28 +0200

    Moved client application from Accounts to AspNetCore

---
+++ be/src/Project/Web/Buyer/Buyer.Web/Areas/Home/Controllers/ApplicationController.cs
@@ -0,0 +1,12 @@
+﻿using Foundation.Extensions.Controllers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Buyer.Web.Areas.Home.Controllers
+{
+    [Area("Home")]
+    [AllowAnonymous]
+    public class ApplicationController : BaseController
+    {
+    }
+}
