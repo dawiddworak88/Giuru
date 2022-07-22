1a493fa815317545ba90432e284ef36fdd8d0763
Sebastian Kasperek <sebastian.kasperek@eltap.com>
2022-07-22 09:50:37 +0200

    Review changes

--- fe/src/project/AspNetCore/areas/DownloadCenter/components/CategoryDetails/CategoryDetails.js
+++ fe/src/project/AspNetCore/areas/DownloadCenter/components/CategoryDetails/CategoryDetails.js
@@ -6,21 +6,19 @@ const CategoryDetails = (props) => {
     return (
         <div className="section dc-category">
             <div className="container">
-                <div className="dc-category-container">
-                    <h3 className="title is-3">{props.title}</h3>
-                    <div className="is-flex is-flex-wrap-wrap">
-                        {props.subcategories.length > 0 && props.subcategories.map((subcategory, index) => {
-                            return (
-                                <a href={subcategory.url} className="dc-category__subcategory-link is-flex is-justify-content-center is-align-items-center m-2" key={index}>
-                                    <span className="subtitle is-6">{subcategory.name}</span>
-                                </a>
-                            )
-                        })}
-                    </div>
-                    {props.files &&
-                        <DownloadCenterFiles {...props.files} />
-                    }
+                <h3 className="title is-3">{props.title}</h3>
+                <div className="is-flex is-flex-wrap-wrap">
+                    {props.subcategories.length > 0 && props.subcategories.map((subcategory, index) => {
+                        return (
+                            <a href={subcategory.url} className="dc-category__subcategory-link is-flex is-justify-content-center is-align-items-center m-2" key={index}>
+                                <span className="subtitle is-6">{subcategory.name}</span>
+                            </a>
+                        )
+                    })}
                 </div>
+                {props.files &&
+                    <DownloadCenterFiles {...props.files} />
+                }
             </div>
         </div>
     )
@@ -28,7 +26,7 @@ const CategoryDetails = (props) => {
 
 CategoryDetails.propTypes = {
     title: PropTypes.string.isRequired,
-    categories: PropTypes.array,
+    subcategories: PropTypes.array,
     files: PropTypes.object
 }
 
