a3cf9334ba66f61c3b2a0162a7c03437111adde9
Sebastian Kasperek <sebastian.kasperek@eltap.com>
2022-09-28 19:02:55 +0200

    Added base CsvFormatter

---
+++ be/src/Infrastructure/Extensions/Foundation.Extensions/Formatters/CsvFormatter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace Foundation.Extensions.Formatters
+{
+    public class CsvFormatter : BufferedMediaTypeFormatter
+    {
+        public CsvFormatter()
+        {
+            this.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/csv"));
+            this.SupportedEncodings.Add(Encoding.UTF8);
+            this.SupportedEncodings.Add(Encoding.Unicode);
+        }
+
+        public override bool CanReadType(Type type)
+        {
+            return false;
+        }
+
+        public override bool CanWriteType(Type type)
+        {
+            if (type is null)
+            {
+                throw new ArgumentNullException("type");
+            }
+
+            return this.IsTypeOfIEnumerable(type);
+        }
+
+        public override void WriteToStream(Type type, object value, Stream writeStream, HttpContent content)
+        {
+            base.WriteToStream(type, value, writeStream, content);
+        }
+
+        private bool IsTypeOfIEnumerable(Type type)
+        {
+            foreach (Type interfaceType in type.GetInterfaces())
+            {
+                if (interfaceType == typeof(IEnumerable))
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
