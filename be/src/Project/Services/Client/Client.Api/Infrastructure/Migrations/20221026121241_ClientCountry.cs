2cf04105a10af3fa11ca0be09990aae3cfe5abd8
Sebastian Kasperek <sebastian.kasperek@eltap.com>
2022-10-26 14:19:06 +0200

    Added migration

---
+++ be/src/Project/Services/Client/Client.Api/Infrastructure/Migrations/20221026121241_ClientCountry.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Client.Api.Infrastructure.Migrations
+{
+    public partial class ClientCountry : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "Clients",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "Clients");
+        }
+    }
+}
