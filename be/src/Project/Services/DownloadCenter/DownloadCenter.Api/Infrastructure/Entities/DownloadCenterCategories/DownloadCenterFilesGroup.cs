36e13362c8be4a8110eeb4a5591f7bafda1c51de
Sebastian Kasperek <sebastian.kasperek@eltap.com>
2022-11-09 15:14:46 +0100

    Added migration for download center

---
+++ be/src/Project/Services/DownloadCenter/DownloadCenter.Api/Infrastructure/Entities/DownloadCenterCategories/DownloadCenterFilesGroup.cs
@@ -0,0 +1,15 @@
+﻿using Foundation.GenericRepository.Entities;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DownloadCenter.Api.Infrastructure.Entities.DownloadCenterCategories
+{
+    public class DownloadCenterFilesGroup : Entity
+    {
+        [Required]
+        public Guid MediaItemId { get; set; }
+
+        [Required]
+        public Guid CategoryFileId { get; set; }
+    }
+}
---
+++ be/src/Project/Services/DownloadCenter/DownloadCenter.Api/Infrastructure/Migrations/20221109141417_DownloadCenterGroups.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DownloadCenter.Api.Infrastructure.Migrations
+{
+    public partial class DownloadCenterGroups : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DownloadCenterFilesGroups",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    MediaItemId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CategoryFileId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    RowVersion = table.Column<byte[]>(type: "rowversion", rowVersion: true, nullable: true),
+                    LastModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DownloadCenterFilesGroups", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DownloadCenterFilesGroups");
+        }
+    }
+}
