869a08228c859867cb454c6df34a6206d54ed91d
Dawid Dworak <dawiddworak88@gmail.com>
2022-12-03 20:17:47 +0100

    Updated strapi to the latest version

--- be/src/Project/Services/Content/Content.Api/config/admin.js
+++ be/src/Project/Services/Content/Content.Api/config/admin.js
@@ -1,5 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', 'f954ceeddf0d400f5152642c61b21987'),
+    secret: env('ADMIN_JWT_SECRET'),
+  },
+  apiToken: {
+    salt: env('API_TOKEN_SALT'),
   },
 });
