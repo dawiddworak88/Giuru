060c40529d0c08d0a8f58012c8080adb37399a66
Sebastian Kasperek <sebastian.kasperek@eltap.com>
2022-12-30 15:01:02 +0100

    Added content repository

--- be/src/Project/Services/Identity/Identity.Api/Areas/Home/DependencyInjection/CompositionRoot.cs
+++ be/src/Project/Services/Identity/Identity.Api/Areas/Home/DependencyInjection/CompositionRoot.cs
@@ -2,6 +2,7 @@
 using Foundation.PageContent.ComponentModels;
 using Foundation.PageContent.Components.Metadatas.ViewModels;
 using Identity.Api.Areas.Home.ModelBuilders;
+using Identity.Api.Areas.Home.Repositories.Content;
 using Identity.Api.Areas.Home.Services.Contents;
 using Identity.Api.Areas.Home.ViewModels;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,6 +14,7 @@ namespace Identity.Api.Areas.Home.DependencyInjection
         public static void RegisterHomeViewsDependencies(this IServiceCollection services)
         {
             services.AddScoped<IContentService, ContentService>();
+            services.AddScoped<IContentRepository, ContentRepository>();
             services.AddScoped<IAsyncComponentModelBuilder<ComponentModelBase, PrivacyPolicyPageViewModel>, PrivacyPolicyPageModelBuilder>();
             services.AddScoped<IAsyncComponentModelBuilder<ComponentModelBase, RegulationsPageViewModel>, RegulationsPageModelBuilder>();
         }
--- be/src/Project/Services/Identity/Identity.Api/Areas/Home/ModelBuilders/RegulationsPageModelBuilder.cs
+++ be/src/Project/Services/Identity/Identity.Api/Areas/Home/ModelBuilders/RegulationsPageModelBuilder.cs
@@ -1,15 +1,11 @@
 ﻿using Foundation.Extensions.ModelBuilders;
-using Foundation.Localization;
 using Foundation.PageContent.ComponentModels;
 using Foundation.PageContent.Components.Footers.ViewModels;
 using Foundation.PageContent.Components.Headers.ViewModels;
 using Foundation.PageContent.Components.Metadatas.ViewModels;
+using Identity.Api.Areas.Home.Repositories.Content;
 using Identity.Api.Areas.Home.ViewModels;
-using Identity.Api.Configurations;
-using Microsoft.Extensions.Localization;
-using Microsoft.Extensions.Options;
 using System.Threading.Tasks;
-using System.Web;
 
 namespace Identity.Api.Areas.Home.ModelBuilders
 {
@@ -18,33 +14,32 @@ namespace Identity.Api.Areas.Home.ModelBuilders
         private readonly IAsyncComponentModelBuilder<ComponentModelBase, MetadataViewModel> _seoModelBuilder;
         private readonly IModelBuilder<HeaderViewModel> _headerModelBuilder;
         private readonly IModelBuilder<FooterViewModel> _footerModelBuilder;
-        private readonly IStringLocalizer<GlobalResources> _globalLocalizer;
-        private readonly IOptionsMonitor<AppSettings> _options;
+        private readonly IContentRepository _contentRepository;
 
         public RegulationsPageModelBuilder(
             IAsyncComponentModelBuilder<ComponentModelBase, MetadataViewModel> seoModelBuilder,
             IModelBuilder<HeaderViewModel> headerModelBuilder,
             IModelBuilder<FooterViewModel> footerModelBuilder,
-            IStringLocalizer<GlobalResources> globalLocalizer,
-            IOptionsMonitor<AppSettings> options)
+            IContentRepository contentRepository)
         {
             _seoModelBuilder = seoModelBuilder;
             _headerModelBuilder = headerModelBuilder;
             _footerModelBuilder = footerModelBuilder;
-            _globalLocalizer = globalLocalizer;
-            _options = options;
+            _contentRepository = contentRepository;
         }
 
         public async Task<RegulationsPageViewModel> BuildModelAsync(ComponentModelBase componentModel)
         {
+            var content = await _contentRepository.GetContentAsync(componentModel.ContentPageKey, componentModel.Language);
+
             var viewModel = new RegulationsPageViewModel
             {
                 Metadata = await _seoModelBuilder.BuildModelAsync(componentModel),
                 Header = _headerModelBuilder.BuildModel(),
                 Content = new ContentPageViewModel
                 { 
-                    Title = _globalLocalizer.GetString("Regulations"),
-                    Content = HttpUtility.UrlDecode(_options.CurrentValue.Regulations)
+                    Title = content.Title,
+                    Content = content.Text
                 },
                 Footer = _footerModelBuilder.BuildModel()
             };
