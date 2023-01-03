dbd6bbbac919db09028b1a1b379305cc49058292
Sebastian Kasperek <sebastian.kasperek@eltap.com>
2023-01-03 14:52:52 +0100

    Added improvements

--- be/src/Project/Services/Inventory/Inventory.Api/Services/Products/ProductService.cs
+++ be/src/Project/Services/Inventory/Inventory.Api/Services/Products/ProductService.cs
@@ -7,29 +7,29 @@ namespace Inventory.Api.Services.Products
 {
     public class ProductService : IProductService
     {
-        private readonly InventoryContext context;
+        private readonly InventoryContext _context;
 
         public ProductService(InventoryContext context)
         {
-            this.context = context;
+            _context = context;
         }
 
         public async Task DeleteProductAsync(Guid? productId)
         {
-            var product = await this.context.Products.FirstOrDefaultAsync(x => x.Id == productId.Value && x.IsActive);
+            var product = await _context.Products.FirstOrDefaultAsync(x => x.Id == productId.Value && x.IsActive);
 
             if (product is not null)
             {
                 product.IsActive = false;
                 product.LastModifiedDate = DateTime.UtcNow;
 
-                await this.context.SaveChangesAsync();
+                await _context.SaveChangesAsync();
             }
         }
 
         public async Task UpdateProductAsync(Guid? productId, string productName, string productSku, string productEan)
         {
-            var product = await this.context.Products.FirstOrDefaultAsync(x => x.Id == productId.Value && x.IsActive);
+            var product = await _context.Products.FirstOrDefaultAsync(x => x.Id == productId.Value && x.IsActive);
 
             if (product is not null)
             {
@@ -38,7 +38,7 @@ namespace Inventory.Api.Services.Products
                 product.Ean = productEan;
                 product.LastModifiedDate = DateTime.UtcNow;
 
-                await this.context.SaveChangesAsync();
+                await _context.SaveChangesAsync();
             }
         }
     }
