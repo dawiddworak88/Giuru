97caab560d532f3b941f32d9bc4c69df259393f1
Dawid Dworak <dawiddworak88@gmail.com>
2023-01-04 16:38:36 +0100

    Added non-clustered index to ordering db (#213)

--- be/src/Project/Services/Ordering/Ordering.Api/Infrastructure/Migrations/20230104145015_AddedNonClusteredIndexToOrderItemStatusChangeIsActiveOrderItemId.Designer.cs
+++ be/src/Project/Services/Ordering/Ordering.Api/Infrastructure/Migrations/20230104145015_AddedNonClusteredIndexToOrderItemStatusChangeIsActiveOrderItemId.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Ordering.Api.Infrastructure;
 
@@ -11,9 +12,10 @@ using Ordering.Api.Infrastructure;
 namespace Ordering.Api.Infrastructure.Migrations
 {
     [DbContext(typeof(OrderingContext))]
-    partial class OrderingContextModelSnapshot : ModelSnapshot
+    [Migration("20230104145015_AddedNonClusteredIndexToOrderItemStatusChangeIsActiveOrderItemId")]
+    partial class AddedNonClusteredIndexToOrderItemStatusChangeIsActiveOrderItemId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
