26211c2a46f57f0c335a4877af881fd8bb8a9c7a
Sebastian Kasperek <sebastian.kasperek@eltap.com>
2023-01-10 12:42:55 +0100

    Added migration

---
+++ be/src/Project/Services/Client/Client.Api/Infrastructure/Migrations/20230110112858_RemovedCountryColumn.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Client.Api.Infrastructure.Migrations
+{
+    public partial class RemovedCountryColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "Clients");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
