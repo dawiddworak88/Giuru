158ed0dc5f46d8f25781d1c7e391fc54e9b585f4
Sebastian Kasperek <sebastian.kasperek@eltap.com>
2023-02-09 11:11:51 +0100

    Improved analytics api

--- be/src/Project/Services/Analytics/Analytics.Api/ServicesModels/SalesAnalytics/DailySalesServiceModel.cs
+++ be/src/Project/Services/Analytics/Analytics.Api/ServicesModels/SalesAnalytics/DailySalesServiceModel.cs
@@ -1,7 +1,10 @@
-﻿namespace Analytics.Api.ServicesModels.SalesAnalytics
+﻿using System;
+
+namespace Analytics.Api.ServicesModels.SalesAnalytics
 {
     public class DailySalesServiceModel
     {
+        public int DayOfWeek { get; set; }
         public int Day { get; set; }
         public int Year { get; set; }
         public int Month { get; set; }
--- be/src/Project/Services/Analytics/Analytics.Api/v1/ResponseModels/DailySalesResponseModel.cs
+++ be/src/Project/Services/Analytics/Analytics.Api/v1/ResponseModels/DailySalesResponseModel.cs
@@ -2,6 +2,7 @@
 {
     public class DailySalesResponseModel
     {
+        public int DayOfWeek { get; set; }
         public int Day { get; set; }
         public int Year { get; set; }
         public int Month { get; set; }
