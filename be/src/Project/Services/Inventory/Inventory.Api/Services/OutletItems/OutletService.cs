a6ecdc1ca55422bf43210f8de3be41b3635f60e2
Sebastian Kasperek <sebastian.kasperek@eltap.com>
2023-02-22 12:46:37 +0100

    Updated choosing title and description for outlet items

--- be/src/Project/Services/Inventory/Inventory.Api/Services/OutletItems/OutletService.cs
+++ be/src/Project/Services/Inventory/Inventory.Api/Services/OutletItems/OutletService.cs
@@ -9,6 +9,7 @@ using Inventory.Api.Infrastructure.Entities;
 using Inventory.Api.ServicesModels.OutletServiceModels;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Localization;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -482,8 +483,8 @@ namespace Inventory.Api.Services.OutletItems
                         AvailableQuantity = y.AvailableQuantity,
                         Quantity = y.Quantity,
                         OutletId = y.Id,
-                        Title = y.Translations.FirstOrDefault(t => t.OutletItemId == y.Id && t.Language == model.Language).Title,
-                        Description = y.Translations.FirstOrDefault(t => t.OutletItemId == y.Id && t.Language == model.Language).Description
+                        Title = y.Translations.FirstOrDefault(t => t.OutletItemId == y.Id && t.Language == model.Language) != null ? y.Translations.FirstOrDefault(t => t.OutletItemId == y.Id && t.Language == model.Language).Title : y.Translations.FirstOrDefault(t => t.OutletItemId == y.Id).Title,
+                        Description = y.Translations.FirstOrDefault(t => t.OutletItemId == y.Id && t.Language == model.Language) != null ? y.Translations.FirstOrDefault(t => t.OutletItemId == y.Id && t.Language == model.Language).Description : y.Translations.FirstOrDefault(t => t.OutletItemId == y.Id).Description
                     });
 
             PagedResults<IEnumerable<OutletSumServiceModel>> pagedResults;
