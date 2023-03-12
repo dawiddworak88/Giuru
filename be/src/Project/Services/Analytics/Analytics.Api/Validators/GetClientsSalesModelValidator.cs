c5bef4a0686eac05f57620e7aa174435411e0b62
Sebastian Kasperek <93117743+SebaKas21@users.noreply.github.com>
2023-03-12 12:38:46 +0100

    Extended AnalyticsApi with top selling clients (#267)
    * Added service models and validator
    
    * Added functionality
    
    * Minor changes for controller

---
+++ be/src/Project/Services/Analytics/Analytics.Api/Validators/GetClientsSalesModelValidator.cs
@@ -0,0 +1,13 @@
+﻿using Analytics.Api.ServicesModels.SalesAnalytics;
+using Analytics.Api.Shared.Validators;
+using FluentValidation;
+
+namespace Analytics.Api.Validators
+{
+    public class GetClientsSalesModelValidator : BaseChartServiceModelValidator<GetClientsSalesServiceModel>
+    {
+        public GetClientsSalesModelValidator() 
+        {
+        }
+    }
+}
