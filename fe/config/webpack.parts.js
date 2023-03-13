b073076e24f777e952ad88e133b0aeedd2056f3b
Sebastian Kasperek <sebastian.kasperek@eltap.com>
2023-03-13 14:24:20 +0100

    Added shared webpack parts

---
+++ fe/config/webpack.parts.js
@@ -0,0 +1,51 @@
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+exports.defaultWebpackConfiguration = ({
+
+} = {}) => {
+    return {
+        resolve: {
+            extensions: [".js", ".jsx"]
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: [
+                        { 
+                            loader: "babel-loader" 
+                        }
+                    ]
+                },
+                {
+                    test: /\.(png|svg|jpg|jpeg|gif|woff|woff2|eot|ttf|otf)$/i,
+                    type: 'asset/inline',
+                },
+                {
+                    test: /\.(scss|css)$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        "css-loader",
+                        "postcss-loader",
+                        "sass-loader"
+                    ]
+                }
+            ]
+        },
+        optimization: {
+            minimize: true,
+            minimizer: [
+                new TerserPlugin(),
+                new CssMinimizerPlugin()
+            ]
+        },
+        performance: {
+            hints: false,
+            maxEntrypointSize: 512000,
+            maxAssetSize: 512000
+        }
+    }
+}
