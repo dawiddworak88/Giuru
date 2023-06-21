3f10b3e8be6973932fba5f1120c0d77d48a504d8
Sebastian Kasperek <sebastian.kasperek@eltap.com>
2023-06-21 09:38:38 +0200

    Added schemas for other pages

---
+++ be/src/Project/Services/Content/Content.Api/src/api/fabrics-catalog-download-page/controllers/fabrics-catalog-download-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * fabrics-catalog-download-page controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::fabrics-catalog-download-page.fabrics-catalog-download-page');
---
+++ be/src/Project/Services/Content/Content.Api/src/api/fabrics-catalog-download-page/services/fabrics-catalog-download-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * fabrics-catalog-download-page service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::fabrics-catalog-download-page.fabrics-catalog-download-page');
