e6b98623eb60c963731ff5148e660ea8d51204d3
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-08-09 13:24:32 +0200

    Changed property in ImageViewModel

--- be/src/Project/Web/Buyer/Buyer.Web/Shared/ViewModels/Images/ImageViewModel.cs
+++ be/src/Project/Web/Buyer/Buyer.Web/Shared/ViewModels/Images/ImageViewModel.cs
@@ -6,10 +6,6 @@ namespace Buyer.Web.Shared.ViewModels.Images
 {
     public class ImageViewModel
     {
-        //public Guid? Id { get; set; }
-        //public string Original { get; set; }
-        //public string Thumbnail { get; set; }
-
         public string ImageSrc { get; set; }
         public IEnumerable<SourceViewModel> Sources { get; set; }
     }
