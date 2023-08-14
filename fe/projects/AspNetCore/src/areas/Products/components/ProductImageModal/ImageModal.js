e67afcd1fdcc30762e2b2dc682c90af6734ba354
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-08-14 15:02:50 +0200

    Improved ImageModal and fixed bug with image height in modal

--- fe/projects/AspNetCore/src/areas/Products/components/ProductImageModal/ImageModal.js
+++ fe/projects/AspNetCore/src/areas/Products/components/ProductImageModal/ImageModal.js
@@ -7,13 +7,13 @@ import LazyLoadConstants from "../../../../shared/constants/LazyLoadConstants";
 import ResponsiveImage from "../../../../shared/components/Picture/ResponsiveImage";
 
 const ImageModal = (props) => {
-    const { isOpen, handleClose, images, index } = props;
-    const [splideContext, setSplideContext] = useState(null);    
-
+    const { isOpen, handleClose, images, title, index } = props;
+    const [splideContext, setSplideContext] = useState(null);     
+        
     useEffect(() => {
-        if(splideContext) {
-            splideContext.go(index)
-        }
+        if(splideContext) {                        
+            splideContext.go(index)                  
+        }        
     })
 
     return (
@@ -25,26 +25,25 @@ const ImageModal = (props) => {
                 className: "image-modal"            
             }}
         >
-            <div className="is-flex is-justify-content-end">
+            <div className="is-flex is-justify-content-end p-2">
                 <Button variant="text" color="inherit" onClick={() => handleClose()} className="image-modal__close-button"><Close /></Button>
             </div>
             <div className="image-modal__container">
                 {images && images.length > 0 &&
                     <Splide
                         onVisible={(context) => setSplideContext(context)}                        
-                        className="image-modal__splide"
                         options={{
-                            type: "slide",
+                            type: 'fade',
                             pagination: false,                              
                             keyboard: 'global',                                                                                           
                         }}
                     >
                         {images.map((image, index) => {
                             return (
-                                <SplideSlide>
-                                    <LazyLoad offset={LazyLoadConstants.defaultOffset()} className="image-modal__image" key={index}>
-                                        <ResponsiveImage sources={image.sources} imageSrc={image.imageSrc} imageAlt={image.imageAlt} />
-                                    </LazyLoad>
+                                <SplideSlide>                                    
+                                        <LazyLoad offset={LazyLoadConstants.defaultOffset()} className="image-modal__image" key={index}>
+                                            <ResponsiveImage sources={image.sources} imageSrc={image.imageSrc} imageAlt={image.imageAlt} imageTitle={title}/>
+                                        </LazyLoad>                                                                      
                                 </SplideSlide>
                             )
                         })
