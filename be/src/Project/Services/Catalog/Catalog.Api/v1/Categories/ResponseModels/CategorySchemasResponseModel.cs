43ce43d7e777ddb595b4caaba57ec64fb8cf025a
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-08-18 10:29:44 +0200

    Added CategorySchemasResponseModel

---
+++ be/src/Project/Services/Catalog/Catalog.Api/v1/Categories/ResponseModels/CategorySchemasResponseModel.cs
@@ -0,0 +1,10 @@
+﻿using Catalog.Api.ServicesModels.Categories;
+using System.Collections.Generic;
+
+namespace Catalog.Api.v1.Categories.ResponseModels
+{
+    public class CategorySchemasResponseModel
+    {
+        public IEnumerable<CategorySchemaServiceModel> Schemas { get; set; }        
+    }
+}
