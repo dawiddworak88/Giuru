1818d09e87699db78976f09880743a8e4117d99b
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-08-18 12:12:37 +0200

    Added domain model CategorySchemas

---
+++ be/src/Project/Web/Seller/Seller.Web/Areas/Products/DomainModels/CategorySchemas.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Seller.Web.Areas.Products.DomainModels
+{
+    public class CategorySchemas
+    {
+        public IEnumerable<CategorySchema> Schemas { get; set; }
+    }
+}
