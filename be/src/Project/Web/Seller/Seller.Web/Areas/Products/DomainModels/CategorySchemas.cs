89b3b94725e34b32e39a9eab9c3896f8fdfb2240
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-08-24 13:16:04 +0200

    Added properties

--- be/src/Project/Web/Seller/Seller.Web/Areas/Products/DomainModels/CategorySchemas.cs
+++ be/src/Project/Web/Seller/Seller.Web/Areas/Products/DomainModels/CategorySchemas.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 
 namespace Seller.Web.Areas.Products.DomainModels
 {
     public class CategorySchemas
     {
+        public Guid? CategotyId { get; set; }
         public IEnumerable<CategorySchema> Schemas { get; set; }
+        public DateTime LastModifiedDate { get; set; }
+        public DateTime CreatedDate { get; set; }
     }
 }
