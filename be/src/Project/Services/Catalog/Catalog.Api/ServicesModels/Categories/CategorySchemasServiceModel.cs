7781d016ae0019b04aff22798005bced6855f4d4
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-08-24 13:00:17 +0200

    Added CategorySchemasServiceModel

---
+++ be/src/Project/Services/Catalog/Catalog.Api/ServicesModels/Categories/CategorySchemasServiceModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Catalog.Api.ServicesModels.Categories
+{
+    public class CategorySchemasServiceModel
+    {
+        public Guid? CategoryId { get; set; }
+        public IEnumerable<SchemaServiceModel> Schemas { get; set; }
+        public DateTime LastModifiedDate { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
