b7b423f695a8d870862efdc471dd408e857c725f
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-08-24 13:12:58 +0200

    Modified properties

--- be/src/Project/Services/Catalog/Catalog.Api/v1/Categories/ResponseModels/CategorySchemasResponseModel.cs
+++ be/src/Project/Services/Catalog/Catalog.Api/v1/Categories/ResponseModels/CategorySchemasResponseModel.cs
@@ -1,10 +1,14 @@
 ﻿using Catalog.Api.ServicesModels.Categories;
+using System;
 using System.Collections.Generic;
 
 namespace Catalog.Api.v1.Categories.ResponseModels
 {
     public class CategorySchemasResponseModel
     {
-        public IEnumerable<CategorySchemaServiceModel> Schemas { get; set; }        
+        public Guid? CategoryId { get; set; }
+        public IEnumerable<SchemaServiceModel> Schemas { get; set; }
+        public DateTime LastModifiedDate { get; set; }
+        public DateTime CreatedDate { get; set; }
     }
 }
