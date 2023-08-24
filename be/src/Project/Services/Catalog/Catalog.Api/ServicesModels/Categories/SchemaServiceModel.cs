c6016811bee0a604debb55e960cc8655be2a3649
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-08-24 13:00:31 +0200

    Added SchemaServiceModel

---
+++ be/src/Project/Services/Catalog/Catalog.Api/ServicesModels/Categories/SchemaServiceModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Catalog.Api.ServicesModels.Categories
+{
+    public class SchemaServiceModel
+    {
+        public Guid? Id { get; set; }
+        public string Schema { get; set; }
+        public string UiSchema { get; set; }
+        public string Language { get; set; }
+    }
+}
