967ac7d7929c3aecca010b0448804081b7330272
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-08-25 12:52:22 +0200

    Changed Schemas property typet to IEnumerable<CategorySchemaRequestModel>

--- be/src/Project/Web/Seller/Seller.Web/Areas/Products/ApiRequestModels/SaveCategoryRequestModel.cs
+++ be/src/Project/Web/Seller/Seller.Web/Areas/Products/ApiRequestModels/SaveCategoryRequestModel.cs
@@ -8,7 +8,7 @@ namespace Seller.Web.Areas.Products.ApiRequestModels
     public class SaveCategoryRequestModel : RequestModelBase
     {
         public string Name { get; set; }        
-        public IEnumerable<CategorySchema> Schemas { get; set; }        
+        public IEnumerable<CategorySchemaRequestModel> Schemas { get; set; }        
         public Guid? ParentCategoryId { get; set; }
         public IEnumerable<FileRequestModel> Files { get; set; }
     }
