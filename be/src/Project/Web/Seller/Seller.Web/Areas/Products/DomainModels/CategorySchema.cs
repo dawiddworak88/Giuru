2d7e3c2f30ec09150f924bf071c07dc56f5f4c25
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-08-25 12:45:18 +0200

    Added nullable to Id property

--- be/src/Project/Web/Seller/Seller.Web/Areas/Products/DomainModels/CategorySchema.cs
+++ be/src/Project/Web/Seller/Seller.Web/Areas/Products/DomainModels/CategorySchema.cs
@@ -4,7 +4,7 @@ namespace Seller.Web.Areas.Products.DomainModels
 {
     public class CategorySchema
     {
-        public Guid Id { get; set; }
+        public Guid? Id { get; set; }
         public string Schema { get; set; }
         public string UiSchema { get; set; }
         public string Language { get; set; }
--- be/src/Project/Web/Seller/Seller.Web/Areas/Products/ViewModels/CategorySchemaViewModel.cs
+++ be/src/Project/Web/Seller/Seller.Web/Areas/Products/ViewModels/CategorySchemaViewModel.cs
@@ -4,7 +4,7 @@ namespace Seller.Web.Areas.Products.ViewModels
 {
     public class CategorySchemaViewModel
     {
-        public Guid Id { get; set; }
+        public Guid? Id { get; set; }
         public string Schema { get; set; }
         public string UiSchema { get; set; }
         public string Language { get; set; }
