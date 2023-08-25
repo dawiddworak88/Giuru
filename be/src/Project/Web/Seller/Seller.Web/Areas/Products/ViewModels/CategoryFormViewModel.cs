f1200485ba228aa909ceed5fd83c2b2b15265c4b
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-08-25 15:08:18 +0200

    Deleted unnecessary space on end line

--- be/src/Project/Web/Seller/Seller.Web/Areas/Products/ViewModels/CategoryFormViewModel.cs
+++ be/src/Project/Web/Seller/Seller.Web/Areas/Products/ViewModels/CategoryFormViewModel.cs
@@ -13,7 +13,7 @@ namespace Seller.Web.Areas.Products.ViewModels
         public Guid? ParentCategoryId { get; set; }
         public IEnumerable<FileViewModel> Files { get; set; }
         public IEnumerable<CategorySchemaViewModel> Schemas { get; set; }
-        public string Language { get; set; }        
+        public string Language { get; set; }
         public CategoryBaseFormViewModel CategoryBase { get; set; }
     }
 }
