fd2d5ebe7fbf8b7fd7cbb25281a9f5141af1596f
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-08-25 12:42:11 +0200

    Added SaveCategorySchenaRequestModel

---
+++ be/src/Project/Services/Catalog/Catalog.Api/v1/Categories/RequestModels/SaveCategorySchemaRequestModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Data.SqlTypes;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Catalog.Api.v1.Categories.RequestModels
+{
+    public class SaveCategorySchemaRequestModel
+    {
+        public Guid? CategoryId { get; set; }
+        public string Schema { get; set; }
+        public string UiSchema { get; set; }
+    }
+}
