fda72a9fb73fb71835a50cff7bf47a8b613dc13f
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-08-25 12:43:42 +0200

    Added CategorySchemaRequestModel

---
+++ be/src/Project/Web/Seller/Seller.Web/Areas/Products/ApiRequestModels/CategorySchemaRequestModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Seller.Web.Areas.Products.ApiRequestModels
+{
+    public class CategorySchemaRequestModel
+    {
+        public Guid? Id { get; set; }
+        public string Schema { get; set; }
+        public string UiSchema { get; set; }
+        public string Language { get; set; }
+    }
+}
