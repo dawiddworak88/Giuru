336a0153318e530af6c7d49f34ca336625f05577
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-09-04 15:07:03 +0200

    Added File class

---
+++ be/src/Project/Web/Seller/Seller.Web/Areas/Products/DomainModels/File.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Seller.Web.Areas.Products.DomainModels
+{
+    public class File
+    {
+        public Guid? Id { get; set; }
+        public string Url { get; set; }
+    }
+}
