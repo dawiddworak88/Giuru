a6542456eacac9c8352f6d8756ec42ee6253d4af
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-09-06 15:18:37 +0200

    Added SaveCategoryOrderRequestModel

---
+++ be/src/Project/Web/Seller/Seller.Web/Areas/Products/ApiRequestModels/SaveCategoryOrderRequestModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Seller.Web.Areas.Products.ApiRequestModels
+{
+    public class SaveCategoryOrderRequestModel
+    {
+        public Guid? Id { get; set; }
+        public int Order { get; set; }
+    }
+}
