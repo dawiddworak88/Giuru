e54d197d09c92c4781308cd3b5f4eb7eeb994a68
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-09-27 07:39:59 +0200

    Added property FabricWebUrl to AppSettings

--- be/src/Project/Web/Buyer/Buyer.Web/Shared/Configurations/AppSettings.cs
+++ be/src/Project/Web/Buyer/Buyer.Web/Shared/Configurations/AppSettings.cs
@@ -21,5 +21,6 @@ namespace Buyer.Web.Shared.Configurations
         public string GoogleTagManagerIdentifier { get; set; }
         public string MakeComplaintUrl { get; set; }
         public string DefaultCulture { get; set; }
+        public string FabricsWebUrl { get; set; }
     }
 }
