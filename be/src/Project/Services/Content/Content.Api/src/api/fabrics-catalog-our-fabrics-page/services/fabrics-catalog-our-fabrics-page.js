a8e6133759104cf44123547cbd33b6916973d2e7
Sebastian Kasperek <93117743+SebaKas21@users.noreply.github.com>
2023-10-08 20:59:43 +0200

    Removed fabrics pages (#290)

--- be/src/Project/Services/Content/Content.Api/src/api/fabrics-catalog-our-fabrics-page/services/fabrics-catalog-our-fabrics-page.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * fabrics-catalog-our-fabrics-page service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::fabrics-catalog-our-fabrics-page.fabrics-catalog-our-fabrics-page');
--- be/src/Project/Services/Content/Content.Api/src/api/fabrics-catalog-privacy-policy-page/services/fabrics-catalog-privacy-policy-page.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * fabrics-catalog-privacy-policy-page service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::fabrics-catalog-privacy-policy-page.fabrics-catalog-privacy-policy-page');
