b75a345092c7c21c78b55d6ea40406809036cbf0
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-10-12 15:37:30 +0200

    Moved response models to proper folders and modified SeoGeraphQlResponseModel

---
+++ be/src/Project/Web/Buyer/Buyer.Web/Shared/GraphQlResponseModels/MainNavigationLinks/MainNavigationLinksGraphQlResponseModel.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Buyer.Web.Shared.GraphQlResponseModels.MainNavigationLinks
+{
+    public class MainNavigationLinksGraphQlResponseModel
+    {
+        [JsonProperty("page")]
+        public Page Page { get; set; }
+    }
+
+    public class Page
+    {
+        [JsonProperty("data")]
+        public Data Data { get; set; }
+    }
+
+    public class Data
+    {
+        [JsonProperty("attributes")]
+        public Attributes Attributes { get; set; }
+    }
+
+    public class Attributes
+    {
+        [JsonProperty("mainNavigationLinks")]
+        public MainNavigationLinks MainNavigationLinks { get; set; }
+    }
+
+    public class MainNavigationLinks
+    {
+        [JsonProperty("links")]
+        public List<Link> Links { get; set; }
+    }
+
+    public class Link
+    {
+        [JsonProperty("href")]
+        public string Href { get; set; }
+
+        [JsonProperty("label")]
+        public string Label { get; set; }
+
+        [JsonProperty("target")]
+        public string Target { get; set; }
+
+        [JsonProperty("isExternal")]
+        public bool IsExternal { get; set; }
+    }
+}
