d200e277c86233a335e206ecc513c85dcc8878f6
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-10-27 11:20:01 +0200

    Added migration to database

---
+++ be/src/Project/Services/Ordering/Ordering.Api/Infrastructure/Migrations/20231027091649_RemovedDeliveryDates.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ordering.Api.Infrastructure.Migrations
+{
+    public partial class RemovedDeliveryDates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExpectedDeliveryDate",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "ExpectedDeliveryFrom",
+                table: "OrderItems");
+
+            migrationBuilder.DropColumn(
+                name: "ExpectedDeliveryTo",
+                table: "OrderItems");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ExpectedDeliveryDate",
+                table: "Orders",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ExpectedDeliveryFrom",
+                table: "OrderItems",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ExpectedDeliveryTo",
+                table: "OrderItems",
+                type: "datetime2",
+                nullable: true);
+        }
+    }
+}
