88e0af3341afa55a631ee3dc1059e43462f4a7e9
Sebastian Kasperek <sebastian.kasperek@eltap.com>
2023-10-31 13:54:51 +0100

    Added views for catalo

--- be/src/Project/Web/Seller/Seller.Web/Areas/Clients/ModelBuilders/ClientDeliveryAddressesPageModelBuilder.cs
+++ be/src/Project/Web/Seller/Seller.Web/Areas/Clients/ModelBuilders/ClientDeliveryAddressesPageModelBuilder.cs
@@ -1,20 +1,46 @@
 ﻿using Foundation.Extensions.ModelBuilders;
 using Foundation.PageContent.ComponentModels;
+using Foundation.PageContent.Components.Footers.ViewModels;
+using Foundation.PageContent.MenuTiles.ViewModels;
+using Seller.Web.Areas.Clients.DomainModels;
 using Seller.Web.Areas.Clients.ViewModels;
+using Seller.Web.Shared.ViewModels;
+using System.Globalization;
 using System.Threading.Tasks;
 
 namespace Seller.Web.Areas.Clients.ModelBuilders
 {
     public class ClientDeliveryAddressesPageModelBuilder : IAsyncComponentModelBuilder<ComponentModelBase, ClientDeliveryAddressesPageViewModel>
     {
-        public ClientDeliveryAddressesPageModelBuilder()
-        {
+        private readonly IAsyncComponentModelBuilder<ComponentModelBase, SellerHeaderViewModel> _headerModelBuilder;
+        private readonly IModelBuilder<MenuTilesViewModel> _menuTilesModelBuilder;
+        private readonly IAsyncComponentModelBuilder<ComponentModelBase, CatalogViewModel<ClientDeliveryAddress>> _clientDeliveryAddressesCatalogModelBuilder;
+        private readonly IModelBuilder<FooterViewModel> _footerModelBuilder;
 
+        public ClientDeliveryAddressesPageModelBuilder(
+            IAsyncComponentModelBuilder<ComponentModelBase, SellerHeaderViewModel> headerModelBuilder,
+            IModelBuilder<MenuTilesViewModel> menuTilesModelBuilder,
+            IAsyncComponentModelBuilder<ComponentModelBase, CatalogViewModel<ClientDeliveryAddress>> clientDeliveryAddressesCatalogModelBuilder,
+            IModelBuilder<FooterViewModel> footerModelBuilder)
+        {
+            _headerModelBuilder = headerModelBuilder;
+            _menuTilesModelBuilder = menuTilesModelBuilder;
+            _clientDeliveryAddressesCatalogModelBuilder = clientDeliveryAddressesCatalogModelBuilder;
+            _footerModelBuilder = footerModelBuilder;
         }
 
-        public Task<ClientDeliveryAddressesPageViewModel> BuildModelAsync(ComponentModelBase componentModel)
+        public async Task<ClientDeliveryAddressesPageViewModel> BuildModelAsync(ComponentModelBase componentModel)
         {
-            throw new System.NotImplementedException();
+            var viewModel = new ClientDeliveryAddressesPageViewModel
+            {
+                Locale = CultureInfo.CurrentUICulture.Name,
+                Header = await _headerModelBuilder.BuildModelAsync(componentModel),
+                MenuTiles = _menuTilesModelBuilder.BuildModel(),
+                Catalog = await _clientDeliveryAddressesCatalogModelBuilder.BuildModelAsync(componentModel),
+                Footer = _footerModelBuilder.BuildModel()
+            };
+
+            return viewModel;
         }
     }
 }
