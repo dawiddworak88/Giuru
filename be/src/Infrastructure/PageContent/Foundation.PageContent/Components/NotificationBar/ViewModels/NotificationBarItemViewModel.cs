44886306acfd04939833f96e34315e55d2e23f81
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-11-14 15:28:48 +0100

    Added NotificationBar Domain, Response and View Models

---
+++ be/src/Infrastructure/PageContent/Foundation.PageContent/Components/NotificationBar/ViewModels/NotificationBarItemViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Foundation.PageContent.Components.Links.ViewModels;
+
+namespace Foundation.PageContent.Components.NotificationBar.ViewModels
+{
+    public class NotificationBarItemViewModel
+    {
+        public string Icon { get; set; }
+        public LinkViewModel Link { get; set; }
+    }
+}
---
+++ be/src/Project/Web/Buyer/Buyer.Web/Shared/DomainModels/GraphQl/NotificationBars/NotificationBarItem.cs
@@ -0,0 +1,10 @@
+﻿using Buyer.Web.Shared.DomainModels.GraphQl.Shared;
+
+namespace Buyer.Web.Shared.DomainModels.GraphQl.NotificationBars
+{
+    public class NotificationBarItem
+    {
+        public string Icon { get; set; }
+        public Link Link { get; set; }
+    }
+}
