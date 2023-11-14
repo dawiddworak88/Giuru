fe8841a9b9d54d973f7fcf00f3f65dffea9e465a
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-11-14 15:31:58 +0100

    Added two methods for MainNavigationLinks and NotificationBar to GraphQlRepository

--- be/src/Project/Web/Buyer/Buyer.Web/Shared/Repositories/GraphQl/IGraphQlRepository.cs
+++ be/src/Project/Web/Buyer/Buyer.Web/Shared/Repositories/GraphQl/IGraphQlRepository.cs
@@ -1,4 +1,7 @@
-﻿using Buyer.Web.Shared.DomainModels.GraphQl;
+﻿using Buyer.Web.Shared.DomainModels.GraphQl.FooterLinks;
+using Buyer.Web.Shared.DomainModels.GraphQl.MainNavigationLinks;
+using Buyer.Web.Shared.DomainModels.GraphQl.NotificationBars;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Buyer.Web.Shared.Repositories.GraphQl
@@ -6,5 +9,7 @@ namespace Buyer.Web.Shared.Repositories.GraphQl
     public interface IGraphQlRepository
     {
         Task<Footer> GetFooterAsync(string contentPageKey, string language, string fallbackLanguage);
+        Task<IEnumerable<NotificationBarItem>> GetNotificationBar(string contentPageKey, string language, string fallbackLanguage);
+        Task<IEnumerable<MainNavigationLink>> GetMainNavigationLinksAsync(string contentPageKey, string language, string fallbackLanguage);
     }
 }
