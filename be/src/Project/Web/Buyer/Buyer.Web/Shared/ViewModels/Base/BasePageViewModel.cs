51b7144d2e8bcaa9cfa08f23a8ba94a0fe991f0d
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-11-17 12:02:29 +0100

    Added MainNavigationViewModel to BasePageViewModel

--- be/src/Project/Web/Buyer/Buyer.Web/Shared/ViewModels/Base/BasePageViewModel.cs
+++ be/src/Project/Web/Buyer/Buyer.Web/Shared/ViewModels/Base/BasePageViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Buyer.Web.Shared.ViewModels.Headers;
 using Foundation.PageContent.Components.Footers.ViewModels;
 using Buyer.Web.Shared.ViewModels.NotificationBar;
+using Foundation.PageContent.Components.MainNavigations.ViewModels;
 
 namespace Buyer.Web.Shared.ViewModels.Base
 {
@@ -9,6 +10,7 @@ namespace Buyer.Web.Shared.ViewModels.Base
         public string Locale { get; set; }
         public NotificationBarViewModel NotificationBar { get; set; }
         public BuyerHeaderViewModel Header { get; set; }
+        public MainNavigationViewModel MainNavigation { get; set; }
         public FooterViewModel Footer { get; set; }
     }
 }
