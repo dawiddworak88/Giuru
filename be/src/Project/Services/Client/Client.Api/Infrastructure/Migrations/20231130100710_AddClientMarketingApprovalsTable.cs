6911251b25ade74fbf80df1140b9d8584aa2ec72
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-11-30 14:07:44 +0100

    Created migration

---
+++ be/src/Project/Services/Client/Client.Api/Infrastructure/Migrations/20231130100710_AddClientMarketingApprovalsTable.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Client.Api.Infrastructure.Migrations
+{
+    public partial class AddClientMarketingApprovalsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsEmailMarketingApproval",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "IsSmsMarketingApproval",
+                table: "Clients");
+
+            migrationBuilder.CreateTable(
+                name: "ClientMarketingApprovals",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsApproved = table.Column<bool>(type: "bit", nullable: false),
+                    ClientId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    RowVersion = table.Column<byte[]>(type: "rowversion", rowVersion: true, nullable: true),
+                    LastModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClientMarketingApprovals", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ClientMarketingApprovals");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsEmailMarketingApproval",
+                table: "Clients",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSmsMarketingApproval",
+                table: "Clients",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
