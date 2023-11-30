1adb059fdabcea4e0776398e5930c5ea5afbf098
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-11-30 14:06:32 +0100

    Added new table for marketing approval

---
+++ be/src/Project/Services/Client/Client.Api/Infrastructure/Clients/Entities/ClientMarketingApproval.cs
@@ -0,0 +1,14 @@
+﻿using Foundation.GenericRepository.Entities;
+using System;
+
+namespace Client.Api.Infrastructure.Clients.Entities
+{
+    public class ClientMarketingApproval : Entity
+    {
+        public string Name { get; set; }
+
+        public bool IsApproved { get; set; }
+
+        public Guid ClientId { get; set; }
+    }
+}
