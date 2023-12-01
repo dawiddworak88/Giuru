493832f1a80e39c58db661c31206b91b972b511b
Sebastian Kasperek <sebastian.kasperek@eltap.com>
2023-12-01 13:31:33 +0100

    Added controller

---
+++ be/src/Project/Services/Client/Client.Api/v1/Controllers/ClientFieldsController.cs
@@ -0,0 +1,12 @@
+﻿using Foundation.ApiExtensions.Controllers;
+
+namespace Client.Api.v1.Controllers
+{
+    public class ClientFieldsController : BaseApiController
+    {
+        public ClientFieldsController()
+        {
+
+        }
+    }
+}
