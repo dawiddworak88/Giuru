2f456d654cc4ace007f8b462a1c297fca52bd371
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-12-04 14:52:59 +0100

    Removed unnecessary properties

--- be/src/Project/Web/Seller/Seller.Web/Areas/Clients/ViewModels/ClientMarketingApproval.cs
+++ be/src/Project/Web/Seller/Seller.Web/Areas/Clients/ViewModels/ClientMarketingApproval.cs
@@ -4,11 +4,7 @@ namespace Seller.Web.Areas.Clients.ViewModels
 {
     public class ClientMarketingApproval
     {
-        public Guid? Id { get; set; }
         public string Name { get; set; }
-        public bool IsApproved { get; set; }
-        public Guid ClientId { get; set; }
-        public DateTime? LastModifiedDate { get; set; }
         public DateTime? CreatedDate { get; set; }
     }
 }
