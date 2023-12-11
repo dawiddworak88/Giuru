33782735c3b45ab2f93581a380eb231ce5779641
Sebastian Kasperek <sebastian.kasperek@eltap.com>
2023-12-11 10:06:07 +0100

    Added client field domain model

---
+++ be/src/Project/Web/Seller/Seller.Web/Areas/Clients/DomainModels/ClientField.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Seller.Web.Areas.Clients.DomainModels
+{
+    public class ClientField
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public bool IsRequired { get; set; }
+        public DateTime LastModifiedDate { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
