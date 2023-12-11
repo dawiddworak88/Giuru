e7aedd8bfb0d8b1224f0041cb2d9ecec1c6c477c
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-12-11 08:38:22 +0100

    Added request model for ClientRepository

---
+++ be/src/Project/Services/Identity/Identity.Api/Areas/Accounts/ApiRequestModels/SaveClientRequestModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Identity.Api.Areas.Accounts.ApiRequestModels
+{
+    public class SaveClientRequestModel
+    {
+        public Guid? Id { get; set; }
+        public Guid? OrganisationId { get; set; }
+        public IEnumerable<string> MarketingApprovals { get; set; }
+    }
+}
