b92f24c172821e20cee2d2658aa8d21f2091f0be
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-12-12 09:09:48 +0100

    Added properties for to SaveClientRequestModel

--- be/src/Project/Services/Identity/Identity.Api/Areas/Accounts/ApiRequestModels/SaveClientRequestModel.cs
+++ be/src/Project/Services/Identity/Identity.Api/Areas/Accounts/ApiRequestModels/SaveClientRequestModel.cs
@@ -6,6 +6,17 @@ namespace Identity.Api.Areas.Accounts.ApiRequestModels
     public class SaveClientRequestModel
     {
         public Guid? Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string CommunicationLanguage { get; set; }
+        public string PhoneNumber { get; set; }
+        public Guid? CountryId { get; set; }
+        public Guid? OrganisationId { get; set; }
+        public IEnumerable<Guid> ClientGroupIds { get; set; }
+        public IEnumerable<Guid> ClientManagerIds { get; set; }
+        public Guid? DefaultDeliveryAddressId { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+        public DateTime? CreatedDate { get; set; }
         public IEnumerable<string> MarketingApprovals { get; set; }
     }
 }
