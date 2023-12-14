79451345f71e4b8acedf1cea34e9a2c39eeded96
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-12-14 09:08:28 +0100

    Added controller for currencies

---
+++ be/src/Project/Services/Global/Global.Api/v1/Controllers/CurrenciesController.cs
@@ -0,0 +1,232 @@
+﻿using Foundation.GenericRepository.Paginations;
+using Global.Api.Services.Currencies;
+using Global.Api.v1.ResponseModels;
+using Microsoft.AspNetCore.Authorization;
+using Global.Api.ServicesModels.Currencies;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using Global.Api.v1.RequestModels;
+using System;
+using System.Linq;
+using Foundation.Account.Definitions;
+using System.Security.Claims;
+using Foundation.Extensions.Helpers;
+using Foundation.Extensions.ExtensionMethods;
+using Global.Api.validators.Currencies;
+using Foundation.Extensions.Exceptions;
+using Foundation.Extensions.Definitions;
+using System.Globalization;
+
+namespace Global.Api.v1.Controllers
+{
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    //[Authorize]
+    [ApiController]
+    public class CurrenciesController : ControllerBase
+    {
+        private readonly ICurrenciesService _currenciesService;
+
+        public CurrenciesController(
+            ICurrenciesService currenciesService) 
+        { 
+            _currenciesService = currenciesService;
+        }
+
+        /// <summary>
+        /// Gets list of currencies
+        /// </summary>
+        /// <param name="searchTerm">The search term.</param>
+        /// <param name="pageIndex">The page index.</param>
+        /// <param name="itemsPerPage">The items per page.</param>
+        /// <param name="orderBy">The optional order by.</param>
+        /// <returns>The list of currencies.</returns>
+        [HttpGet, MapToApiVersion("1.0")]
+        [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(PagedResults<IEnumerable<CurrencyResponseModel>>))]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        [ProducesResponseType((int)HttpStatusCode.UnprocessableEntity)]
+        public async Task<IActionResult> Get(string searchTerm, int? pageIndex, int? itemsPerPage, string orderBy)
+        {
+            var sellerClaim = User.Claims.FirstOrDefault(x => x.Type == AccountConstants.Claims.OrganisationIdClaim);
+
+            var serviceModel = new GetCurrenciesServiceModel
+            {
+                SearchTerm = searchTerm,
+                PageIndex = pageIndex,
+                ItemsPerPage = itemsPerPage,
+                OrderBy = orderBy,
+                Language = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Email)?.Value,
+                OrganisationId = GuidHelper.ParseNullable(sellerClaim?.Value)
+            };
+
+            var currencies = _currenciesService.Get(serviceModel);
+
+            if (currencies is not null)
+            {
+                var response = new PagedResults<IEnumerable<CurrencyResponseModel>>(currencies.Total, currencies.PageSize)
+                {
+                    Data = currencies.Data.OrEmptyIfNull().Select(x => new CurrencyResponseModel
+                    {
+                        Id = x.Id,
+                        CurrencyCode = x.CurrencyCode,
+                        Symbol = x.Symbol,
+                        Name = x.Name,
+                        LastModifiedDate = x.LastModifiedDate,
+                        CreatedDate = x.CreatedDate
+                    })
+                };
+
+                return StatusCode((int)HttpStatusCode.OK, response);
+            }
+
+            return StatusCode((int)HttpStatusCode.UnprocessableEntity);
+        }
+
+        /// <summary>
+        /// Creates or updates (if currency id is set).
+        /// </summary>
+        /// <param name="request">the model.</param>
+        /// <returns>OK.</returns>
+        [HttpPost, MapToApiVersion("1.0")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.UnprocessableEntity)]
+        public async Task<IActionResult> Save(CurrencyRequestModel request)
+        {
+            var sellerClaim = User.Claims.FirstOrDefault(x => x.Type == AccountConstants.Claims.OrganisationIdClaim);
+
+            if (request.Id.HasValue)
+            {
+                var serviceModel = new UpdateCurrencyServiceModel
+                {
+                    Id = request.Id,
+                    CurrencyCode = request.CurrencyCode,
+                    Symbol = request.Symbol,
+                    Name = request.Name,
+                    Language = CultureInfo.CurrentCulture.Name,
+                    Username = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Email)?.Value,
+                    OrganisationId = GuidHelper.ParseNullable(sellerClaim?.Value)
+                };
+
+                var validator = new UpdateCurrencyModelValidator();
+                var validationResult = await validator.ValidateAsync(serviceModel);
+
+                if (validationResult.IsValid)
+                {
+                    await _currenciesService.UpdateAsync(serviceModel);
+
+                    return StatusCode((int)HttpStatusCode.OK);
+                }
+
+                throw new CustomException(string.Join(ErrorConstants.ErrorMessagesSeparator, validationResult.Errors.Select(x => x.ErrorMessage)), (int)HttpStatusCode.UnprocessableEntity);
+            }
+            else
+            {
+                var serviceModel = new CreateCurrencyServiceModel
+                {
+                    CurrencyCode = request.CurrencyCode,
+                    Symbol = request.Symbol,
+                    Name= request.Name,
+                    Language = CultureInfo.CurrentCulture.Name,
+                    Username = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Email)?.Value,
+                    OrganisationId = GuidHelper.ParseNullable(sellerClaim?.Value)
+                };
+
+                var validator = new CreateCurrencyModelValidator();
+                var validationResult = await validator.ValidateAsync(serviceModel);
+
+                if (validationResult.IsValid)
+                {
+                    await _currenciesService.CreateAsync(serviceModel);
+
+                    return StatusCode((int)HttpStatusCode.OK);
+                }
+
+                throw new CustomException(string.Join(ErrorConstants.ErrorMessagesSeparator, validationResult.Errors.Select(x => x.ErrorMessage)), (int)HttpStatusCode.UnprocessableEntity);
+            }
+        }
+
+        /// <summary>
+        /// Deletes Currency
+        /// </summary>
+        /// <param name="id">The id.</param>
+        /// <returns>Ok.</returns>
+        [HttpDelete, MapToApiVersion("1.0")]
+        [Route("{id}")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        [ProducesResponseType((int)HttpStatusCode.UnprocessableEntity)]
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            var sellerClaim = User.Claims.FirstOrDefault(x => x.Type == AccountConstants.Claims.OrganisationIdClaim);
+
+            var serviceModel = new DeleteCurrencyServiceModel
+            {
+                Id = id,
+                Username = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Email)?.Value,
+                OrganisationId = GuidHelper.ParseNullable(sellerClaim?.Value)
+            };
+
+            var validator = new DeleteCurrencyModelValidator();
+            var validationResult = await validator.ValidateAsync(serviceModel);
+
+            if (validationResult.IsValid)
+            {
+                await _currenciesService.DeleteAsync(serviceModel);
+
+                return StatusCode((int)HttpStatusCode.OK);
+            }
+
+            throw new CustomException(string.Join(ErrorConstants.ErrorMessagesSeparator, validationResult.Errors.Select(x => x.ErrorMessage)), (int)HttpStatusCode.UnprocessableEntity);
+        }
+
+        /// <summary>
+        /// Get currency by id
+        /// </summary>
+        /// <param name="id">The id.</param>
+        /// <returns>The currency.</returns>
+        [HttpGet, MapToApiVersion("1.0")]
+        [Route("{id}")]
+        [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(CurrencyResponseModel))]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        [ProducesResponseType((int)HttpStatusCode.UnprocessableEntity)]
+        public async Task<IActionResult> Get(Guid? id)
+        {
+            var sellerClaim = User.Claims.FirstOrDefault(x => x.Type == AccountConstants.Claims.OrganisationIdClaim);
+
+            var serviceModel = new GetCurrencyServiceModel
+            { 
+                Id = id,
+                Language = CultureInfo.CurrentCulture.Name,
+                Username = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Email)?.Value,
+                OrganisationId = GuidHelper.ParseNullable(sellerClaim?.Value)
+            };
+
+            var validator = new GetCurrencyModelValidator();
+            var validationResult = await validator.ValidateAsync(serviceModel);
+
+            if (validationResult.IsValid)
+            {
+                var currency = await _currenciesService.GetAsync(serviceModel);
+
+                if (currency is not null)
+                {
+                    var response = new CurrencyResponseModel
+                    {
+                        Id = currency.Id,
+                        CurrencyCode = currency.CurrencyCode,
+                        Symbol = currency.Symbol,
+                        Name = currency.Name,
+                        LastModifiedDate = currency.LastModifiedDate,
+                        CreatedDate = currency.CreatedDate
+                    };
+
+                    return StatusCode((int)HttpStatusCode.OK, response);
+                }
+            }
+
+            throw new CustomException(string.Join(ErrorConstants.ErrorMessagesSeparator, validationResult.Errors.Select(x => x.ErrorMessage)), (int)HttpStatusCode.UnprocessableEntity); ;
+        }
+    }
+}
