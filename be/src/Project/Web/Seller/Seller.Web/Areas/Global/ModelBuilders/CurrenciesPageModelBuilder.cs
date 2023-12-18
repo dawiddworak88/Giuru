c06ff91eeae23bc10b8bd028077be5abdffc3668
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-12-18 15:18:21 +0100

    Added ModelBuilders for CurrenciesPage and CurrencyPage

---
+++ be/src/Project/Web/Seller/Seller.Web/Areas/Global/ModelBuilders/CurrenciesPageModelBuilder.cs
@@ -0,0 +1,50 @@
+﻿using Foundation.Extensions.ModelBuilders;
+using Foundation.PageContent.ComponentModels;
+using Foundation.PageContent.Components.Footers.ViewModels;
+using Foundation.PageContent.MenuTiles.ViewModels;
+using Seller.Web.Areas.Global.DomainModels;
+using Seller.Web.Areas.Global.ViewModels;
+using Seller.Web.Shared.ViewModels;
+using System;
+using System.Globalization;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Seller.Web.Areas.Global.ModelBuilders
+{
+    public class CurrenciesPageModelBuilder : IAsyncComponentModelBuilder<ComponentModelBase, CurrenciesPageViewModel>
+    {
+        private readonly IAsyncComponentModelBuilder<ComponentModelBase, SellerHeaderViewModel> _headerModelBuilder;
+        private readonly IModelBuilder<MenuTilesViewModel> _menuTitlesModelBuilder;
+        private readonly IAsyncComponentModelBuilder<ComponentModelBase, CatalogViewModel<Currency>> _currencyCatalogModelBuilder;
+        private readonly IModelBuilder<FooterViewModel> _footerModelBuilder;
+
+        public CurrenciesPageModelBuilder(
+            IAsyncComponentModelBuilder<ComponentModelBase, SellerHeaderViewModel> headerModelBuilder,
+            IModelBuilder<MenuTilesViewModel> menuTitlesModelBuilder,
+            IAsyncComponentModelBuilder<ComponentModelBase, CatalogViewModel<Currency>> currencyCatalogModelBuilder,
+            IModelBuilder<FooterViewModel> footerModelBuilder)
+        {
+            _headerModelBuilder = headerModelBuilder;
+            _menuTitlesModelBuilder = menuTitlesModelBuilder;
+            _currencyCatalogModelBuilder = currencyCatalogModelBuilder;
+            _footerModelBuilder = footerModelBuilder;
+        }
+
+        public async Task<CurrenciesPageViewModel> BuildModelAsync(ComponentModelBase componentModel)
+        {
+            var viewModel = new CurrenciesPageViewModel
+            {
+                Locale = CultureInfo.CurrentCulture.Name,
+                Header = await _headerModelBuilder.BuildModelAsync(componentModel),
+                MenuTiles = _menuTitlesModelBuilder.BuildModel(),
+                Catalog = await _currencyCatalogModelBuilder.BuildModelAsync(componentModel),
+                Footer = _footerModelBuilder.BuildModel()
+            };
+
+            Console.WriteLine("viewModel " + JsonSerializer.Serialize(viewModel.Catalog));
+
+            return viewModel;
+        }
+    }
+}
---
+++ be/src/Project/Web/Seller/Seller.Web/Areas/Global/ModelBuilders/CurrencyFormModelBuilder.cs
@@ -0,0 +1,64 @@
+﻿using Foundation.Extensions.ModelBuilders;
+using Foundation.Localization;
+using Foundation.PageContent.ComponentModels;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Localization;
+using OpenTelemetry;
+using Seller.Web.Areas.Global.Repositories;
+using Seller.Web.Areas.Global.ViewModels;
+using System.Globalization;
+using System.Threading.Tasks;
+
+namespace Seller.Web.Areas.Global.ModelBuilders
+{
+    public class CurrencyFormModelBuilder : IAsyncComponentModelBuilder<ComponentModelBase, CurrencyFormViewModel>
+    {
+        private readonly IStringLocalizer<GlobalResources> _globalLocalizer;
+        private readonly LinkGenerator _linkGenerator;
+        private readonly ICurrenciesRepository _currenciesRepository;
+
+        public CurrencyFormModelBuilder(
+            IStringLocalizer<GlobalResources> globalLocalizer,
+            LinkGenerator linkGenerator,
+            ICurrenciesRepository currenciesRepository) 
+        { 
+            _globalLocalizer = globalLocalizer;
+            _linkGenerator = linkGenerator;
+            _currenciesRepository = currenciesRepository;
+        }
+
+        public async Task<CurrencyFormViewModel> BuildModelAsync(ComponentModelBase componentModel)
+        {
+            var viewModel = new CurrencyFormViewModel
+            {
+                Title = _globalLocalizer.GetString("EditCurrency"),
+                IdLabel = _globalLocalizer.GetString("Id"),
+                NameLabel = _globalLocalizer.GetString("Name"),
+                SaveText = _globalLocalizer.GetString("SaveText"),
+                FieldRequiredErrorMessage = _globalLocalizer.GetString("FieldRequiredErrorMessage"),
+                GeneralErrorMessage = _globalLocalizer.GetString("AnErrorOccurred"),
+                NavigateToCurrencies = _globalLocalizer.GetString("NavigateToCurrenciesText"),
+                SaveUrl = _linkGenerator.GetPathByAction("Index", "CurrenciesApi", new { Area = "Global", culture = CultureInfo.CurrentUICulture.Name }),
+                CurrenciesUrl = _linkGenerator.GetPathByAction("Index", "Currencies", new { Area = "Global", culture = CultureInfo.CurrentUICulture.Name }),
+                CurrencyCodeLabel = _globalLocalizer.GetString("CurrencyCodeLabel"),
+                SymbolLabel = _globalLocalizer.GetString("SymbolLabel"),
+
+            };
+
+            if (componentModel.Id.HasValue)
+            {
+                var currency = await _currenciesRepository.GetAsync(componentModel.Token, componentModel.Language, componentModel.Id);
+
+                if (currency is not null)
+                { 
+                    viewModel.Id = componentModel.Id;
+                    viewModel.CurrencyCode = currency.CurrencyCode;
+                    viewModel.Symbol = currency.Symbol;
+                    viewModel.Name = currency.Name;
+                }
+            }
+
+            return viewModel;
+        }
+    }
+}
