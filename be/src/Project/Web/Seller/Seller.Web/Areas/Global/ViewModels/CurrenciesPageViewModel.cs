1924203dd87be5b124be52627209916732020168
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-12-18 15:19:04 +0100

    Added view models for CurrencyPage and CurrenciesPage

---
+++ be/src/Project/Web/Seller/Seller.Web/Areas/Global/ViewModels/CurrenciesPageViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Seller.Web.Areas.Global.DomainModels;
+using Seller.Web.Shared.ViewModels;
+
+namespace Seller.Web.Areas.Global.ViewModels
+{
+    public class CurrenciesPageViewModel : BasePageViewModel
+    {
+        public CatalogViewModel<Currency> Catalog { get; set; }
+    }
+}
