bc5074305527eff2e09a8c3f1b88c63ca114e6f8
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-12-19 11:13:43 +0100

    Added CurrencyPage

---
+++ fe/projects/Seller.Portal/src/areas/Global/pages/CurrencyPage/CurrencyPage.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { ToastContainer } from "react-toastify";
+import { ThemeProvider } from "@mui/material/styles";
+import GlobalHelper from "../../../../shared/helpers/globals/GlobalHelper";
+import Header from "../../../../shared/components/Header/Header";
+import Store from "../../../../shared/stores/Store";
+import Footer from "../../../../shared/components/Footer/Footer";
+import MenuTiles from "../../../../shared/components/MenuTiles/MenuTiles";
+import CurrencyForm from "../../components/CurrencyForm/CurrencyForm";
+
+const CurrencyPage = (props) => {
+    return (
+        <ThemeProvider theme={GlobalHelper.initMuiTheme()}>
+            <ToastContainer />
+            <Store>
+                <Header {...props.header}></Header>
+                <MenuTiles {...props.menuTiles} />
+                <CurrencyForm {...props.countryForm} />
+                <Footer {...props.footer}></Footer>
+            </Store>
+        </ThemeProvider>
+    );
+}
+
+export default CurrencyPage;
