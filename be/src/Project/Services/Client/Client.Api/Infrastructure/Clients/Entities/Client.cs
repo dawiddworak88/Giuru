709265ffa36d9108786e01d43d6875fdc443af5d
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-12-20 12:11:51 +0100

    Added CurrencyId to client entity

--- be/src/Project/Services/Client/Client.Api/Infrastructure/Clients/Entities/Client.cs
+++ be/src/Project/Services/Client/Client.Api/Infrastructure/Clients/Entities/Client.cs
@@ -19,6 +19,8 @@ namespace Client.Api.Infrastructure.Clients.Entities
 
         public Guid? CountryId { get; set; }
 
+        public Guid? CurrencyId { get; set; }
+
         [Required]
         public Guid OrganisationId { get; set; }
 
