d4d8edfee004f23828147c8cfa2e2bd77d47d225
Piotr Zaranek <piotrek.zaranek@gmail.com>
2024-01-05 13:02:08 +0100

    Removed unnecessary MarketingApporvalsConstants

--- be/src/Project/Services/Client/Client.Api/Definitions/MarketingApporvalConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Client.Api.Definitions
-{
-    public static class MarketingApporvalConstants
-    {
-        public static readonly string EmailMarketingApprovalName = "EMAIL_MARKETING_APPROVAL";
-        public static readonly string SmsMarketingApprovalName = "SMS_MARKETING_APPROVAL";
-    }
-}
