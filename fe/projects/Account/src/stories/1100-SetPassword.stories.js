59462990595e0779f67840053670fa723947d96e
Piotr Zaranek <piotrek.zaranek@gmail.com>
2024-01-05 12:49:38 +0100

    Removed ELTAP from marketing approvlas text

--- fe/projects/Account/src/stories/1100-SetPassword.stories.js
+++ fe/projects/Account/src/stories/1100-SetPassword.stories.js
@@ -11,10 +11,10 @@ var setPasswordForm = {
   passwordRequiredErrorMessage: "Enter password",
   passwordFormatErrorMessage: "Password must be at least 8 characters long, consist of at least one capital letter, one small letter, a digit and a special character, e.g. P@ssw0rd",
   enterPasswordText: "Enter password",
-  marketingApprovalHeader: "Marketing form for people cooperating with ELTAP",
-  marketingApprovalText: "If you would like to receive information from us about the current offer, current campaigns, introduced functionalities or changes, as well as other activities conducted by ELTAP, please complete the marketing consents below. Consents may be withdrawn at any time, and the withdrawal of consent does not affect actions taken before the date of its withdrawal.",
-  emailMarketingApprovalLabel: "I consent to receiving information from ELTAP electronically, to an e-mail address that I secure",
-  smsMarketingApprovalLabel: "I consent to telephone contact from ELTAP for marketing purposes"
+  marketingApprovalHeader: "Marketing form",
+  marketingApprovalText: "If you would like to receive information from us about the current offer, current campaigns, introduced functionalities or changes, as well as other activities conducted, please complete the marketing consents below. Consents may be withdrawn at any time, and the withdrawal of consent does not affect actions taken before the date of its withdrawal.",
+  emailMarketingApprovalLabel: "I consent to receiving information electronically, to an e-mail address that I secure",
+  smsMarketingApprovalLabel: "I consent to telephone contact for marketing purposes"
 };
 
 export const SetPasswordPageStory = () => <SetPasswordPage header={header} setPasswordForm={setPasswordForm} footer={footer} />;
