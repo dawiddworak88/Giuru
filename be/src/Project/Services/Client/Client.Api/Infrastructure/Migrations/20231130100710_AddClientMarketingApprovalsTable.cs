9ceb00f601121395c82106a445cb554f08080b9f
Piotr Zaranek <piotrek.zaranek@gmail.com>
2024-01-08 10:40:54 +0100

    Removed commands for not existing fields in Clients table

--- be/src/Project/Services/Client/Client.Api/Infrastructure/Migrations/20231130100710_AddClientMarketingApprovalsTable.cs
+++ be/src/Project/Services/Client/Client.Api/Infrastructure/Migrations/20231130100710_AddClientMarketingApprovalsTable.cs
@@ -9,14 +9,6 @@ namespace Client.Api.Infrastructure.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropColumn(
-                name: "IsEmailMarketingApproval",
-                table: "Clients");
-
-            migrationBuilder.DropColumn(
-                name: "IsSmsMarketingApproval",
-                table: "Clients");
-
             migrationBuilder.CreateTable(
                 name: "ClientMarketingApprovals",
                 columns: table => new
@@ -40,20 +32,6 @@ namespace Client.Api.Infrastructure.Migrations
         {
             migrationBuilder.DropTable(
                 name: "ClientMarketingApprovals");
-
-            migrationBuilder.AddColumn<bool>(
-                name: "IsEmailMarketingApproval",
-                table: "Clients",
-                type: "bit",
-                nullable: false,
-                defaultValue: false);
-
-            migrationBuilder.AddColumn<bool>(
-                name: "IsSmsMarketingApproval",
-                table: "Clients",
-                type: "bit",
-                nullable: false,
-                defaultValue: false);
         }
     }
 }
