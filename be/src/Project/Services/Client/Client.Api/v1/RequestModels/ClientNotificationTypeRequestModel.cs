b22fc6c15272a6aa517322d61f62432d9a0d7ef9
Piotr Zaranek <piotrek.zaranek@gmail.com>
2024-01-24 15:59:52 +0100

    Added request and response model for notifications

---
+++ be/src/Project/Services/Client/Client.Api/v1/RequestModels/ClientNotificationTypeRequestModel.cs
@@ -0,0 +1,10 @@
+﻿using Foundation.ApiExtensions.Models.Request;
+using System;
+
+namespace Client.Api.v1.RequestModels
+{
+    public class ClientNotificationTypeRequestModel : RequestModelBase
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ be/src/Project/Services/Client/Client.Api/v1/ResponseModels/ClientNotificationTypeResponseModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Client.Api.v1.ResponseModels
+{
+    public class ClientNotificationTypeResponseModel
+    {
+        public Guid? Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime LasModifiedDate { get; set; }
+    }
+}
