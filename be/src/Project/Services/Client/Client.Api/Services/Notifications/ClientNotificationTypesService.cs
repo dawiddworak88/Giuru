0f6ac9f0c61d34c0bb1584dc8274172fd66a60d0
Piotr Zaranek <piotrek.zaranek@gmail.com>
2024-01-24 15:57:39 +0100

    Added Service for NotificationTypes

---
+++ be/src/Project/Services/Client/Client.Api/Services/Notifications/ClientNotificationTypesService.cs
@@ -0,0 +1,35 @@
+﻿using Client.Api.ServicesModels.Notification;
+using Foundation.GenericRepository.Paginations;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Client.Api.Services.Notifications
+{
+    public class ClientNotificationTypesService : IClientNotificationTypesService
+    {
+        public async Task<ClientNotificationTypeServiceModel> CreateAsync(CreateClientNotificationTypeServiceModel model)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task Delete(DeleteClientNotificationTypeServiceModel model)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<PagedResults<IEnumerable<ClientNotificationTypeServiceModel>>> GetAsync(GetClientNotificationTypesServiceModel model)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<ClientNotificationTypeServiceModel> GetAsync(GetClientNotificationTypeServiceModel model)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<ClientNotificationTypeServiceModel> UpdateAsync(UpdateClientNotificationTypeServiceModel model)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ be/src/Project/Services/Client/Client.Api/Services/Notifications/IClientNotificationTypesService.cs
@@ -0,0 +1,17 @@
+﻿using Client.Api.ServicesModels.Notification;
+using Foundation.GenericRepository.Paginations;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Client.Api.Services.Notifications
+{
+    public interface IClientNotificationTypesService
+    {
+        Task<PagedResults<IEnumerable<ClientNotificationTypeServiceModel>>> GetAsync(GetClientNotificationTypesServiceModel model);
+        Task<ClientNotificationTypeServiceModel> GetAsync(GetClientNotificationTypeServiceModel model);
+        Task Delete(DeleteClientNotificationTypeServiceModel model);
+        Task<ClientNotificationTypeServiceModel> UpdateAsync(UpdateClientNotificationTypeServiceModel model);
+        Task<ClientNotificationTypeServiceModel> CreateAsync(CreateClientNotificationTypeServiceModel model);
+    }
+}
