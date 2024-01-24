f2e358390f1946279090bf02a8dc732c8d36135f
Sebastian Kasperek <93117743+SebaKas21@users.noreply.github.com>
2024-01-24 09:27:05 +0100

    Updated node to new LTS (#325)
    * Updated patch version
    
    * Updated minor version Seller
    
    * Updated major version and changed version for react-draft-wysiwyg package
    
    * Changed node version
    
    * Updated major packages in buyer
    
    * Changed node version
    
    * Updated patch packages
    
    * Changed node version
    
    * Updated minor packages
    
    * Updated major packages
    
    * Chnaged node version
    
    * Updated major version Client.Api
    
    * Changed middleware config
    
    * Added missing required packages and changed node version
    
    * Changed node version
    
    * Added storybook/core and storybook/csf-toolt
    
    * Updated patch for packages
    
    * Fixed storybook configuration for Seller.Web
    
    * Fixed storybook configuration for Buyer.Web
    
    * Fixed storybook configuration for identity web
    
    * Updated README
    
    * Added missing information to prerequisites in readme
    
    ---------
    
    Co-authored-by: Piotr Zaranek <piotrek.zaranek@gmail.com>

--- fe/projects/AspNetCore/.storybook/main.js
+++ fe/projects/AspNetCore/.storybook/main.js
@@ -1,11 +1,19 @@
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
 module.exports = {
   stories: ["../src/**/*.stories.js"],
   addons: [
-    "@storybook/preset-create-react-app",
-    "@storybook/addon-actions",
     "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
   ],
-  core: {
-    builder: "webpack5",
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {
+      builder: {
+        useSWC: true,
+      },
+    },
   }
 };
