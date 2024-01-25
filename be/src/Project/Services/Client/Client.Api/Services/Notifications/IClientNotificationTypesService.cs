3c22442cccbd2e49d035f991cab4258b88951e02
Piotr Zaranek <piotrek.zaranek@gmail.com>
2024-01-25 15:11:57 +0100

    Corrected name and type methods

--- be/src/Project/Services/Client/Client.Api/Services/Notifications/IClientNotificationTypesService.cs
+++ be/src/Project/Services/Client/Client.Api/Services/Notifications/IClientNotificationTypesService.cs
@@ -8,9 +8,9 @@ namespace Client.Api.Services.Notifications
 {
     public interface IClientNotificationTypesService
     {
-        Task<PagedResults<IEnumerable<ClientNotificationTypeServiceModel>>> GetAsync(GetClientNotificationTypesServiceModel model);
+        PagedResults<IEnumerable<ClientNotificationTypeServiceModel>> Get(GetClientNotificationTypesServiceModel model);
         Task<ClientNotificationTypeServiceModel> GetAsync(GetClientNotificationTypeServiceModel model);
-        Task Delete(DeleteClientNotificationTypeServiceModel model);
+        Task DeleteAsync(DeleteClientNotificationTypeServiceModel model);
         Task<ClientNotificationTypeServiceModel> UpdateAsync(UpdateClientNotificationTypeServiceModel model);
         Task<ClientNotificationTypeServiceModel> CreateAsync(CreateClientNotificationTypeServiceModel model);
     }
