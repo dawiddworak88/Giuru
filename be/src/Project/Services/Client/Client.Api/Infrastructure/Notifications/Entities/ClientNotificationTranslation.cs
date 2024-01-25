fc54d82a04c2eb52cc72c86ec611cb4f2fe70da2
Piotr Zaranek <piotrek.zaranek@gmail.com>
2024-01-25 09:29:51 +0100

    Corrected ClientNotificationTranslation class

--- be/src/Project/Services/Client/Client.Api/Infrastructure/Notifications/Entities/ClientNotificationTranslation.cs
+++ be/src/Project/Services/Client/Client.Api/Infrastructure/Notifications/Entities/ClientNotificationTranslation.cs
@@ -5,7 +5,7 @@ namespace Client.Api.Infrastructure.Notifications.Entities
 {
     public class ClientNotificationTranslation : Entity
     {
-        public ClientNotification ClientNotificationId { get; set; }
+        public Guid? ClientNotificationId { get; set; }
         public string Message { get; set; }
         public string Language { get; set; }
     }
