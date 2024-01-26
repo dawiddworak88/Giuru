d2dede8fbb06756e1204a5a8a05eb35155aad178
Piotr Zaranek <piotrek.zaranek@gmail.com>
2024-01-26 08:33:20 +0100

    Corrected ClientNotificationType property

--- be/src/Project/Services/Client/Client.Api/Infrastructure/Notifications/Entities/ClientNotificationType.cs
+++ be/src/Project/Services/Client/Client.Api/Infrastructure/Notifications/Entities/ClientNotificationType.cs
@@ -6,6 +6,6 @@ namespace Client.Api.Infrastructure.Notifications.Entities
     public class ClientNotificationType : Entity
     {
         public virtual ClientNotificationTypeApproval ClientNotificationTypeApproval { get; set; }
-        public IEnumerable<ClientNotificationTypeTranslations> Translations { get; set; }
+        public virtual IEnumerable<ClientNotificationTypeTranslations> Translations { get; set; }
     }
 }
