51b9713448591f5a351e8dc27f52e437ee66ce3b
Piotr Zaranek <piotrek.zaranek@gmail.com>
2024-01-30 14:32:40 +0100

    Added model builders for edit notificationType

---
+++ be/src/Project/Web/Seller/Seller.Web/Areas/Clients/ViewModels/ClientNotificationTypeFormViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Seller.Web.Areas.Clients.ViewModels
+{
+    public class ClientNotificationTypeFormViewModel
+    {
+        public Guid? Id { get; set; }
+        public string IdLabel { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string NameRequiredErrorMessage { get; set; }
+        public string NameLabel { get; set; }
+        public string SaveText { get; set; }
+        public string SaveUrl { get; set; }
+        public string GeneralErrorMessage { get; set; }
+        public string NavigateToClientNotificationTypes { get; set; }
+        public string ClientNotificationTypesUrl { get; set; }
+    }
+}
