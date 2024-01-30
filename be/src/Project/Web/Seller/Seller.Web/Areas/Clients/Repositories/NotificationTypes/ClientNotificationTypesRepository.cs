e47b93bce0e9576d1b5c3b16e76c98f40d5ee6b1
Piotr Zaranek <piotrek.zaranek@gmail.com>
2024-01-30 14:41:29 +0100

    Added implementation repository for notificationType

---
+++ be/src/Project/Web/Seller/Seller.Web/Areas/Clients/Repositories/NotificationTypes/ClientNotificationTypesRepository.cs
@@ -0,0 +1,135 @@
+﻿using DocumentFormat.OpenXml.Vml.Spreadsheet;
+using Foundation.ApiExtensions.Communications;
+using Foundation.ApiExtensions.Models.Request;
+using Foundation.ApiExtensions.Models.Response;
+using Foundation.ApiExtensions.Services.ApiClientServices;
+using Foundation.ApiExtensions.Shared.Definitions;
+using Foundation.Extensions.Exceptions;
+using Foundation.GenericRepository.Paginations;
+using Microsoft.Extensions.Options;
+using Seller.Web.Areas.Clients.ApiRequestModels;
+using Seller.Web.Areas.Clients.DomainModels;
+using Seller.Web.Shared.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Seller.Web.Areas.Clients.Repositories.NotificationTypes
+{
+    public class ClientNotificationTypesRepository : IClientNotificationTypesRepository
+    {
+        private readonly IApiClientService _apiClientService;
+        private readonly IOptions<AppSettings> _settings;
+
+        public ClientNotificationTypesRepository(
+            IApiClientService apiClientService,
+            IOptions<AppSettings> settings)
+        {
+            _apiClientService = apiClientService;
+            _settings = settings;
+        }
+
+        public async Task DeleteAsync(string token, string language, Guid? id)
+        {
+            var apiRequest = new ApiRequest<RequestModelBase>
+            {
+                Language = language,
+                Data = new RequestModelBase(),
+                AccessToken = token,
+                EndpointAddress = $"{_settings.Value.ClientUrl}{ApiConstants.Client.NotificationTypesApiEndpoint}/{id}"
+            };
+
+            var response = await _apiClientService.DeleteAsync<ApiRequest<RequestModelBase>, RequestModelBase, BaseResponseModel>(apiRequest);
+
+            if (!response.IsSuccessStatusCode && response?.Data is not null)
+            {
+                throw new CustomException(response.Data.Message, (int)response.StatusCode);
+            }
+        }
+
+        public async Task<PagedResults<IEnumerable<ClientNotificationType>>> GetAsync(string token, string language, string searchTerm, int pageIndex, int itemsPerPage, string orderBy)
+        {
+            var requestModel = new PagedRequestModelBase
+            {
+                SearchTerm = searchTerm,
+                PageIndex = pageIndex,
+                ItemsPerPage = itemsPerPage,
+                OrderBy = orderBy
+            };
+
+            var apiRequest = new ApiRequest<PagedRequestModelBase>
+            {
+                Language = language,
+                Data = requestModel,
+                AccessToken = token,
+                EndpointAddress = $"{_settings.Value.ClientUrl}{ApiConstants.Client.NotificationTypesApiEndpoint}"
+            };
+
+            var response = await _apiClientService.GetAsync<ApiRequest<PagedRequestModelBase>, PagedRequestModelBase, PagedResults<IEnumerable<ClientNotificationType>>>(apiRequest);
+
+            if (response.IsSuccessStatusCode && response.Data?.Data is not null)
+            {
+                return new PagedResults<IEnumerable<ClientNotificationType>>(response.Data.Total, response.Data.PageSize)
+                {
+                    Data = response.Data.Data
+                };
+            }
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new CustomException(response.Message, (int)response.StatusCode);
+            }
+
+            return default;
+        }
+
+        public async Task<ClientNotificationType> GetAsync(string token, string language, Guid? id)
+        {
+            var apiRequest = new ApiRequest<RequestModelBase>
+            {
+                Language = language,
+                Data = new RequestModelBase(),
+                AccessToken = token,
+                EndpointAddress = $"{_settings.Value.ClientUrl}{ApiConstants.Client.NotificationTypesApiEndpoint}/{id}"
+            };
+
+            var response = await _apiClientService.GetAsync<ApiRequest<RequestModelBase>, RequestModelBase, ClientNotificationType>(apiRequest);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new CustomException(response.Message, (int)response.StatusCode);
+            }
+
+            if (response.IsSuccessStatusCode && response.Data is not null) 
+            {
+                return response.Data;
+            }
+
+            return default;
+        }
+
+        public async Task SaveAsync(string token, string language, Guid? id, string name)
+        {
+            var requestModel = new ClientNotificationTypeRequestModel
+            {
+                Id = id,
+                Name = name
+            };
+
+            var apiReqest = new ApiRequest<ClientNotificationTypeRequestModel>
+            {
+                Language = language,
+                Data = requestModel,
+                AccessToken = token,
+                EndpointAddress = $"{_settings.Value.ClientUrl}{ApiConstants.Client.NotificationTypesApiEndpoint}"
+            };
+
+            var response = await _apiClientService.PostAsync<ApiRequest<ClientNotificationTypeRequestModel>, ClientNotificationTypeRequestModel, BaseResponseModel>(apiReqest);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new CustomException(response.Data.Message, (int)response.StatusCode);
+            }
+        }
+    }
+}
--- be/src/Project/Web/Seller/Seller.Web/Areas/Clients/Repositories/NotificationTypes/IClientNotificationTypesRepository.cs
+++ be/src/Project/Web/Seller/Seller.Web/Areas/Clients/Repositories/NotificationTypes/IClientNotificationTypesRepository.cs
@@ -8,7 +8,7 @@ namespace Seller.Web.Areas.Clients.Repositories.NotificationTypes
 {
     public interface IClientNotificationTypesRepository
     {
-        Task<Guid> SaveAsync(string token, string language, Guid? id, string name);
+        Task SaveAsync(string token, string language, Guid? id, string name);
         Task<PagedResults<IEnumerable<ClientNotificationType>>> GetAsync(string token, string language, string searchTerm, int pageIndex, int itemsPerPage, string orderBy);
         Task<ClientNotificationType> GetAsync(string token, string language, Guid? id);
         Task DeleteAsync(string token, string language, Guid? id);
