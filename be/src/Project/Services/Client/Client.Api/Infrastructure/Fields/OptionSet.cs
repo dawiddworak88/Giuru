f0a828e797996e0aa7b7bd43d0260c3600631ab3
Sebastian Kasperek <sebastian.kasperek@eltap.com>
2024-01-31 14:27:28 +0100

    Removed redundant virtual table

--- be/src/Project/Services/Client/Client.Api/Infrastructure/Fields/OptionSet.cs
+++ be/src/Project/Services/Client/Client.Api/Infrastructure/Fields/OptionSet.cs
@@ -1,10 +1,8 @@
 ﻿using Foundation.GenericRepository.Entities;
-using System.Collections.Generic;
 
 namespace Client.Api.Infrastructure.Fields
 {
     public class OptionSet : Entity
     {
-        public virtual IEnumerable<OptionSetTranslation> OptionSetTranslations { get; set; }
     }
 }
