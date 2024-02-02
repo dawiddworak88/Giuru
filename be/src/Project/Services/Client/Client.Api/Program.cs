0cf59f18b7a83c33e8712b451d235081bd3fa485
Dawid Dworak <dawiddworak88@gmail.com>
2024-02-02 08:50:08 +0100

    Fixed a problem with IS4 (#327)
    * Changed target framework to net8.0
    
    * Changed sdk version
    
    * Updated NuGet packages in all projects
    
    * Changed creating LogzioDataCenter object
    
    * Updated nuget packages for telemtry to prerelease version
    
    * CHnaged implementation for Tracing and Metrics method
    
    * Fixed Piotr problem with "not instaled .net8"
    
    * Changed .Net version and port
    
    * changed port for services
    
    * Commented invoke functions
    
    * Fixed EF problem with ConnectionStrings
    
    * Fixed OpenTelemetry loop
    
    * Added ports to api url in buyer, seller and identity-api
    
    * Chnaged EnviromentVariables and routing configuration
    
    * Corrected value MinimumSameSitePolicy
    
    * Removed unnecessary comma
    
    * Fixed analytics-api environment links
    
    * Updated to .NET 8.0.1
    
    * Fixed a problem with IS4
    
    ---------
    
    Co-authored-by: Piotr Zaranek <piotrek.zaranek@gmail.com>
    Co-authored-by: Sebastian Kasperek <sebastian.kasperek@eltap.com>

--- be/src/Project/Services/Client/Client.Api/Program.cs
+++ be/src/Project/Services/Client/Client.Api/Program.cs
@@ -26,10 +26,7 @@ using Foundation.Telemetry.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Host.ConfigureAppConfiguration((_, config) =>
-{
-    config.AddEnvironmentVariables();
-});
+builder.Configuration.AddEnvironmentVariables();
 
 builder.Host.UseSerilog((hostingContext, loggerConfiguration) =>
 {
@@ -48,7 +45,10 @@ builder.Host.UseSerilog((hostingContext, loggerConfiguration) =>
             hostingContext.Configuration["LogzIoType"],
             new LogzioOptions
             {
-                DataCenterSubDomain = hostingContext.Configuration["LogzIoDataCenterSubDomain"]
+                DataCenter = new LogzioDataCenter
+                {
+                    SubDomain = hostingContext.Configuration["LogzIoDataCenterSubDomain"]
+                }
             });
     }
 
@@ -131,20 +131,17 @@ app.UseAuthorization();
 
 app.UseCustomHeaderRequestLocalizationProvider(builder.Configuration, app.Services.GetService<IOptionsMonitor<LocalizationSettings>>());
 
-app.UseEndpoints(endpoints =>
+app.MapControllers();
+
+app.MapHealthChecks("/hc", new HealthCheckOptions
 {
-    endpoints.MapControllers();
+    Predicate = _ => true,
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
 
-    endpoints.MapHealthChecks("/hc", new HealthCheckOptions()
-    {
-        Predicate = _ => true,
-        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
-    });
-
-    endpoints.MapHealthChecks("/liveness", new HealthCheckOptions
-    {
-        Predicate = r => r.Name.Contains("self")
-    });
+app.MapHealthChecks("/liveness", new HealthCheckOptions
+{
+    Predicate = r => r.Name.Contains("self")
 });
 
 app.Run();
