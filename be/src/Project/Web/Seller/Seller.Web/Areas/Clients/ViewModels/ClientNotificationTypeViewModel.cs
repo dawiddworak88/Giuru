ed8b6b11fc5e5db33b516da2dcefd96305842fa1
Piotr Zaranek <piotrek.zaranek@gmail.com>
2024-02-02 08:07:17 +0100

    Added view model for approval

---
+++ be/src/Project/Web/Seller/Seller.Web/Areas/Clients/ViewModels/ClientNotificationTypeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Seller.Web.Areas.Clients.ViewModels
+{
+    public class ClientNotificationTypeViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public bool IsApproved { get; set; }
+        public DateTime ApprovalDate { get; set; }
+    }
+}
