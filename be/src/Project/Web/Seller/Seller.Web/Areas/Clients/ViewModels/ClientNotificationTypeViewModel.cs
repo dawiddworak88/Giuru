e079511c4d93b526ab5bc1d133f4784c0d990a26
Piotr Zaranek <piotrek.zaranek@gmail.com>
2024-02-06 08:31:36 +0100

    Chnaged ApprovalDate type property

--- be/src/Project/Web/Seller/Seller.Web/Areas/Clients/ViewModels/ClientNotificationTypeViewModel.cs
+++ be/src/Project/Web/Seller/Seller.Web/Areas/Clients/ViewModels/ClientNotificationTypeViewModel.cs
@@ -7,6 +7,6 @@ namespace Seller.Web.Areas.Clients.ViewModels
         public Guid Id { get; set; }
         public string Name { get; set; }
         public bool IsApproved { get; set; }
-        public DateTime ApprovalDate { get; set; }
+        public DateTime? ApprovalDate { get; set; }
     }
 }
