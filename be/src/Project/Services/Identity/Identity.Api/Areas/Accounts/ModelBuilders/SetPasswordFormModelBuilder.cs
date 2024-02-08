7c0a8ab1ae69bdb8ea591b53343a3d1848b4d712
Piotr Zaranek <piotrek.zaranek@gmail.com>
2024-02-08 12:04:07 +0100

    Added geting clientNotificationTypes

--- be/src/Project/Services/Identity/Identity.Api/Areas/Accounts/ModelBuilders/SetPasswordFormModelBuilder.cs
+++ be/src/Project/Services/Identity/Identity.Api/Areas/Accounts/ModelBuilders/SetPasswordFormModelBuilder.cs
@@ -1,14 +1,21 @@
 ﻿using Feature.Account;
 using Foundation.Extensions.ModelBuilders;
+using Foundation.GenericRepository.Definitions;
 using Foundation.Localization;
 using Identity.Api.Areas.Accounts.ComponentModels;
+using Identity.Api.Areas.Accounts.Definitions;
+using Identity.Api.Areas.Accounts.Repositories;
 using Identity.Api.Areas.Accounts.ViewModels;
+using Identity.Api.Services.Tokens;
 using Identity.Api.Services.Users;
 using Identity.Api.ServicesModels.Users;
+using Identity.Api.Configurations;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Options;
 using System.Globalization;
 using System.Threading.Tasks;
+using Identity.Api.Areas.Accounts.Models;
 
 namespace Identity.Api.Areas.Accounts.ModelBuilders
 {
@@ -18,17 +25,26 @@ namespace Identity.Api.Areas.Accounts.ModelBuilders
         private readonly IStringLocalizer<GlobalResources> _globalLocalizer;
         private readonly IStringLocalizer<AccountResources> _accountLocalizer;
         private readonly LinkGenerator _linkGenerator;
+        private readonly IClientNotificationTypeRepository _clientNotificationTypeRepository;
+        private readonly ITokenService _tokenService;
+        private readonly IOptions<AppSettings> _options;
 
         public SetPasswordFormModelBuilder(
             IStringLocalizer<GlobalResources> globalLocalizer, 
             IStringLocalizer<AccountResources> accountLocalizer, 
             LinkGenerator linkGenerator,
-            IUsersService usersService)
+            IUsersService usersService,
+            IClientNotificationTypeRepository clientNotificationTypeRepository,
+            ITokenService tokenService,
+            IOptions<AppSettings> options)
         {
             _globalLocalizer = globalLocalizer;
             _accountLocalizer = accountLocalizer;
             _linkGenerator = linkGenerator;
             _usersService = usersService;
+            _clientNotificationTypeRepository = clientNotificationTypeRepository;
+            _tokenService = tokenService;
+            _options = options;
         }
 
         public async Task<SetPasswordFormViewModel> BuildModelAsync(SetPasswordFormComponentModel componentModel)
@@ -48,9 +64,7 @@ namespace Identity.Api.Areas.Accounts.ModelBuilders
                 GeneralErrorMessage = _globalLocalizer.GetString("AnErrorOccurred"),
                 PasswordSetSuccessMessage = _accountLocalizer.GetString("PasswordUpdated"),
                 MarketingApprovalHeader = _accountLocalizer.GetString("MarketingApprovalHeader"),
-                MarketingApprovalText = _accountLocalizer.GetString("MarketingApprovalText"),
-                EmailMarketingApprovalLabel = _accountLocalizer.GetString("EmailMarketingApprovalLabel"),
-                SmsMarketingApprovalLabel = _accountLocalizer.GetString("SmsMarketingApprovalLabel")
+                MarketingApprovalText = _accountLocalizer.GetString("MarketingApprovalText")
             };
 
             if (componentModel.Id.HasValue)
@@ -69,6 +83,15 @@ namespace Identity.Api.Areas.Accounts.ModelBuilders
                 }
             }
 
+            var token = await _tokenService.GetTokenAsync(_options.Value.ApiEmail, _options.Value.ApiOrganisationId, _options.Value.ApiAppSecret);
+
+            var notificationTypes = await _clientNotificationTypeRepository.GetByIds(token, componentModel.Language, $"{ClientNotificationTypeConstants.SmsMarketingApprovalId},{ClientNotificationTypeConstants.EmailMarketingApprovalId}", null, Constants.DefaultPageIndex, Constants.DefaultItemsPerPage, $"{nameof(ClientNotificationTypeApproval.CreatedDate)} desc");
+
+            if (notificationTypes is not null)
+            {
+                viewModel.NotificationTypes = notificationTypes;
+            }
+            
             return viewModel;
         }
     }
