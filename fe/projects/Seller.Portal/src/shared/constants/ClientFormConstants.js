0c142e8806bbbe43099e4895f7685febed8380d7
Piotr Zaranek <piotrek.zaranek@gmail.com>
2024-02-12 17:18:09 +0100

    Removed unnecessary constants

--- fe/projects/Seller.Portal/src/shared/constants/ClientFormConstants.js
+++
@@ -1,9 +0,0 @@
-export default class ClientFormConstants {
-    static emailMarketingApprovalName() {
-        return "EMAIL_MARKETING_APPROVAL";
-    }
-
-    static smsMarketingApprovalName() {
-        return "SMS_MARKETING_APPROVAL";
-    }
-}
