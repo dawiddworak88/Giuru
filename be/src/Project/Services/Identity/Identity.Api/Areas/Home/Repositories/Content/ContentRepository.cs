943cdf0237e8226217e754b86d308ea0c066da86
Piotr Zaranek <piotrek.zaranek@gmail.com>
2024-02-12 12:35:04 +0100

    Added method to get policy

--- be/src/Project/Services/Identity/Identity.Api/Areas/Home/Repositories/Content/ContentRepository.cs
+++ be/src/Project/Services/Identity/Identity.Api/Areas/Home/Repositories/Content/ContentRepository.cs
@@ -8,7 +8,10 @@ using System;
 using System.Threading.Tasks;
 using Foundation.Extensions.ExtensionMethods;
 using System.Linq;
-using Identity.Api.Areas.Home.ResponseModels;
+using Identity.Api.Areas.Home.DomainModels;
+using Identity.Api.Areas.Home.ResponseModels.Content;
+using Identity.Api.Areas.Home.ResponseModels.Policy;
+using Microsoft.AspNetCore.Mvc.Formatters;
 
 namespace Identity.Api.Areas.Home.Repositories.Content
 {
@@ -65,6 +68,60 @@ namespace Identity.Api.Areas.Home.Repositories.Content
             catch (Exception exception)
             {
                 _logger.LogError(exception, $"Couldn't get content for ${contentPageKey} in language ${language}");
+            }
+
+            return default;
+        }
+
+        public async Task<DomainModels.Policy> GetPolicyAsync(string language)
+        {
+            try
+            {
+                var query = new GraphQLRequest
+                {
+                    Query = $@"query getPrivacyPolicy {{
+                      privacyPolicyPage(locale: ""{language}"") {{
+                        data {{
+                          id
+                          attributes {{
+                            policy {{
+                              title
+                              description
+                              accordion {{
+                                accordionItems {{
+                                  id
+                                  title
+                                  description
+                                }}
+                              }}
+                            }}
+                          }}
+                        }}
+                      }}
+                    }}"
+                };
+
+                var response = await _graphQlClient.SendQueryAsync<JObject>(query);
+
+                if (response.Errors.OrEmptyIfNull().Any() is false && response?.Data is not null)
+                {
+                    var metaData = JsonConvert.DeserializeObject<PolicyGraphQlResponseModel>(response.Data.ToString());
+
+                    return new DomainModels.Policy
+                    {
+                        Title = metaData.Page?.Data?.Attributes?.Policy?.Title,
+                        Description = metaData.Page?.Data?.Attributes?.Policy?.Description,
+                        Accordions = metaData.Page?.Data?.Attributes?.Policy?.Accordion?.AccordionItems.Select(x => new DomainModels.AccordionItem
+                        {
+                            Title = x.Title,
+                            Description = x.Description
+                        })
+                    };
+                }
+            }
+            catch (Exception exception)
+            {
+                _logger.LogError(exception, $"Couldn't get policy for privacy policy in language ${language}");
             }
 
             return default;
--- be/src/Project/Services/Identity/Identity.Api/Areas/Home/Repositories/Content/IContentRepository.cs
+++ be/src/Project/Services/Identity/Identity.Api/Areas/Home/Repositories/Content/IContentRepository.cs
@@ -1,9 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using Identity.Api.Areas.Home.DomainModels;
+using System.Threading.Tasks;
 
 namespace Identity.Api.Areas.Home.Repositories.Content
 {
     public interface IContentRepository
     {
         Task<DomainModels.Content> GetContentAsync(string contentPageKey, string language);
+        Task<Policy> GetPolicyAsync(string language);
     }
 }
