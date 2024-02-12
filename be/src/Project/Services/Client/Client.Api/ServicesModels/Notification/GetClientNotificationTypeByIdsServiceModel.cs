866ae5eb5445f7fc84b4b890743b2251d5d9bf95
Piotr Zaranek <piotrek.zaranek@gmail.com>
2024-02-12 16:02:05 +0100

    Changed type of class

--- be/src/Project/Services/Client/Client.Api/ServicesModels/Notification/GetClientNotificationTypeByIdsServiceModel.cs
+++ be/src/Project/Services/Client/Client.Api/ServicesModels/Notification/GetClientNotificationTypeByIdsServiceModel.cs
@@ -4,11 +4,8 @@ using Foundation.Extensions.Models;
 
 namespace Client.Api.ServicesModels.Notification
 {
-    public class GetClientNotificationTypeByIdsServiceModel : BaseServiceModel
+    public class GetClientNotificationTypeByIdsServiceModel : PagedBaseServiceModel
     {
         public IEnumerable<Guid> Ids { get; set; }
-        public int? PageIndex { get; set; }
-        public int? ItemsPerPage { get; set; }
-        public string OrderBy { get; set; }
     }
 }
