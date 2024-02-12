127b1de8c93877a3f1eac3bbfc1b651ff96909a2
Piotr Zaranek <piotrek.zaranek@gmail.com>
2024-02-12 12:38:08 +0100

    Added models for policy

---
+++ be/src/Project/Services/Identity/Identity.Api/Areas/Home/DomainModels/Policy.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Identity.Api.Areas.Home.DomainModels
+{
+    public class Policy
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public IEnumerable<AccordionItem> Accordions { get; set; }
+    } 
+}
