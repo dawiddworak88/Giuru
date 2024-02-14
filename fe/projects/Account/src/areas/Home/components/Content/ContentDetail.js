b1f40944b112e4e952baf83247f42948392518ba
Piotr Zaranek <piotrek.zaranek@gmail.com>
2024-02-14 14:29:02 +0100

    Changed ContentDetails

--- fe/projects/Account/src/areas/Home/components/Content/ContentDetail.js
+++ fe/projects/Account/src/areas/Home/components/Content/ContentDetail.js
@@ -1,26 +1,49 @@
 import React from "react";
+import { Accordion, AccordionDetails, AccordionSummary, Typography } from "@mui/material";
 import PropTypes from "prop-types";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { marked } from "marked";
 
-function ContentDetail(props) {
+const ContentDetail = (props) => {
 
     return (
-
         <section className="section">
             <div className="container">
-                <div className="content has-text-justified"></div>
-                <h1 className="title is-3">{props.title}</h1>
-                {props.content &&
-                    <div dangerouslySetInnerHTML={{__html: marked.parse(props.content)}}></div>
+                <h2 className="title is-3">{props.title}</h2>
+                {props.description &&
+                    <div dangerouslySetInnerHTML={{__html: marked.parse(props.description)}}></div>
                 }
+                <div>
+                    {props.accordionItems && props.accordionItems.length > 0 &&
+                        props.accordionItems.map((accordion, index) => {
+                            return (
+                                <div key={index} className="mt-5">
+                                    <Accordion className="accordion">
+                                        <AccordionSummary
+                                            expandIcon={<ExpandMoreIcon />}
+                                            aria-controls="panel-content"
+                                            id={index}
+                                        >
+                                            <Typography fontWeight={"bold"}>{accordion.title}</Typography>
+                                        </AccordionSummary>
+                                        <AccordionDetails>
+                                            <Typography>{accordion.description}</Typography>
+                                        </AccordionDetails>
+                                    </Accordion>
+                                </div>
+                            );
+                        })
+                    }
+                </div>
             </div>
         </section>
     );
-}
+};
 
 ContentDetail.propTypes = {
     title: PropTypes.string.isRequired,
-    content: PropTypes.string
+    description: PropTypes.string,
+    accordionItems: PropTypes.array
 };
 
 export default ContentDetail;
