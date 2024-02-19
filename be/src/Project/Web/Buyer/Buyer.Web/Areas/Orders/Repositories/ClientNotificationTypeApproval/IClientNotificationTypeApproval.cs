29948acec01aad9a6f3d1dfd6beb964a12820234
Piotr Zaranek <piotrek.zaranek@gmail.com>
2024-02-19 15:25:38 +0100

    Added repositories for ClientNotificationTypeApproval

---
+++ be/src/Project/Web/Buyer/Buyer.Web/Areas/Orders/Repositories/ClientNotificationTypeApproval/IClientNotificationTypeApproval.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Buyer.Web.Areas.Orders.Repositories.NotificationTypeApproval
+{
+    public interface IClientNotificationTypeApproval
+    {
+        public Task<IEnumerable<DomainModels.ClientNotificationTypeApproval>> GetAsync(string token, string language, Guid? clientId);
+    }
+}
