5e1b55c04761c7939315f9b041b1d61032008642
Sebastian Kasperek <sebastian.kasperek@eltap.com>
2024-02-22 14:48:57 +0100

    Added migration

--- be/src/Project/Services/Catalog/Catalog.Api/Infrastructure/Migrations/20240222134746_AddedFulfilmentDate.Designer.cs
+++ be/src/Project/Services/Catalog/Catalog.Api/Infrastructure/Migrations/20240222134746_AddedFulfilmentDate.Designer.cs
@@ -4,6 +4,7 @@ using Foundation.Catalog.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Catalog.Api.Infrastructure.Migrations
 {
     [DbContext(typeof(CatalogContext))]
-    partial class CatalogContextModelSnapshot : ModelSnapshot
+    [Migration("20240222134746_AddedFulfilmentDate")]
+    partial class AddedFulfilmentDate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ be/src/Project/Services/Catalog/Catalog.Api/Infrastructure/Migrations/20240222134746_AddedFulfilmentDate.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Catalog.Api.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedFulfilmentDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FulfilmentDate",
+                table: "Products",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CategoryImages_CategoryId",
+                table: "CategoryImages",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_Parentid",
+                table: "Categories",
+                column: "Parentid");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Categories_Categories_Parentid",
+                table: "Categories",
+                column: "Parentid",
+                principalTable: "Categories",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CategoryImages_Categories_CategoryId",
+                table: "CategoryImages",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Categories_Categories_Parentid",
+                table: "Categories");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CategoryImages_Categories_CategoryId",
+                table: "CategoryImages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CategoryImages_CategoryId",
+                table: "CategoryImages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_Parentid",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "FulfilmentDate",
+                table: "Products");
+        }
+    }
+}
