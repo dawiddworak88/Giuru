71aa7021dbe90615b7f0cd49f02d545127639290
Piotr Zaranek <piotrek.zaranek@gmail.com>
2024-03-02 19:13:21 +0100

    Removed unnecessary adding index amd foreignKey

--- be/src/Project/Services/Catalog/Catalog.Api/Infrastructure/Migrations/20240301153846_FulfillmentTime.cs
+++ be/src/Project/Services/Catalog/Catalog.Api/Infrastructure/Migrations/20240301153846_FulfillmentTime.cs
@@ -15,52 +15,11 @@ namespace Catalog.Api.Infrastructure.Migrations
                 table: "Products",
                 type: "int",
                 nullable: true);
-
-            migrationBuilder.CreateIndex(
-                name: "IX_CategoryImages_CategoryId",
-                table: "CategoryImages",
-                column: "CategoryId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Categories_Parentid",
-                table: "Categories",
-                column: "Parentid");
-
-            migrationBuilder.AddForeignKey(
-                name: "FK_Categories_Categories_Parentid",
-                table: "Categories",
-                column: "Parentid",
-                principalTable: "Categories",
-                principalColumn: "Id");
-
-            migrationBuilder.AddForeignKey(
-                name: "FK_CategoryImages_Categories_CategoryId",
-                table: "CategoryImages",
-                column: "CategoryId",
-                principalTable: "Categories",
-                principalColumn: "Id",
-                onDelete: ReferentialAction.Cascade);
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropForeignKey(
-                name: "FK_Categories_Categories_Parentid",
-                table: "Categories");
-
-            migrationBuilder.DropForeignKey(
-                name: "FK_CategoryImages_Categories_CategoryId",
-                table: "CategoryImages");
-
-            migrationBuilder.DropIndex(
-                name: "IX_CategoryImages_CategoryId",
-                table: "CategoryImages");
-
-            migrationBuilder.DropIndex(
-                name: "IX_Categories_Parentid",
-                table: "Categories");
-
             migrationBuilder.DropColumn(
                 name: "FulfillmentTime",
                 table: "Products");
