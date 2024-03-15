2ddc65089381adb17cca470aef0d905b4c5e04c0
Piotr Zaranek <piotrek.zaranek@gmail.com>
2024-03-15 20:22:38 +0100

    Changed header style

--- fe/projects/AspNetCore/src/shared/components/Header/Header.js
+++ fe/projects/AspNetCore/src/shared/components/Header/Header.js
@@ -2,20 +2,25 @@ import React, { useState, useContext, useEffect } from "react";
 import { toast } from "react-toastify";
 import Autosuggest from "react-autosuggest";
 import PropTypes from "prop-types";
-import { Button } from "@mui/material";
+import { Button, FormControl, Icon, InputLabel, MenuItem, Select } from "@mui/material";
 import LanguageSwitcher from "../../../shared/components/LanguageSwitcher/LanguageSwitcher";
 import HeaderConstants from "./HeaderConstants";
 import QueryStringSerializer from "../../../shared/helpers/serializers/QueryStringSerializer";
 import NavigationHelper from "../../../shared/helpers/globals/NavigationHelper";
 import { Context } from "../../stores/Store";
-import { ShoppingCart } from '@mui/icons-material';
+import CategoryIcon from "../../Icons/Category";
+import RemoveIcon from "../../Icons/Remove";
+import SearchIcon from "../../Icons/Search";
+import { ShoppingCart, ExpandMoreOutlined } from '@mui/icons-material';
 import AuthenticationHelper from "../../../shared/helpers/globals/AuthenticationHelper";
+import UserPopup from "../UserPopup/UserPopup";
 
 function Header(props) {
     const [state, dispatch] = useContext(Context);
     const [searchTerm, setSearchTerm] = useState(props.searchTerm ? props.searchTerm : "");
     const [suggestions, setSuggestions] = useState([]);
     const [totalBasketItems, setTotalBasketItems] = useState(props.totalBasketItems ? props.totalBasketItems : 0);
+    const [searchArea, setSearchArea] = useState(1);
 
     const getSuggestionValue = (suggestion) => {
         return suggestion;
@@ -87,8 +92,8 @@ function Header(props) {
 
     const searchInputProps = {
         placeholder: props.searchPlaceholderLabel,
-        className: "search__field",
         value: searchTerm,
+        className: "search__field",
         onChange: (_, { newValue, method }) => {
             setSearchTerm(newValue);
         }
@@ -103,61 +108,89 @@ function Header(props) {
         setTotalBasketItems(state.totalBasketItems);
 
     }, [state])
-    
+
     return (
         <header>
-            <nav className="is-flex is-justify-content-space-between p-3 px-4 is-align-items-center header">
-                <div className="navbar-brand is-align-items-center">
-                    <a className="navbar-logo" href={props.logo.targetUrl}>
-                        <img src={props.logo.logoUrl} alt={props.logo.logoAltLabel} />
+            <nav className="is-flex p-4 px-4 is-align-items-center header">
+                <div className="navbar__start is-flex is-justify-content-center">
+                    <a href={props.logo.targetUrl}>
+                        <img src={props.logo.logoUrl} alt={props.logo.logoAltLabel} className="navbar__start__logo"/>
                     </a>
-                    <div className="navbar-start">
-                        <form action={props.searchUrl} method="get" role="search" onSubmit={onSearchSubmit}>
-                            <div className="field is-flex is-flex-centered search">
-                                <Autosuggest
-                                    suggestions={suggestions}
-                                    onSuggestionsFetchRequested={onSuggestionsFetchRequested}
-                                    onSuggestionsClearRequested={() => setSuggestions([])}
-                                    getSuggestionValue={getSuggestionValue}
-                                    onSuggestionSelected={onSuggestionSelected}
-                                    renderSuggestion={renderSuggestion}
-                                    inputProps={searchInputProps} 
-                                />
-                                <Button type="submit" variant="contained" color="secondary" className="search-button">
-                                    {props.searchLabel}
-                                </Button>
+                </div>
+                <div className="navbar__search is-flex">
+                    <div className="navbar__search__text is-flex">
+                        <div className="navbar__search__text__icon">
+                            <SearchIcon />
+                        </div>
+                        <div className="navbar__search__text__input">
+                            <Autosuggest
+                                suggestions={suggestions}
+                                onSuggestionsFetchRequested={onSuggestionsFetchRequested}
+                                onSuggestionsClearRequested={() => setSuggestions([])}
+                                getSuggestionValue={getSuggestionValue}
+                                onSuggestionSelected={onSuggestionSelected}
+                                renderSuggestion={renderSuggestion}
+                                inputProps={searchInputProps}
+                            />
                             </div>
-                        </form>
+                        <div className="navbar__search__text__remove">
+                            { searchTerm.length > 0 &&
+                                <Button 
+                                    onClick={() => setSearchTerm("")}
+                                    sx={{ 
+                                        p: 0,
+                                        minWidth: '24px',
+                                    }}
+                                >
+                                    <RemoveIcon />
+                                </Button>
+                            }
+                        </div>
+                    </div>
+                    <div className="navbar__search__area">
+                        <FormControl fullWidth>
+                            <Select
+                                className="navbar__search__area__select"
+                                value={searchArea}
+                                onChange={(e) => {setSearchArea(e.target.value)}}
+                                displayEmpty
+                                IconComponent={ExpandMoreOutlined}
+                                sx={{ 
+                                    boxShadow: 'none',
+                                    borderRadius: 0,
+                                    '.MuiOutlinedInput-notchedOutline': { border: 0 },
+                                    '.MuiSelect-select': { p: 1, textAlign: 'center' }
+                                    }}
+                                >
+                                <MenuItem value={1}>
+                                    <div className="is-flex pt-2 is-justify-content-center">
+                                        <div className="pr-2">
+                                            <CategoryIcon />
+                                        </div>
+                                        <div>
+                                            Wszytkie
+                                        </div>
+                                    </div>
+                                </MenuItem>
+                                <MenuItem value={2}>
+                                <div className="is-flex pt-2">
+                                        <div className="pr-2">
+                                            <CategoryIcon />
+                                        </div>
+                                        <div>
+                                            Stany magazynowe
+                                        </div>
+                                    </div>
+                                </MenuItem>
+                            </Select>
+                        </FormControl>
                     </div>
                 </div>
-                <div className="navbar-container">
-                    <div className="navbar-end is-flex is-align-items-center">
-                        {props.isLoggedIn ? (
-                            props.signOutLink &&
-                                <div className="navbar-item">
-                                    <span className="welcome-text">{props.welcomeText} {props.name}, </span>
-                                    <a href={props.signOutLink.url} className="button is-text">{props.signOutLink.text}</a>
-                                </div>
-                        ) : (
-                            props.signInLink &&
-                                <div className="navbar-item">
-                                    <a className="button is-text" href={props.signInLink.url}>
-                                        {props.signInLink.text}
-                                    </a>
-                                </div>
-                        )}
-                        <div className="navbar-item">
-                            <LanguageSwitcher {...props.languageSwitcher} />
-                        </div>
-                        <div className="navbar-item">
-                            <a href={props.basketUrl} className="button is-text" title={props.goToCartLabel} aria-label={props.goToCartLabel}>
-                                <ShoppingCart />
-                                {totalBasketItems > 0 &&
-                                    <span className="count">{totalBasketItems}</span>
-                                }
-                            </a>
-                        </div>
+                <div className="navbar__actions is-flex is-justify-content-center">
+                    <div className="navbar__actions__language">
+                        <LanguageSwitcher {...props.languageSwitcher} />
                     </div>
+                    <UserPopup {...props.userPopup} />
                 </div>
             </nav>
         </header>
@@ -177,3 +210,56 @@ Header.propTypes = {
 };
 
 export default Header;
+
+{/* <div className="navbar-brand is-align-items-center">
+<a className="navbar-logo" href={props.logo.targetUrl}>
+    <img src={props.logo.logoUrl} alt={props.logo.logoAltLabel} />
+</a>
+<div className="navbar-start">
+    <form action={props.searchUrl} method="get" role="search" onSubmit={onSearchSubmit}>
+        <div className="field is-flex is-flex-centered search">
+            <Autosuggest
+                suggestions={suggestions}
+                onSuggestionsFetchRequested={onSuggestionsFetchRequested}
+                onSuggestionsClearRequested={() => setSuggestions([])}
+                getSuggestionValue={getSuggestionValue}
+                onSuggestionSelected={onSuggestionSelected}
+                renderSuggestion={renderSuggestion}
+                inputProps={searchInputProps} 
+            />
+            <Button type="submit" variant="contained" color="secondary" className="search-button">
+                {props.searchLabel}
+            </Button>
+        </div>
+    </form>
+</div>
+</div>
+<div className="navbar-container">
+<div className="navbar-end is-flex is-align-items-center">
+    {props.isLoggedIn ? (
+        props.signOutLink &&
+            <div className="navbar-item">
+                <span className="welcome-text">{props.welcomeText} {props.name}, </span>
+                <a href={props.signOutLink.url} className="button is-text">{props.signOutLink.text}</a>
+            </div>
+    ) : (
+        props.signInLink &&
+            <div className="navbar-item">
+                <a className="button is-text" href={props.signInLink.url}>
+                    {props.signInLink.text}
+                </a>
+            </div>
+    )}
+    <div className="navbar-item">
+        <LanguageSwitcher {...props.languageSwitcher} />
+    </div>
+    <div className="navbar-item">
+        <a href={props.basketUrl} className="button is-text" title={props.goToCartLabel} aria-label={props.goToCartLabel}>
+            <ShoppingCart />
+            {totalBasketItems > 0 &&
+                <span className="count">{totalBasketItems}</span>
+            }
+        </a>
+    </div>
+</div>
+</div> */}
