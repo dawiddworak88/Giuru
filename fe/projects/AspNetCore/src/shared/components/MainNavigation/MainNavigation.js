6984de71d4fc0fd156d95663ac109b2f8b2694f6
Piotr Zaranek <piotrek.zaranek@gmail.com>
2024-03-20 11:58:58 +0100

    Removed mobile styles

--- fe/projects/AspNetCore/src/shared/components/MainNavigation/MainNavigation.js
+++ fe/projects/AspNetCore/src/shared/components/MainNavigation/MainNavigation.js
@@ -1,23 +1,11 @@
-import React, {useState, useRef} from "react";
+import React from "react";
 import PropTypes from "prop-types";
-import { Close, Menu } from "@mui/icons-material";
 
 function MainNavigation(props) {
-    const [isOpen, setIsOpen] = useState(false);
-    const ref = useRef(null);
-
-    const openNavigation = () => {
-        setIsOpen(!isOpen)
-    }
 
     return (
-        <nav className="main-nav" ref={ref}>
-            <div className="main-nav__mobile">
-                <div className="icon" onClick={openNavigation}>
-                    {isOpen ? <Close /> : <Menu />}
-                </div>
-            </div>
-            <div className={`main-nav__links-container ${isOpen ? "active" : ""}`}>
+        <nav className="main-nav">
+            <div className='main-nav__links-container'>
                 {props.links && props.links.map((link, index) => 
                     <a key={index} href={link.url} target={link.target}>{link.text}</a>
                 )}
