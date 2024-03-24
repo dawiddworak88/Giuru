9a18e0b31032938405dde1b84473ec7ca2256e01
Sebastian Kasperek <sebastian.kasperek@eltap.com>
2024-03-24 02:58:05 +0100

    Updeted OrderStatuses controller

--- be/src/Project/Services/Ordering/Ordering.Api/v1/Controllers/OrderStatusesController.cs
+++ be/src/Project/Services/Ordering/Ordering.Api/v1/Controllers/OrderStatusesController.cs
@@ -1,7 +1,5 @@
 ﻿using Foundation.Account.Definitions;
 using Foundation.ApiExtensions.Controllers;
-using Foundation.Extensions.Definitions;
-using Foundation.Extensions.Exceptions;
 using Foundation.Extensions.Helpers;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -11,7 +9,6 @@ using System.Net;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Ordering.Api.ServicesModels;
-using Ordering.Api.Validators;
 using Ordering.Api.v1.ResponseModels;
 using Ordering.Api.Services.Orders;
 
@@ -23,11 +20,11 @@ namespace Ordering.Api.v1.Controllers
     [ApiController]
     public class OrderStatusesController : BaseApiController
     {
-        private readonly IOrdersService ordersService;
+        private readonly IOrdersService _ordersService;
 
         public OrderStatusesController(IOrdersService ordersService)
         {
-            this.ordersService = ordersService;
+            _ordersService = ordersService;
         }
 
         /// <summary>
@@ -40,18 +37,18 @@ namespace Ordering.Api.v1.Controllers
         [ProducesResponseType((int)HttpStatusCode.UnprocessableEntity)]
         public async Task<IActionResult> Get()
         {
-            var sellerClaim = this.User.Claims.FirstOrDefault(x => x.Type == AccountConstants.Claims.OrganisationIdClaim);
+            var sellerClaim = User.Claims.FirstOrDefault(x => x.Type == AccountConstants.Claims.OrganisationIdClaim);
 
             var serviceModel = new GetOrderStatusesServiceModel
             {
                 Language = CultureInfo.CurrentCulture.Name,
-                Username = this.User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Email)?.Value,
+                Username = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Email)?.Value,
                 OrganisationId = GuidHelper.ParseNullable(sellerClaim?.Value)
             };
 
-            var orders = await this.ordersService.GetOrderStatusesAsync(serviceModel);
+            var orders = await _ordersService.GetOrderStatusesAsync(serviceModel);
 
-            if (orders != null)
+            if (orders is not null)
             {
                 var response = orders.Select(x => new OrderStatusResponseModel 
                 { 
@@ -61,10 +58,10 @@ namespace Ordering.Api.v1.Controllers
                     CreatedDate = x.CreatedDate
                 });
 
-                return this.StatusCode((int)HttpStatusCode.OK, response);
+                return StatusCode((int)HttpStatusCode.OK, response);
             }
 
-            return this.StatusCode((int)HttpStatusCode.UnprocessableEntity);
+            return StatusCode((int)HttpStatusCode.UnprocessableEntity);
         }
     }
 }
