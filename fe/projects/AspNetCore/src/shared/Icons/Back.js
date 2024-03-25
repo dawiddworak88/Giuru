9a3db35ba9d1ae06973ce038e39ea419597832b7
Piotr Zaranek <piotrek.zaranek@gmail.com>
2024-03-25 07:38:52 +0100

    Added back icon

---
+++ fe/projects/AspNetCore/src/shared/Icons/Back.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const BackIcon = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
+            <path d="M17 9H1.00007" stroke="#171717" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M8.4668 16.5L1.00016 9L8.4668 1.5" stroke="#171717" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+    )
+}
+
+export default BackIcon;
