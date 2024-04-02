d314620482f19f074380b4a185b94a483ff7fcea
Piotr Zaranek <piotrek.zaranek@gmail.com>
2024-04-02 10:19:04 +0200

    Added constants for search areas

---
+++ be/src/Project/Web/Buyer/Buyer.Web/Shared/Definitions/Header/SearchConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace Buyer.Web.Shared.Definitions.Header
+{
+    public static class SearchConstants
+    {
+        public static class SearchArea
+        {
+            public static int All = 1;
+            public static int StockLevel = 2;
+        }
+    }
+}
