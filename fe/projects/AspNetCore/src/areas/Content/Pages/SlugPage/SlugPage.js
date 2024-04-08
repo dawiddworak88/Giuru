65dadcac51f092e4d899ab84b3e9542d954578fc
Sebastian Kasperek <sebastian.kasperek@eltap.com>
2024-04-08 13:49:27 +0200

    Fixed ssr problem

--- fe/projects/AspNetCore/src/areas/Content/Pages/SlugPage/SlugPage.js
+++ fe/projects/AspNetCore/src/areas/Content/Pages/SlugPage/SlugPage.js
@@ -3,13 +3,14 @@ import { ToastContainer } from "react-toastify";
 import { ThemeProvider } from "@mui/material/styles";
 import Store from "../../../../shared/stores/Store";
 import Header from "../../../../shared/components/Header/Header";
+import GlobalHelper from "../../../../shared/helpers/globals/GlobalHelper";
 import MainNavigation from "../../../../shared/components/MainNavigation/MainNavigation";
 import Footer from "../../../../shared/components/Footer/Footer";
 
 const SlugPage = (props) => {
     
     return (
-        <ThemeProvider>
+        <ThemeProvider theme={GlobalHelper.initMuiTheme(props.locale)}>
             <ToastContainer />
             <Store>
                 <Header {...props.header}/>
