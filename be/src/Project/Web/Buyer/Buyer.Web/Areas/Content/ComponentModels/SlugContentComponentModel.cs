92e2aabdba8daa4477ae873a972fb6a735eff76a
Sebastian Kasperek <sebastian.kasperek@eltap.com>
2024-04-08 12:00:44 +0200

    Added new component model

---
+++ be/src/Project/Web/Buyer/Buyer.Web/Areas/Content/ComponentModels/SlugContentComponentModel.cs
@@ -0,0 +1,9 @@
+﻿using Foundation.PageContent.ComponentModels;
+
+namespace Buyer.Web.Areas.Content.ComponentModels
+{
+    public class SlugContentComponentModel : ComponentModelBase
+    {
+        public string Slug { get; set; }
+    }
+}
