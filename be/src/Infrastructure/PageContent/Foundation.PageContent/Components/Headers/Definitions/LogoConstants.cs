5007f8049ba4b85127c8527f7bf3ec969b450695
Piotr Zaranek <piotrek.zaranek@gmail.com>
2024-04-10 10:03:05 +0200

    Revert "Removed unnecessary height property"
    This reverts commit 52698b4e3bc587814511f6cc5658b56984765fdd.

--- be/src/Infrastructure/PageContent/Foundation.PageContent/Components/Headers/Definitions/LogoConstants.cs
+++ be/src/Infrastructure/PageContent/Foundation.PageContent/Components/Headers/Definitions/LogoConstants.cs
@@ -4,7 +4,7 @@ namespace Foundation.PageContent.Components.Headers.Definitions
 {
     public static class LogoConstants
     {
-        public static readonly Guid LogoMediaId = Guid.Parse("54f48c59-5ebb-4d48-63c2-08db23358558");
+        public static readonly Guid LogoMediaId = Guid.Parse("85b14b09-856d-4fd0-8af4-7c077953b214");
         public static readonly Guid FaviconMediaId = Guid.Parse("0b8ed469-e84c-455b-b616-12a60d38da1c");
     }
 }
