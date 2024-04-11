500e06aed626fe04a231a78de3f3500bd33e9cb5
Piotr Zaranek <piotrek.zaranek@gmail.com>
2024-04-11 19:40:54 +0200

    Added migrations

---
+++ be/src/Project/Services/Client/Client.Api/Infrastructure/Migrations/20240411172250_AddedAddressesToClientsApplication.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Client.Api.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedAddressesToClientsApplication : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "BillingAddressId",
+                table: "ClientsApplications",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "DeliveryAddressId",
+                table: "ClientsApplications",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeliveryAddressEqualBillingAddress",
+                table: "ClientsApplications",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "ClientsApplicationAddresses",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Street = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Region = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PostalCode = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Country = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    RowVersion = table.Column<byte[]>(type: "rowversion", rowVersion: true, nullable: true),
+                    LastModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClientsApplicationAddresses", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ClientsApplicationAddresses");
+
+            migrationBuilder.DropColumn(
+                name: "BillingAddressId",
+                table: "ClientsApplications");
+
+            migrationBuilder.DropColumn(
+                name: "DeliveryAddressId",
+                table: "ClientsApplications");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeliveryAddressEqualBillingAddress",
+                table: "ClientsApplications");
+        }
+    }
+}
