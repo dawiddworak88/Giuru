4e04cf3bf851d82c10e04647c3809236a5e47a7e
Sebastian Kasperek <sebastian.kasperek@eltap.com>
2024-04-12 10:52:31 +0200

    Added graphql response model

---
+++ be/src/Project/Web/Buyer/Buyer.Web/Areas/Content/GraphQlResponseModels/SlugPageGraphQlResponseModel.cs
@@ -0,0 +1,69 @@
+﻿using Buyer.Web.Areas.Content.Repositories;
+using Foundation.PageContent.ResponseModels.Seo;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Buyer.Web.Areas.Content.GraphQlResponseModels
+{
+    public class SlugPageGraphQlResponseModel
+    {
+        [JsonProperty("landingPages")]
+        public LandingPagec Test { get; set; }
+    }
+
+    public class LandingPagec
+    {
+        [JsonProperty("data")]
+        public IEnumerable<Page> Data { get; set; }
+    }
+
+    public class Page
+    {
+        [JsonProperty("attributes")]
+        public LandingPageAttributes Attributes { get; set; }
+    }
+
+    public class LandingPageAttributes
+    {
+        [JsonProperty("slug")]
+        public string Slug { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("seo")]
+        public Seo Seo { get; set; }
+
+        [JsonConverter(typeof(BlockConverter))]
+        public IEnumerable<Block> Blocks { get; set; }
+    }
+
+    public class Block
+    {
+        [JsonProperty("__typename")]
+        public string TypeName { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+    };
+
+    public class ComponentSharedSlider : Block
+    {
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("navigation")]
+        public bool HasNavigation { get; set; }
+
+        [JsonProperty("skus")]
+        public string Skus { get; set; }
+    }
+
+    public class ComponentSharedContent : Block
+    {
+        public IEnumerable<JObject> Content { get; set; }
+    }
+}
