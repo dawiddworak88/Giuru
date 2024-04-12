c4d9e9dc0a8b65f70a1458d0a81ea5c4d76a8af2
Sebastian Kasperek <sebastian.kasperek@eltap.com>
2024-04-12 10:51:22 +0200

    Added domain models

---
+++ be/src/Project/Web/Buyer/Buyer.Web/Areas/Content/DomainModel/ContentBlockPage.cs
@@ -0,0 +1,7 @@
+﻿namespace Buyer.Web.Areas.Content.DomainModel
+{
+    public class ContentBlockPage : BlockPage
+    {
+        public string Content { get; set; }
+    }
+}
---
+++ be/src/Project/Web/Buyer/Buyer.Web/Areas/Content/DomainModel/Slug.cs
@@ -0,0 +1,12 @@
+﻿using Foundation.PageContent.ResponseModels.Seo;
+using System.Collections.Generic;
+
+namespace Buyer.Web.Areas.Content.DomainModel
+{
+    public class Slug
+    {
+        public string Title { get; set; }
+        public Seo Seo { get; set; }
+        public IEnumerable<BlockPage> Blocks { get; set; }
+    }
+}
