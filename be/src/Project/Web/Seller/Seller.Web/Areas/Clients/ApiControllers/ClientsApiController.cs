3f6e8b6314e7135b0bbce0d4699b6ffae6d39a8e
Piotr Zaranek <piotrek.zaranek@gmail.com>
2024-04-23 10:39:05 +0200

    Improved condition

--- be/src/Project/Web/Seller/Seller.Web/Areas/Clients/ApiControllers/ClientsApiController.cs
+++ be/src/Project/Web/Seller/Seller.Web/Areas/Clients/ApiControllers/ClientsApiController.cs
@@ -86,11 +86,11 @@ namespace Seller.Web.Areas.Clients.ApiControllers
 
             var clientId = await _clientsRepository.SaveAsync(token, language, model.Id, model.Name, model.Email, model.CommunicationLanguage, model.CountryId, model.PreferedCurrencyId, model.PhoneNumber, model.IsDisabled, organisationId.Value, model.ClientGroupIds, model.ClientManagerIds, model.DefaultDeliveryAddressId, model.DefaultBillingAddressId);
 
-            if (model.ClientApprovalIds is not null)
+            if (model.ClientApprovalIds is not null && model.ClientApprovalIds.Any())
             {
                 await _clientNotificationTypeApprovalRepository.SaveAsync(token, language, model.Id, model.ClientApprovalIds);
             }
-            
+
             if (model.FieldsValues is not null && model.FieldsValues.Any())
             {
                 await _clientFieldValuesRepository.SaveAsync(token, language, clientId,
