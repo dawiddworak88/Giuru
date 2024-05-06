de9b14b4613f6db7f776d6504afbfda3176c14f7
Piotr Zaranek <piotrek.zaranek@gmail.com>
2024-05-06 13:51:42 +0200

    Added constants for product fabric

---
+++ be/src/Project/Web/Buyer/Buyer.Web/Areas/Products/Definitions/ProductFabricsConstants.cs
@@ -0,0 +1,47 @@
+﻿using System.Globalization;
+
+namespace Buyer.Web.Areas.Products.Definitions
+{
+    public static class ProductFabricsConstants
+    {
+        public static string GetPrimaryFabircKey(string language)
+        {
+            string key;
+
+            if (language == "pl")
+            {
+                key = "tkaninaPodstawowa";
+            }
+            else if (language == "en")
+            {
+                key = "primaryFabrics";
+            }
+            else
+            {
+                key = "primarstoffe";
+            }
+
+            return key;
+        }
+
+        public static string GetSecondaryFabircKey(string language)
+        {
+            string key;
+
+            if (language == "pl")
+            {
+                key = "tkaninaDodatkowa";
+            }
+            else if (language == "en")
+            {
+                key = "secondaryFabrics";
+            }
+            else
+            {
+                key = "sekundarefarbe";
+            }
+
+            return key;
+        }
+    }
+}
