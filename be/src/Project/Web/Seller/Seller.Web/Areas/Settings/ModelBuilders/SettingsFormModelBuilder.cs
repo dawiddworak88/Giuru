416b2e4701255b51e8f97a4109ec7de46582ab23
Piotr Zaranek <piotrek.zaranek@gmail.com>
2024-06-06 15:13:37 +0200

    Added properties for settings

--- be/src/Project/Web/Seller/Seller.Web/Areas/Settings/ModelBuilders/SettingsFormModelBuilder.cs
+++ be/src/Project/Web/Seller/Seller.Web/Areas/Settings/ModelBuilders/SettingsFormModelBuilder.cs
@@ -3,6 +3,7 @@ using Foundation.Localization;
 using Foundation.PageContent.ComponentModels;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Localization;
+using Seller.Web.Areas.Settings.Repositories;
 using Seller.Web.Areas.Settings.ViewModels;
 using System.Globalization;
 using System.Threading.Tasks;
@@ -11,27 +12,34 @@ namespace Seller.Web.Areas.Settings.ModelBuilders
 {
     public class SettingsFormModelBuilder : IAsyncComponentModelBuilder<ComponentModelBase, SettingsFormViewModel>
     {
-        private readonly IStringLocalizer globalLocalizer;
-        private readonly LinkGenerator linkGenerator;
+        private readonly IStringLocalizer _globalLocalizer;
+        private readonly LinkGenerator _linkGenerator;
+        private readonly ISettingRepository _settingRepository;
 
         public SettingsFormModelBuilder(
             IStringLocalizer<GlobalResources> globalLocalizer,
-            LinkGenerator linkGenerator)
+            LinkGenerator linkGenerator,
+            ISettingRepository settingRepository)
         {
-            this.globalLocalizer = globalLocalizer;
-            this.linkGenerator = linkGenerator;
+            _globalLocalizer = globalLocalizer;
+            _linkGenerator = linkGenerator;
+            _settingRepository = settingRepository;
         }
 
         public async Task<SettingsFormViewModel> BuildModelAsync(ComponentModelBase componentModel)
         {
             var viewModel = new SettingsFormViewModel
             {
-                Title = this.globalLocalizer.GetString("Settings"),
-                GeneralErrorMessage = this.globalLocalizer.GetString("AnErrorOccurred"),
-                ProductsIndexTriggerUrl = this.linkGenerator.GetPathByAction("Index", "ProductsIndexTriggerApi", new { Area = "Products", culture = CultureInfo.CurrentUICulture.Name }),
-                ReindexProductsText = this.globalLocalizer.GetString("ReindexProducts")
+                Title = _globalLocalizer.GetString("Settings"),
+                GeneralErrorMessage = _globalLocalizer.GetString("AnErrorOccurred"),
+                ProductsIndexTriggerUrl = _linkGenerator.GetPathByAction("Index", "ProductsIndexTriggerApi", new { Area = "Products", culture = CultureInfo.CurrentUICulture.Name }),
+                ReindexProductsText = _globalLocalizer.GetString("ReindexProducts"),
+                Settings = await _settingRepository.GetAsync(componentModel.Token, componentModel.Language, componentModel.SellerId),
+                SaveText = _globalLocalizer.GetString("SaveText"),
+                ExternalCompletionDatesText = _globalLocalizer.GetString("ExternalCompletionDatesText"),
+                SaveUrl = _linkGenerator.GetPathByAction("Index", "SettingsApi", new { Area = "Settings", culture = CultureInfo.CurrentCulture.Name })
             };
-            
+
             return viewModel;
         }
     }
--- be/src/Project/Web/Seller/Seller.Web/Areas/Settings/ViewModels/SettingsFormViewModel.cs
+++ be/src/Project/Web/Seller/Seller.Web/Areas/Settings/ViewModels/SettingsFormViewModel.cs
@@ -1,4 +1,6 @@
-﻿namespace Seller.Web.Areas.Settings.ViewModels
+﻿using System.Collections.Generic;
+
+namespace Seller.Web.Areas.Settings.ViewModels
 {
     public class SettingsFormViewModel 
     { 
@@ -6,5 +8,9 @@
         public string ReindexProductsText { get; set; }
         public string GeneralErrorMessage { get; set; }
         public string ProductsIndexTriggerUrl { get; set; }
+        public string SaveUrl { get; set; }
+        public string SaveText { get; set; }
+        public string ExternalCompletionDatesText { get; set; }
+        public Dictionary<string, string> Settings { get; set; }
     }
 }
