54d61b9e98d50f1be142095880c967cd5961d7e7
Piotr Zaranek <piotrek.zaranek@gmail.com>
2024-06-06 15:11:28 +0200

    Added repository, request model, domain model for settings

---
+++ be/src/Project/Web/Seller/Seller.Web/Areas/Settings/ApiRequestModels/SaveSettingsRequestModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Seller.Web.Areas.Settings.ApiRequestModels
+{
+    public class SaveSettingsRequestModel
+    {
+        public Guid? SellerId { get; set; }
+        public Dictionary<string, string> Settings { get; set; }
+    }
+}
---
+++ be/src/Project/Web/Seller/Seller.Web/Areas/Settings/Repositories/ISettingRepository.cs
@@ -0,0 +1,13 @@
+﻿using Seller.Web.Areas.Settings.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Seller.Web.Areas.Settings.Repositories
+{
+    public interface ISettingRepository
+    {
+        Task<Dictionary<string, string>> GetAsync(string token, string language, Guid? sellerId);
+        Task PostAsync(string token, string language, Guid? sellerId, Dictionary<string, string> settings);
+    }
+}
