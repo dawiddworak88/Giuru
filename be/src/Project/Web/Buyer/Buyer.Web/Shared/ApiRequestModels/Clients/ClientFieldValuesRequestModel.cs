4a4277997147cce5b5f2d5b70376940e1a421dec
Piotr Zaranek <piotrek.zaranek@gmail.com>
2024-06-13 15:37:48 +0200

    Added PagedClientFieldValuesRequestModel

---
+++ be/src/Project/Web/Buyer/Buyer.Web/Shared/ApiRequestModels/Clients/ClientFieldValuesRequestModel.cs
@@ -0,0 +1,10 @@
+﻿using Foundation.Extensions.Models;
+using System;
+
+namespace Buyer.Web.Shared.ApiRequestModels.Clients
+{
+    public class PagedClientFieldValuesRequestModel : PagedBaseServiceModel
+    {
+        public Guid? ClientId { get; set; }
+    }
+}
