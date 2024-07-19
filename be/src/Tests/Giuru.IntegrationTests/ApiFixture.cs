ba2e72036825b852ea90c3e6799f9a6dbd42618a
Sebastian Kasperek <sebastian.kasperek@eltap.com>
2024-07-19 14:59:35 +0200

    Fixed problem with building docker image

--- be/src/Tests/Giuru.IntegrationTests/ApiFixture.cs
+++ be/src/Tests/Giuru.IntegrationTests/ApiFixture.cs
@@ -1,10 +1,11 @@
 using DotNet.Testcontainers.Builders;
 using DotNet.Testcontainers.Containers;
 using DotNet.Testcontainers.Networks;
-using Microsoft.AspNetCore.Mvc.Testing;
+using Giuru.IntegrationTests.Images;
 using System.Threading.Tasks;
 using Testcontainers.Elasticsearch;
 using Testcontainers.MsSql;
+using Testcontainers.RabbitMq;
 using Testcontainers.Redis;
 
 namespace Giuru.IntegrationTests
@@ -13,8 +14,10 @@ namespace Giuru.IntegrationTests
     {
         private INetwork _giuruNetwork;
         private RedisContainer _redisContainer;
+        private RabbitMqContainer _rabbitMqContainer;
         private MsSqlContainer _msSqlContainer;
         private ElasticsearchContainer _elasticsearchContainer;
+        private IContainer _catalogContainer;
         private IContainer _sellerWebContainer;
 
         public async Task InitializeAsync()
@@ -28,9 +31,12 @@ namespace Giuru.IntegrationTests
                 .WithExposedPort(9113)
                 .Build();
 
+            await _redisContainer.StartAsync();
+
             _msSqlContainer = new MsSqlBuilder()
                 .WithName("mssql")
                 .WithNetwork(_giuruNetwork)
+                //.WithNetworkAliases("sqldata")
                 .WithPassword("YourStrongPassword!")
                 .WithPortBinding(9111, 1433)
                 .WithExposedPort(9111)
@@ -47,6 +53,38 @@ namespace Giuru.IntegrationTests
                 .Build();
 
             await _elasticsearchContainer.StartAsync();
+
+            _rabbitMqContainer = new RabbitMqBuilder()
+                .WithName("rabbitmq")
+                .WithNetwork(_giuruNetwork)
+                .Build();
+
+            await _rabbitMqContainer.StartAsync();
+
+            var catalogImage = new CatalogApiImage();
+
+            await catalogImage.InitializeAsync();
+
+            _catalogContainer = new ContainerBuilder()
+                .WithName("catalog-api")
+                .WithImage(catalogImage)
+                .WithNetwork(_giuruNetwork)
+                .WithExposedPort(9101)
+                .WithPortBinding(9101, 8080)
+                .WithEnvironment("ASPNETCORE_ENVIRONMENT", "Development")
+                .WithEnvironment("RedisUrl", $"host.docker.internal:9113,abortConnect=false")
+                .WithEnvironment("ConnectionString", $"Server=host.docker.internal,9111;Database=CatalogDb;User Id=sa;Password=YourStrongPassword!;TrustServerCertificate=True")
+                .WithEnvironment("ElasticsearchUrl", _elasticsearchContainer.GetConnectionString())
+                .WithEnvironment("ElasticsearchIndex", "catalog")
+                .WithEnvironment("EventBusConnection", _rabbitMqContainer.GetConnectionString())
+                .WithEnvironment("EventBusRetryCount", "5")
+                .WithEnvironment("EventBusRequestedHeartbeat", "60")
+                .WithEnvironment("SupportedCultures", "de,en,pl")
+                .WithEnvironment("DefaultCulture", "en")
+                .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(8080))
+                .Build();
+
+            await _catalogContainer.StartAsync();
 
             /*var sellerWebFactory = new WebApplicationFactory<Program>()
                 .WithWebHostBuilder(builder =>
@@ -70,6 +108,12 @@ namespace Giuru.IntegrationTests
 
             await _elasticsearchContainer.StopAsync();
             await _elasticsearchContainer.DisposeAsync();
+
+            await _catalogContainer.StopAsync();
+            await _catalogContainer.DisposeAsync();
+
+            await _rabbitMqContainer.StopAsync();
+            await _rabbitMqContainer.DisposeAsync();
 
             await _giuruNetwork.DisposeAsync();
         }
--- be/src/Tests/Giuru.IntegrationTests/ExTest.cs
+++ be/src/Tests/Giuru.IntegrationTests/ExTest.cs
@@ -5,6 +5,13 @@ namespace Giuru.IntegrationTests
     [Collection(nameof(ApiCollection))]
     public class ExTest
     {
+        private readonly ApiFixture _apiFixture;
+
+        public ExTest(ApiFixture apiFixture)
+        {
+            _apiFixture = apiFixture;
+        }
+
         [Fact]
         public async Task Test()
         {
