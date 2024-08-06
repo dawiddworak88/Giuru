fea47e197ca9d965edfbc91892c4a961bda4a9c6
Piotr Zaranek <piotrek.zaranek@gmail.com>
2024-08-06 13:12:01 +0200

    Changed this. to _

--- be/src/Project/Web/Buyer/Buyer.Web/Areas/Products/Controllers/CategoryController.cs
+++ be/src/Project/Web/Buyer/Buyer.Web/Areas/Products/Controllers/CategoryController.cs
@@ -1,16 +1,13 @@
 ﻿using Buyer.Web.Areas.Products.ComponentModels;
 using Buyer.Web.Areas.Products.ViewModels.Categories;
 using Buyer.Web.Shared.Definitions.Basket;
-using Foundation.Account.Definitions;
 using Foundation.ApiExtensions.Definitions;
 using Foundation.Extensions.Controllers;
-using Foundation.Extensions.Helpers;
 using Foundation.Extensions.ModelBuilders;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Globalization;
-using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
@@ -19,11 +16,11 @@ namespace Buyer.Web.Areas.Products.Controllers
     [Area("Products")]
     public class CategoryController : BaseController
     {
-        private readonly IAsyncComponentModelBuilder<SearchProductsComponentModel, CategoryPageViewModel> categoryPageModelBuilder;
+        private readonly IAsyncComponentModelBuilder<SearchProductsComponentModel, CategoryPageViewModel> _categoryPageModelBuilder;
 
         public CategoryController(IAsyncComponentModelBuilder<SearchProductsComponentModel, CategoryPageViewModel> categoryPageModelBuilder)
         {
-            this.categoryPageModelBuilder = categoryPageModelBuilder;
+            _categoryPageModelBuilder = categoryPageModelBuilder;
         }
 
         public async Task<IActionResult> Index(Guid? id, string searchTerm)
@@ -33,17 +30,17 @@ namespace Buyer.Web.Areas.Products.Controllers
                 Id = id,
                 SearchTerm = searchTerm,
                 Language = CultureInfo.CurrentUICulture.Name,
-                IsAuthenticated = this.User.Identity.IsAuthenticated,
-                Name = this.User.Identity.Name,
+                IsAuthenticated = User.Identity.IsAuthenticated,
+                Name = User.Identity.Name,
                 Token = await HttpContext.GetTokenAsync(ApiExtensionsConstants.TokenName),
-                BasketId = string.IsNullOrWhiteSpace(this.Request.Cookies[BasketConstants.BasketCookieName]) ? null : Guid.Parse(this.Request.Cookies[BasketConstants.BasketCookieName]),
+                BasketId = string.IsNullOrWhiteSpace(Request.Cookies[BasketConstants.BasketCookieName]) ? null : Guid.Parse(Request.Cookies[BasketConstants.BasketCookieName]),
                 ContentPageKey = "categoryPage",
-                SellerId = GuidHelper.ParseNullable((User.Identity as ClaimsIdentity).Claims.FirstOrDefault(x => x.Type == AccountConstants.Claims.OrganisationIdClaim)?.Value)
+                UserEmail = User.FindFirstValue(ClaimTypes.Email)
             };
 
-            var viewModel = await this.categoryPageModelBuilder.BuildModelAsync(componentModel);
+            var viewModel = await _categoryPageModelBuilder.BuildModelAsync(componentModel);
 
-            return this.View(viewModel);
+            return View(viewModel);
         }
     }
 }
--- be/src/Project/Web/Buyer/Buyer.Web/Areas/Products/ModelBuilders/Categories/CategoryCatalogModelBuilder.cs
+++ be/src/Project/Web/Buyer/Buyer.Web/Areas/Products/ModelBuilders/Categories/CategoryCatalogModelBuilder.cs
@@ -16,11 +16,11 @@ namespace Buyer.Web.Areas.Products.ModelBuilders.Categories
 {
     public class CategoryCatalogModelBuilder : IAsyncComponentModelBuilder<SearchProductsComponentModel, CategoryCatalogViewModel>
     {
-        private readonly ICatalogModelBuilder<SearchProductsComponentModel, CategoryCatalogViewModel> catalogModelBuilder;
-        private readonly IAsyncComponentModelBuilder<ComponentModelBase, SidebarViewModel> sidebarModelBuilder;
-        private readonly IAsyncComponentModelBuilder<ComponentModelBase, ModalViewModel> modalModelBuilder;
-        private readonly IProductsService productsService;
-        private readonly ICategoryRepository categoryRepository;
+        private readonly ICatalogModelBuilder<SearchProductsComponentModel, CategoryCatalogViewModel> _catalogModelBuilder;
+        private readonly IAsyncComponentModelBuilder<ComponentModelBase, SidebarViewModel> _sidebarModelBuilder;
+        private readonly IAsyncComponentModelBuilder<ComponentModelBase, ModalViewModel> _modalModelBuilder;
+        private readonly IProductsService _productsService;
+        private readonly ICategoryRepository _categoryRepository;
 
         public CategoryCatalogModelBuilder(
             ICatalogModelBuilder<SearchProductsComponentModel, CategoryCatalogViewModel> catalogModelBuilder,
@@ -29,30 +29,31 @@ namespace Buyer.Web.Areas.Products.ModelBuilders.Categories
             IProductsService productsService,
             ICategoryRepository categoryRepository)
         {
-            this.catalogModelBuilder = catalogModelBuilder;
-            this.sidebarModelBuilder = sidebarModelBuilder;
-            this.productsService = productsService;
-            this.categoryRepository = categoryRepository;
-            this.modalModelBuilder = modalModelBuilder;
+            _catalogModelBuilder = catalogModelBuilder;
+            _sidebarModelBuilder = sidebarModelBuilder;
+            _productsService = productsService;
+            _categoryRepository = categoryRepository;
+            _modalModelBuilder = modalModelBuilder;
         }
 
         public async Task<CategoryCatalogViewModel> BuildModelAsync(SearchProductsComponentModel componentModel)
         {
-            var viewModel = this.catalogModelBuilder.BuildModel(componentModel);
+            var viewModel = _catalogModelBuilder.BuildModel(componentModel);
 
-            var category = await this.categoryRepository.GetCategoryAsync(componentModel.Id, componentModel.Token, componentModel.Language);
+            var category = await _categoryRepository.GetCategoryAsync(componentModel.Id, componentModel.Token, componentModel.Language);
 
             if (category != null)
             {
                 viewModel.Title = category.Name;
                 viewModel.CategoryId = category.Id;
-                viewModel.Sidebar = await this.sidebarModelBuilder.BuildModelAsync(componentModel);
-                viewModel.Modal = await this.modalModelBuilder.BuildModelAsync(componentModel);
+                viewModel.Sidebar = await _sidebarModelBuilder.BuildModelAsync(componentModel);
+                viewModel.Modal = await _modalModelBuilder.BuildModelAsync(componentModel);
                 viewModel.OrderBy = nameof(Product.Name);
-                viewModel.PagedItems = await this.productsService.GetProductsAsync(
+                viewModel.PagedItems = await _productsService.GetProductsAsync(
                     null,
                     componentModel.Id,
                     componentModel.SellerId,
+                    componentModel.UserEmail,
                     componentModel.Language,
                     componentModel.SearchTerm,
                     false,
