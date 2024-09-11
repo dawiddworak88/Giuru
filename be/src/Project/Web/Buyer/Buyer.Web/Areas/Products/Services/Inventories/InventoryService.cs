f3e272ce112b28f9a225fa4a3de6f554b42f98bf
Piotr Zaranek <piotrek.zaranek@gmail.com>
2024-09-11 08:51:18 +0200

    Added getting productAttrubutes

--- be/src/Project/Web/Buyer/Buyer.Web/Areas/Products/Services/Inventories/InventoryService.cs
+++ be/src/Project/Web/Buyer/Buyer.Web/Areas/Products/Services/Inventories/InventoryService.cs
@@ -1,5 +1,8 @@
 ﻿using Buyer.Web.Areas.Products.Repositories.Inventories;
+using Buyer.Web.Areas.Products.Services.Products;
 using Buyer.Web.Areas.Products.ViewModels.Products;
+using Buyer.Web.Areas.Shared.Definitions.Products;
+using Foundation.GenericRepository.Paginations;
 using Microsoft.AspNetCore.Routing;
 using System.Collections.Generic;
 using System.Globalization;
@@ -12,20 +15,37 @@ namespace Buyer.Web.Areas.Products.Services.Inventories
     {
         private readonly IInventoryRepository _inventoryRepository;
         private readonly LinkGenerator _linkGenerator;
+        private readonly IProductsService _productsService;
 
-        public InventoryService(IInventoryRepository inventoryRepository, LinkGenerator linkGenerator) 
+        public InventoryService(
+            IInventoryRepository inventoryRepository,
+            LinkGenerator linkGenerator,
+            IProductsService productsService) 
         {
             _inventoryRepository = inventoryRepository;
             _linkGenerator = linkGenerator;
+            _productsService = productsService;
         }
 
         public async Task<IEnumerable<ProductSuggestionViewModel>> GetInventoryProductSuggestionsAsync(string searchTerm, int size, string language, string token)
         {
             var suggestions = await _inventoryRepository.GetAvailableInventoryProductsSuggestions(token, language, searchTerm, size);
 
+            var products = await _productsService.GetProductsAsync(
+                suggestions.Select(x => x.Id),
+                null,
+                null,
+                language,
+                null,
+                true,
+                PaginationConstants.DefaultPageIndex,
+                ProductConstants.ProductsCatalogPaginationPageSize,
+                token);
+
             return suggestions.Select(x => new ProductSuggestionViewModel
             {
                 Name = x.Name,
+                Attributes = products?.Data?.FirstOrDefault(y => y.Id == x.Id)?.ProductAttributes,
                 Sku = x.Sku,
                 Url = _linkGenerator.GetPathByAction("Index", "Product", new { Area = "Products", culture = CultureInfo.CurrentUICulture.Name, x.Id }),
             });
