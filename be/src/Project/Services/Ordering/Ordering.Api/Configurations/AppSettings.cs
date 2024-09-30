8564aca8c44e913163f414d774e55691415e5588
Piotr Zaranek <piotrek.zaranek@gmail.com>
2024-09-30 15:52:53 +0200

    Added ActionSendGridCancleOrderTemplateId

--- be/src/Project/Services/Ordering/Ordering.Api/Configurations/AppSettings.cs
+++ be/src/Project/Services/Ordering/Ordering.Api/Configurations/AppSettings.cs
@@ -6,6 +6,7 @@
         public string IdentityUrl { get; set; }
         public string ActionSendGridCustomOrderTemplateId { get; set; }
         public string ActionSendGridConfirmationOrderTemplateId { get; set; }
+        public string ActionSendGridCancleOrderTemplateId { get; set; }
         public string SenderEmail { get; set; }
         public string SenderName { get; set; }
     }
