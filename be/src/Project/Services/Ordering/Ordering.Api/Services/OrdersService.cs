68269f40ccea9dec1da07db1a11f8543694e8237
Piotr Zaranek <piotrek.zaranek@gmail.com>
2024-10-02 11:08:54 +0200

    Set testing values

--- be/src/Project/Services/Ordering/Ordering.Api/Services/OrdersService.cs
+++ be/src/Project/Services/Ordering/Ordering.Api/Services/OrdersService.cs
@@ -614,8 +614,8 @@ namespace Ordering.Api.Services
 
                 await _mailingService.SendTemplateAsync(new TemplateEmail
                 {
-                    RecipientEmailAddress = _configuration.Value.SenderEmail,
-                    RecipientName = _configuration.Value.SenderName,
+                    RecipientEmailAddress = "piotr.zaranek@eltap.com", //_configuration.Value.SenderEmail,
+                    RecipientName = "Test", //_configuration.Value.SenderName,
                     SenderEmailAddress = _configuration.Value.SenderEmail,
                     SenderName = _configuration.Value.SenderName,
                     TemplateId = _configuration.Value.ActionSendGridCancelOrderTemplateId,
