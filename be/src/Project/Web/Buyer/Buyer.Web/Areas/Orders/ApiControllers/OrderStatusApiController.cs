78de266e742438f2a2aaa2a4c6c6e3d9fb1a70e5
Piotr Zaranek <piotrek.zaranek@gmail.com>
2024-10-08 07:54:17 +0200

    Changed this. to _ and added checking order item status

--- be/src/Project/Web/Buyer/Buyer.Web/Areas/Orders/ApiControllers/OrderStatusApiController.cs
+++ be/src/Project/Web/Buyer/Buyer.Web/Areas/Orders/ApiControllers/OrderStatusApiController.cs
@@ -8,6 +8,8 @@ using Foundation.Localization;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Localization;
+using Newtonsoft.Json;
+using System;
 using System.Globalization;
 using System.Net;
 using System.Threading.Tasks;
@@ -17,44 +19,49 @@ namespace Buyer.Web.Areas.Orders.ApiControllers
     [Area("Orders")]
     public class OrderStatusApiController : BaseApiController
     {
-        private readonly IOrdersRepository ordersRepository;
-        private readonly IStringLocalizer<OrderResources> orderLocalizer;
+        private readonly IOrdersRepository _ordersRepository;
+        private readonly IStringLocalizer<OrderResources> _orderLocalizer;
 
         public OrderStatusApiController(
             IOrdersRepository ordersRepository,
             IStringLocalizer<OrderResources> orderLocalizer)
         {
-            this.ordersRepository = ordersRepository;
-            this.orderLocalizer = orderLocalizer;
+            _ordersRepository = ordersRepository;
+            _orderLocalizer = orderLocalizer;
         }
 
         [HttpPost]
         public async Task<IActionResult> Cancel([FromBody] UpdateOrderStatusRequestModel model)
         {
-            if (!model.OrderStatusId.Equals(OrdersConstants.OrderStatuses.NewId))
+            if (model.OrderStatusId.Equals(OrdersConstants.OrderStatuses.NewId) is false)
             {
-                throw new CustomException(this.orderLocalizer.GetString("CancellationOrderError"), (int)HttpStatusCode.BadRequest);
+                throw new CustomException(_orderLocalizer.GetString("CancellationOrderError"), (int)HttpStatusCode.BadRequest);
             }
 
             var token = await HttpContext.GetTokenAsync(ApiExtensionsConstants.TokenName);
             var language = CultureInfo.CurrentUICulture.Name;
 
-            var orderStatusId = await this.ordersRepository.SaveOrderStatusAsync(token, language, model.OrderId, OrdersConstants.OrderStatuses.CancelId);
+            var orderStatusId = await _ordersRepository.SaveOrderStatusAsync(token, language, model.OrderId, OrdersConstants.OrderStatuses.CancelId);
 
-            return this.StatusCode((int)HttpStatusCode.OK, new { OrderStatusId = orderStatusId, Message = this.orderLocalizer.GetString("SuccessfullyCanceledOrder").Value });
+            return StatusCode((int)HttpStatusCode.OK, new { OrderStatusId = orderStatusId, Message = _orderLocalizer.GetString("SuccessfullyCanceledOrder").Value });
         }
 
         [HttpPost]
         public async Task<IActionResult> CancelOrderItem([FromBody] UpdateOrderItemStatusRequestModel request)
         {
+            if (request.OrderItemStatusId.Equals(OrdersConstants.OrderStatuses.NewId) is false)
+            {
+                throw new CustomException(_orderLocalizer.GetString("CancellationOrderError"), (int)HttpStatusCode.BadRequest);
+            }
+
             var token = await HttpContext.GetTokenAsync(ApiExtensionsConstants.TokenName);
             var language = CultureInfo.CurrentUICulture.Name;
 
-            await this.ordersRepository.UpdateOrderItemStatusAsync(token, language, request.Id, OrdersConstants.OrderStatuses.CancelId);
+            await _ordersRepository.UpdateOrderItemStatusAsync(token, language, request.Id, OrdersConstants.OrderStatuses.CancelId);
 
-            var orderItemStatusChanges = await this.ordersRepository.GetOrderItemStatusesAsync(token, language, request.Id);
+            var orderItemStatusChanges = await _ordersRepository.GetOrderItemStatusesAsync(token, language, request.Id);
 
-            return this.StatusCode((int)HttpStatusCode.OK, new { StatusChanges = orderItemStatusChanges.StatusChanges, Message = this.orderLocalizer.GetString("SuccessfullyCanceledOrder").Value });
+            return StatusCode((int)HttpStatusCode.OK, new { StatusChanges = orderItemStatusChanges.StatusChanges, Message = _orderLocalizer.GetString("SuccessfullyCanceledOrder").Value });
         }
     }
 }
