8e465b578a240eeb85d968864fc52c83797662e1
Piotr Zaranek <piotrek.zaranek@gmail.com>
2024-10-09 12:08:13 +0200

    Removed testing values

--- be/src/Project/Services/Ordering/Ordering.Api/Services/OrdersService.cs
+++ be/src/Project/Services/Ordering/Ordering.Api/Services/OrdersService.cs
@@ -615,8 +615,8 @@ namespace Ordering.Api.Services
             {
                 await _mailingService.SendTemplateAsync(new TemplateEmail
                 {
-                    RecipientEmailAddress = "piotr.zaranek@eltap.com", //_configuration.Value.SenderEmail,
-                    RecipientName = "Piotr", //_configuration.Value.SenderName,
+                    RecipientEmailAddress = _configuration.Value.SenderEmail,
+                    RecipientName = _configuration.Value.SenderName,
                     SenderEmailAddress = _configuration.Value.SenderEmail,
                     SenderName = _configuration.Value.SenderName,
                     TemplateId = _configuration.Value.ActionSendGridCancelOrderTemplateId,
@@ -802,8 +802,8 @@ namespace Ordering.Api.Services
 
                 await _mailingService.SendTemplateAsync(new TemplateEmail
                 {
-                    RecipientEmailAddress = "piotr.zaranek@eltap.com", //_configuration.Value.SenderEmail,
-                    RecipientName = "Piotr", //_configuration.Value.SenderName,
+                    RecipientEmailAddress = _configuration.Value.SenderEmail,
+                    RecipientName = _configuration.Value.SenderName,
                     SenderEmailAddress = _configuration.Value.SenderEmail,
                     SenderName = _configuration.Value.SenderName,
                     TemplateId = _configuration.Value.ActionSendGridCancelOrderItemTemplateId,
