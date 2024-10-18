66d34d7b6389273ebb7e9a8d86a9900ca49f6d85
Piotr Zaranek <piotrek.zaranek@gmail.com>
2024-10-18 16:31:42 +0200

    Created service models for approvals

---
+++ be/src/Project/Services/Identity/Identity.Api/ServicesModels/Approvals/CreateApprovalServiceModel.cs
@@ -0,0 +1,9 @@
+﻿using Foundation.Extensions.Models;
+
+namespace Identity.Api.ServicesModels.Approvals
+{
+    public class CreateApprovalServiceModel : BaseServiceModel
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ be/src/Project/Services/Identity/Identity.Api/ServicesModels/Approvals/GetApprovalServiceModel.cs
@@ -0,0 +1,10 @@
+﻿using Foundation.Extensions.Models;
+using System;
+
+namespace Identity.Api.ServicesModels.Approvals
+{
+    public class GetApprovalServiceModel : BaseServiceModel
+    {
+        public Guid? Id { get; set; }
+    }
+}
