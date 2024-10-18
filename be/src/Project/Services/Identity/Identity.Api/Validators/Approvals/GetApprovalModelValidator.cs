ae392e4cc9a08c659e2c6caf6107ed65e5b1fa7f
Piotr Zaranek <piotrek.zaranek@gmail.com>
2024-10-18 16:33:19 +0200

    Created model validators for approvals

---
+++ be/src/Project/Services/Identity/Identity.Api/Validators/Approvals/GetApprovalModelValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Foundation.Extensions.Validators;
+using Identity.Api.ServicesModels.Approvals;
+
+namespace Identity.Api.Validators.Approvals
+{
+    public class GetApprovalModelValidator : BaseServiceModelValidator<GetApprovalServiceModel>
+    {
+        public GetApprovalModelValidator()
+        {
+            RuleFor(x => x.Id).NotNull().NotEmpty();
+        }
+    }
+}
