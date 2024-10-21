46c0b57d6eb7b6410b027fc8c4d33b74adb282c8
Piotr Zaranek <piotrek.zaranek@gmail.com>
2024-10-21 12:29:52 +0200

    Removed client apporval implementation in Seller.Web

--- be/src/Project/Web/Seller/Seller.Web/Areas/Clients/ViewModels/ClientNotificationTypePageViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using Seller.Web.Shared.ViewModels;
-
-namespace Seller.Web.Areas.Clients.ViewModels
-{
-    public class ClientNotificationTypePageViewModel : BasePageViewModel
-    {
-        public ClientNotificationTypeFormViewModel NotificationTypeForm { get; set; }
-    }
-}
