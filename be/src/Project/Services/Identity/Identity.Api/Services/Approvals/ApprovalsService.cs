f07fec1df0b9f8275a92b03c8e1a7ba686e35a11
Piotr Zaranek <piotrek.zaranek@gmail.com>
2024-10-21 10:15:13 +0200

    Added service for approvals

---
+++ be/src/Project/Services/Identity/Identity.Api/Services/Approvals/ApprovalsService.cs
@@ -0,0 +1,149 @@
+﻿using Foundation.Extensions.Exceptions;
+using Foundation.Extensions.ExtensionMethods;
+using Foundation.GenericRepository.Definitions;
+using Foundation.GenericRepository.Extensions;
+using Foundation.GenericRepository.Paginations;
+using Foundation.Localization;
+using Identity.Api.Infrastructure;
+using Identity.Api.Infrastructure.Approvals.Entities;
+using Identity.Api.ServicesModels.Approvals;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Localization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Identity.Api.Services.Approvals
+{
+    public class ApprovalsService : IApprovalsService
+    {
+        private IdentityContext _context;
+        private IStringLocalizer<GlobalResources> _globalLocalizer;
+
+        public ApprovalsService(
+            IdentityContext context,
+            IStringLocalizer<GlobalResources> globalLocalizer)
+        {
+            _context = context;
+            _globalLocalizer = globalLocalizer;
+        }
+
+        public async Task CreateAsync(CreateApprovalServiceModel model)
+        {
+            var approval = new Approval();
+
+            await _context.Approvals.AddAsync(approval.FillCommonProperties());
+
+            var approvalTranslation = new ApprovalTranslation
+            {
+                ApprovalId = approval.Id,
+                Name = model.Name,
+                Language = model.Language
+            };
+
+            await _context.ApprovalTranslations.AddAsync(approvalTranslation.FillCommonProperties());
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(DeleteApprovalServiceModel model)
+        {
+            var approval = await _context.Approvals.FirstOrDefaultAsync(x => x.Id == model.Id && x.IsActive);
+
+            if (approval is null)
+            {
+                throw new CustomException(_globalLocalizer.GetString("ApprovalNotFound"), (int)HttpStatusCode.NoContent);
+            }
+
+            approval.IsActive = false;
+
+            await _context.SaveChangesAsync();
+        }
+
+        public PagedResults<IEnumerable<ApprovalServiceModel>> Get(GetApprovalsServiceModel model)
+        {
+            var approvals = _context.Approvals.Where(x => x.IsActive)
+                .Include(x => x.Translations)
+                .AsSingleQuery()
+                .Select(x => new ApprovalServiceModel
+                {
+                    Id = x.Id,
+                    Name = x.Translations.FirstOrDefault(t => t.ApprovalId == x.Id && t.Language == model.Language) != null ? x.Translations.FirstOrDefault(t => t.ApprovalId == x.Id && t.Language == model.Language).Name : x.Translations.FirstOrDefault(t => t.ApprovalId == x.Id).Name,
+                    CreatedDate = x.CreatedDate,
+                    LastModifiedDate = x.LastModifiedDate
+                });
+
+            if (string.IsNullOrEmpty(model.SearchTerm) is false)
+            {
+                approvals = approvals.Where(x => x.Name.StartsWith(model.SearchTerm));
+            }
+
+            approvals = approvals.ApplySort(model.OrderBy);
+
+            PagedResults<IEnumerable<ApprovalServiceModel>> pagedResult;
+
+            if (model.PageIndex.HasValue is false || model.ItemsPerPage.HasValue is false)
+            {
+                approvals = approvals.Take(Constants.MaxItemsPerPageLimit);
+
+                pagedResult = approvals.PagedIndex(new Pagination(approvals.Count(), Constants.MaxItemsPerPage), Constants.DefaultPageIndex);
+            }
+            else
+            {
+                pagedResult = approvals.PagedIndex(new Pagination(approvals.Count(), model.ItemsPerPage.Value), model.PageIndex.Value);
+            }
+
+            return pagedResult;
+        }
+
+        public async Task<ApprovalServiceModel> GetAsync(GetApprovalServiceModel model)
+        {
+            var approval = await _context.Approvals.FirstOrDefaultAsync(x => x.Id == model.Id && x.IsActive);
+
+            if (approval is null)
+            {
+                throw new CustomException(_globalLocalizer.GetString("ApprovalNotFound"), (int)HttpStatusCode.NoContent);
+            }
+
+            return new ApprovalServiceModel
+            {
+                Id = approval.Id,
+                Name = approval.Translations.FirstOrDefault(t => t.ApprovalId == approval.Id && t.Language == model.Language) != null ? approval.Translations.FirstOrDefault(t => t.ApprovalId == approval.Id && t.Language == model.Language).Name : approval.Translations.FirstOrDefault(t => t.ApprovalId == approval.Id).Name,
+                CreatedDate = approval.CreatedDate
+            };
+        }
+
+        public async Task UpdateAsync(UpdateApprovalServiceModel model)
+        {
+            var approval = await _context.Approvals.FirstOrDefaultAsync(x => x.Id == model.Id && x.IsActive);
+
+            if (approval is null)
+            {
+                throw new CustomException(_globalLocalizer.GetString("ApprovalNotFound"), (int)HttpStatusCode.NoContent);
+            }
+
+            var approvalTranslation = await _context.ApprovalTranslations.FirstOrDefaultAsync(x => x.ApprovalId == approval.Id && x.IsActive && x.Language == model.Language);
+
+            if (approvalTranslation is not null)
+            {
+                approvalTranslation.Name = model.Name;
+                approvalTranslation.LastModifiedDate = DateTime.UtcNow;
+            }
+            else
+            {
+                var newApprovalTranslation = new ApprovalTranslation()
+                {
+                    Name = model.Name,
+                    Language = model.Language,
+                };
+
+                await _context.ApprovalTranslations.AddAsync(newApprovalTranslation.FillCommonProperties());
+            }
+
+            approval.LastModifiedDate = DateTime.UtcNow;
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ be/src/Project/Services/Identity/Identity.Api/Services/Approvals/IApprovalsService.cs
@@ -0,0 +1,16 @@
+﻿using Foundation.GenericRepository.Paginations;
+using Identity.Api.ServicesModels.Approvals;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Identity.Api.Services.Approvals
+{
+    public interface IApprovalsService
+    {
+        Task CreateAsync(CreateApprovalServiceModel model);
+        Task UpdateAsync(UpdateApprovalServiceModel model);
+        Task DeleteAsync(DeleteApprovalServiceModel model);
+        Task<ApprovalServiceModel> GetAsync(GetApprovalServiceModel model);
+        PagedResults<IEnumerable<ApprovalServiceModel>> Get(GetApprovalsServiceModel model);
+    }
+}
