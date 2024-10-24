2c51c6ff9b70e25a536c19f644ac4522cfdc0603
Piotr Zaranek <piotrek.zaranek@gmail.com>
2024-10-24 12:47:03 +0200

    Removed unnecessary property

--- be/src/Project/Web/Seller/Seller.Web/Areas/Clients/ApiRequestModels/ClientApprovalRequestModel.cs
+++ be/src/Project/Web/Seller/Seller.Web/Areas/Clients/ApiRequestModels/ClientApprovalRequestModel.cs
@@ -5,7 +5,6 @@ namespace Seller.Web.Areas.Clients.ApiRequestModels
 {
     public class ClientApprovalRequestModel : RequestModelBase
     {
-        public Guid? Id { get; set; }
         public string Name { get; set; }
     }
 }
