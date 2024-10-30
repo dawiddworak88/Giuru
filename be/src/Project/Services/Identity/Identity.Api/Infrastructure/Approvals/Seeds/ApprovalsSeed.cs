5c89c1f7df041b75b740c20efec6d400a9f1c19e
Piotr Zaranek <piotrek.zaranek@gmail.com>
2024-10-30 12:47:09 +0100

    Added seeds for approvals related with marketing information

---
+++ be/src/Project/Services/Identity/Identity.Api/Infrastructure/Approvals/Seeds/ApprovalsSeed.cs
@@ -0,0 +1,66 @@
+﻿using Foundation.GenericRepository.Extensions;
+using Identity.Api.Infrastructure.Approvals.Entities;
+using System;
+using System.Linq;
+
+namespace Identity.Api.Infrastructure.Approvals.Seeds
+{
+    public static class ApprovalsSeed
+    {
+        public static void SeedApprovals(IdentityContext context)
+        {
+            SeedApproval(
+                context,
+                Guid.Parse("7addb70a-0942-488d-ba27-afa5b570ede5"),
+                "I consent to receiving commercial information from ELTAP via electronic means, to the email address I have provided",
+                "Wyrażam zgodę na otrzymywanie informacji handlowych od ELTAP drogą elektroniczną, na wskazany przeze mnie adres e-mail",
+                "Ich stimme zu, kommerzielle Informationen von ELTAP auf elektronischem Wege an die von mir angegebene E-Mail-Adresse zu erhalten");
+
+            SeedApproval(
+                context,
+                Guid.Parse("d34ec3d3-bb88-4de5-9391-d9d29609551a"),
+                "I consent to being contacted by phone by ELTAP for marketing purposes",
+                "Wyrażam zgodę na kontakt telefoniczny ze strony ELTAP w celach marketingowych",
+                "Ich stimme einem telefonischen Kontakt durch ELTAP zu Marketingzwecken zu");
+        }
+
+        private static void SeedApproval(IdentityContext context, Guid id, string englishName, string polishName, string germanName)
+        {
+            if (context.Approvals.Any(x => x.Id == id) is false)
+            {
+                var approval = new Approval
+                {
+                    Id = id,
+                };
+
+                var enApprovalTranslation = new ApprovalTranslation
+                {
+                    ApprovalId = id,
+                    Name = englishName,
+                    Language = "en"
+                };
+
+                var plApprovalTranslation = new ApprovalTranslation
+                {
+                    ApprovalId = id,
+                    Name = polishName,
+                    Language = "pl"
+                };
+
+                var deApprovalTranslation = new ApprovalTranslation
+                {
+                    ApprovalId = id,
+                    Name = germanName,
+                    Language = "de"
+                };
+
+                context.Add(approval.FillCommonProperties());
+                context.Add(enApprovalTranslation.FillCommonProperties());
+                context.Add(plApprovalTranslation.FillCommonProperties());
+                context.Add(deApprovalTranslation.FillCommonProperties());
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
