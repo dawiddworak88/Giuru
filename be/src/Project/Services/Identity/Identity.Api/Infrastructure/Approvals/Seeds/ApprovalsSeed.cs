3542727aa9198287ba9c900e9b39176f7edbc47e
Piotr Zaranek <piotrek.zaranek@gmail.com>
2024-10-30 13:32:53 +0100

    Removed ELTAP words

--- be/src/Project/Services/Identity/Identity.Api/Infrastructure/Approvals/Seeds/ApprovalsSeed.cs
+++ be/src/Project/Services/Identity/Identity.Api/Infrastructure/Approvals/Seeds/ApprovalsSeed.cs
@@ -12,16 +12,16 @@ namespace Identity.Api.Infrastructure.Approvals.Seeds
             SeedApproval(
                 context,
                 Guid.Parse("7addb70a-0942-488d-ba27-afa5b570ede5"),
-                "I consent to receiving commercial information from ELTAP via electronic means, to the email address I have provided",
-                "Wyrażam zgodę na otrzymywanie informacji handlowych od ELTAP drogą elektroniczną, na wskazany przeze mnie adres e-mail",
-                "Ich stimme zu, kommerzielle Informationen von ELTAP auf elektronischem Wege an die von mir angegebene E-Mail-Adresse zu erhalten");
+                "I consent to receiving commercial information via electronic means, to the email address I have provided.",
+                "Wyrażam zgodę na otrzymywanie informacji handlowych drogą elektroniczną, na wskazany przeze mnie adres e-mail.",
+                "Ich stimme zu, kommerzielle Informationen auf elektronischem Wege an die von mir angegebene E-Mail-Adresse zu erhalten.");
 
             SeedApproval(
                 context,
                 Guid.Parse("d34ec3d3-bb88-4de5-9391-d9d29609551a"),
-                "I consent to being contacted by phone by ELTAP for marketing purposes",
-                "Wyrażam zgodę na kontakt telefoniczny ze strony ELTAP w celach marketingowych",
-                "Ich stimme einem telefonischen Kontakt durch ELTAP zu Marketingzwecken zu");
+                "I consent to being contacted by phone for marketing purposes.",
+                "Wyrażam zgodę na kontakt telefoniczny w celach marketingowych.",
+                "Ich erteile meine Zustimmung zu telefonischem Kontakt zu Marketingzwecken.");
         }
 
         private static void SeedApproval(IdentityContext context, Guid id, string englishName, string polishName, string germanName)
