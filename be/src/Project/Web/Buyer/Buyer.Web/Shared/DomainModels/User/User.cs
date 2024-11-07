6e00612b55de92892b725fa0e9c2ecd645a5e2d9
Piotr Zaranek <piotrek.zaranek@gmail.com>
2024-11-07 08:07:12 +0100

    Added IdentityRepository

---
+++ be/src/Project/Web/Buyer/Buyer.Web/Shared/DomainModels/User/User.cs
@@ -0,0 +1,8 @@
+﻿namespace Buyer.Web.Shared.DomainModels.User
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ be/src/Project/Web/Buyer/Buyer.Web/Shared/Repositories/Identity/IIdentityRepository.cs
@@ -0,0 +1,10 @@
+﻿using Buyer.Web.Shared.DomainModels.User;
+using System.Threading.Tasks;
+
+namespace Buyer.Web.Shared.Repositories.Identity
+{
+    public interface IIdentityRepository
+    {
+        Task<User> GetAsync(string token, string language, string email);
+    }
+}
