680791c03a4482be8c2f7e7462c0182846b47b8e
Sebastian Kasperek <sebastian.kasperek@eltap.com>
2025-01-16 13:48:59 +0100

    Added new controllers

---
+++ be/src/Tests/Giuru.MockAuth/Controllers/UserApprovalsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net;
+
+namespace Giuru.MockAuth.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class UserApprovalsController : ControllerBase
+    {
+        public UserApprovalsController() { }
+
+        [HttpGet]
+        [Route("{userId}")]
+        public IActionResult GetByUserId()
+        {
+            return StatusCode((int)HttpStatusCode.OK, Array.Empty<object>());
+        }
+    }
+}
