22324d0a2a3f7b1a1a389a3884e8e8c17346d898
Piotr Zaranek <piotrek.zaranek@gmail.com>
2025-02-10 08:47:37 +0100

    Removed inherit from domain model and added necessary properties

--- be/src/Project/Web/Buyer/Buyer.Web/Areas/Products/ApiResponseModels/ProductQuantitiesResponseModel.cs
+++ be/src/Project/Web/Buyer/Buyer.Web/Areas/Products/ApiResponseModels/ProductQuantitiesResponseModel.cs
@@ -1,9 +1,14 @@
-﻿using Buyer.Web.Areas.Products.DomainModels;
+﻿using System.Collections.Generic;
+using System;
 
 namespace Buyer.Web.Areas.Products.ApiResponseModels
 {
-    public class ProductQuantitiesResponseModel : Product
+    public class ProductQuantitiesResponseModel
     {
+        public Guid Id { get; set; }
+        public string Sku { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<Guid> Images { get; set; }
         public double StockQuantity { get; set; }
         public double OutletQuantity { get; set; }
     }
--- be/src/Project/Web/Seller/Seller.Web/Areas/Products/ApiResponseModels/ProductQuantitiesResponseModel.cs
+++ be/src/Project/Web/Seller/Seller.Web/Areas/Products/ApiResponseModels/ProductQuantitiesResponseModel.cs
@@ -1,9 +1,14 @@
-﻿using Seller.Web.Areas.Products.DomainModels;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Seller.Web.Areas.Products.ApiResponseModels
 {
-    public class ProductQuantitiesResponseModel : Product
+    public class ProductQuantitiesResponseModel
     {
+        public Guid Id { get; set; }
+        public string Sku { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<Guid> Images { get; set; }
         public double StockQuantity { get; set; }
         public double OutletQuantity { get; set; }
     }
