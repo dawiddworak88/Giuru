aa21f502485245c7b04786028870c1bc4c944863
Piotr Zaranek <piotrek.zaranek@gmail.com>
2025-02-12 13:08:21 +0100

    Added description property

--- be/src/Project/Web/Seller/Seller.Web/Areas/Clients/DomainModels/Approval.cs
+++ be/src/Project/Web/Seller/Seller.Web/Areas/Clients/DomainModels/Approval.cs
@@ -6,6 +6,7 @@ namespace Seller.Web.Areas.Clients.DomainModels
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
         public DateTime CreatedDate { get; set; }
         public DateTime LastModifiedDate { get; set; }
     }
