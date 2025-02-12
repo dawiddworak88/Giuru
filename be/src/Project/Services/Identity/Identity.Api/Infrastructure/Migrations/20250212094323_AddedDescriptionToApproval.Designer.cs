d48d12271bbc51a5c69436707d9009d37f4f4c1d
Piotr Zaranek <piotrek.zaranek@gmail.com>
2025-02-12 11:50:27 +0100

    Added descrition field to apporval translation

--- be/src/Project/Services/Identity/Identity.Api/Infrastructure/Migrations/20250212094323_AddedDescriptionToApproval.Designer.cs
+++ be/src/Project/Services/Identity/Identity.Api/Infrastructure/Migrations/20250212094323_AddedDescriptionToApproval.Designer.cs
@@ -4,6 +4,7 @@ using Identity.Api.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NetTopologySuite.Geometries;
 
@@ -12,9 +13,11 @@ using NetTopologySuite.Geometries;
 namespace Identity.Api.Infrastructure.Migrations
 {
     [DbContext(typeof(IdentityContext))]
-    partial class IdentityContextModelSnapshot : ModelSnapshot
+    [Migration("20250212094323_AddedDescriptionToApproval")]
+    partial class AddedDescriptionToApproval
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
