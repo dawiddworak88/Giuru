687b277b806983c18ea317cdd98e923ceb8ddc6f
Piotr Zaranek <piotrek.zaranek@gmail.com>
2025-02-18 13:49:07 +0100

    Added constants for attribute from strapi

---
+++ be/src/Project/Services/Identity/Identity.Api/Areas/Accounts/Definitions/GraphQlConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace Identity.Api.Areas.Accounts.Definitions
+{
+    public static class GraphQlConstants
+    {
+        public static readonly string PersonalDataAdministrator = "personalDataAdministrator";
+        public static readonly string MarketingFormDescription = "marketingFormDescription";
+    }
+}
