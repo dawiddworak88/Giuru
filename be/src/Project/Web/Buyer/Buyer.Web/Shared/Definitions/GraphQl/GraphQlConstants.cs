bfa5de9e646a079a9f4f02582bfd8b4b206336c7
Piotr Zaranek <piotrek.zaranek@gmail.com>
2025-02-20 13:07:22 +0100

    Added constants fo graphql

---
+++ be/src/Project/Web/Buyer/Buyer.Web/Shared/Definitions/GraphQl/GraphQlConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace Buyer.Web.Shared.Definitions.GraphQl
+{
+    public static class GraphQlConstants
+    {
+        public static readonly string PersonalDataAdministrator = "personalDataAdministrator";
+    }
+}
