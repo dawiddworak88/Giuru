d9d33c01c5a04e5ac8d671bcce9046cb1c2bd359
Piotr Zaranek <piotrek.zaranek@gmail.com>
2025-02-27 14:57:10 +0100

    Added ValidateStockOutletQuantitiesAsync method

--- be/src/Project/Web/Buyer/Buyer.Web/Shared/Services/Baskets/BasketService.cs
+++ be/src/Project/Web/Buyer/Buyer.Web/Shared/Services/Baskets/BasketService.cs
@@ -1,11 +1,17 @@
 ﻿using Buyer.Web.Areas.Orders.Repositories.Baskets;
+using Buyer.Web.Areas.Products.Repositories;
+using Buyer.Web.Areas.Products.Repositories.Inventories;
 using Buyer.Web.Shared.DomainModels.Baskets;
+using Foundation.Extensions.Exceptions;
 using Foundation.Extensions.ExtensionMethods;
+using Foundation.Localization;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Localization;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace Buyer.Web.Shared.Services.Baskets
@@ -14,13 +20,25 @@ namespace Buyer.Web.Shared.Services.Baskets
     {
         private readonly LinkGenerator linkGenerator;
         private readonly IBasketRepository basketRepository;
+        private readonly IInventoryRepository inventoryRepository;
+        private readonly IOutletRepository outletRepository;
+        private readonly IStringLocalizer<OrderResources> orderLocalizer;
+        private readonly IStringLocalizer<GlobalResources> globalLocalizer;
 
         public BasketService(
             LinkGenerator linkGenerator,
-            IBasketRepository basketRepository)
+            IBasketRepository basketRepository,
+            IInventoryRepository inventoryRepository,
+            IOutletRepository outletRepository,
+            IStringLocalizer<OrderResources> orderLocalizer,
+            IStringLocalizer<GlobalResources> globalLocalizer)
         {
             this.linkGenerator = linkGenerator;
             this.basketRepository = basketRepository;
+            this.inventoryRepository = inventoryRepository;
+            this.outletRepository = outletRepository;
+            this.orderLocalizer = orderLocalizer;
+            this.globalLocalizer = globalLocalizer;
         }
 
         public async Task<IEnumerable<BasketItem>> GetBasketAsync(Guid? basketId, string token, string language)
@@ -52,6 +70,48 @@ namespace Buyer.Web.Shared.Services.Baskets
             }
 
             return default;
+        }
+
+        public async Task ValidateStockOutletQuantitiesAsync(Guid? basketId, string token, string language)
+        {
+            var basket = await this.basketRepository.GetBasketById(token, language, basketId);
+
+            if (basket is not null && basket.Items.Any())
+            {
+                if (basket.Items.Any(x => x.StockQuantity > 0))
+                {
+                    var stockItems = basket.Items.Where(x => x.StockQuantity > 0);
+
+                    var inventoryProducts = await this.inventoryRepository.GetAvailbleProductsByProductIdsAsync(token, language, stockItems.Select(x => x.ProductId.Value));
+
+                    foreach (var item in stockItems)
+                    {
+                        var inventoryProduct = inventoryProducts.FirstOrDefault(x => x.ProductId == item.ProductId);
+
+                        if (inventoryProduct is not null && item.StockQuantity > inventoryProduct.AvailableQuantity)
+                        {
+                            throw new CustomException($"{this.orderLocalizer.GetString("StockQuantityError").Value} {item.ProductName} ({item.ProductSku}) {this.globalLocalizer.GetString("InBasket")} {item.StockQuantity} {this.globalLocalizer.GetString("MaximalLabel")} {inventoryProduct.AvailableQuantity}", (int)HttpStatusCode.Conflict);
+                        }
+                    }
+                }
+
+                if (basket.Items.Any(x => x.OutletQuantity > 0))
+                {
+                    var outletItems = basket.Items.Where(x => x.OutletQuantity > 0);
+
+                    var outletProducts = await this.outletRepository.GetOutletProductsByProductsIdAsync(token, language, outletItems.Select(x => x.ProductId.Value));
+
+                    foreach (var item in outletItems)
+                    {
+                        var outletProduct = outletProducts.FirstOrDefault(x => x.ProductId == item.ProductId);
+
+                        if (outletProduct is not null && item.OutletQuantity > outletProduct.AvailableQuantity)
+                        {
+                            throw new CustomException($"{this.orderLocalizer.GetString("OutletQuantityError").Value} {item.ProductName} ({item.ProductSku}) {this.globalLocalizer.GetString("InBasket")} {item.StockQuantity} {this.globalLocalizer.GetString("MaximalLabel")} {outletProduct.AvailableQuantity}", (int)HttpStatusCode.Conflict);
+                        }
+                    }
+                }
+            }
         }
     }
 }
