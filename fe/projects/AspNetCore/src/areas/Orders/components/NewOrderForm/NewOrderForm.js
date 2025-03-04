08f903a038507ec0e9123425880695fca7fab244
Piotr Zaranek <112848485+PiotrZaranek@users.noreply.github.com>
2025-03-04 12:34:50 +0100

    Add column "in total" in new order form  (#426)
    * Added missing properties
    
    * Added cell for total quantity of product
    
    * Added InTotalLabel property
    
    * Added InTotalLabel
    
    * Moved calculate total product quantity to the function

--- fe/projects/AspNetCore/src/areas/Orders/components/NewOrderForm/NewOrderForm.js
+++ fe/projects/AspNetCore/src/areas/Orders/components/NewOrderForm/NewOrderForm.js
@@ -346,6 +346,10 @@ function NewOrderForm(props) {
 
     const disabledActionButtons = orderItems.length === 0 ? !customOrder ? true : false : false;
 
+    const getTotalQuantities = (item) => {
+        return item.quantity + item.stockQuantity + item.outletQuantity;
+    }
+
     return (
         <section className="section order">
             <h1 className="subtitle is-4">{props.title}</h1>
@@ -494,6 +498,7 @@ function NewOrderForm(props) {
                                                     <TableCell>{props.quantityLabel}</TableCell>
                                                     <TableCell>{props.stockQuantityLabel}</TableCell>
                                                     <TableCell>{props.outletQuantityLabel}</TableCell>
+                                                    <TableCell className="has-text-weight-bold">{props.inTotalLabel}</TableCell>
                                                     <TableCell>{props.externalReferenceLabel}</TableCell>
                                                     <TableCell>{props.moreInfoLabel}</TableCell>
                                                 </TableRow>
@@ -514,6 +519,7 @@ function NewOrderForm(props) {
                                                         <TableCell>{item.quantity}</TableCell>
                                                         <TableCell>{item.stockQuantity}</TableCell>
                                                         <TableCell>{item.outletQuantity}</TableCell>
+                                                        <TableCell className="has-text-weight-bold">{getTotalQuantities(item)}</TableCell>
                                                         <TableCell>{item.externalReference}</TableCell>
                                                         <TableCell>{item.moreInfo}</TableCell>
                                                     </TableRow>
