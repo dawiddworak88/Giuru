5882b15bba038d2bccf2fb98425d968e557e37ca
Piotr Zaranek <piotrek.zaranek@gmail.com>
2025-03-13 18:32:03 +0100

    Added helper class for tests

---
+++ be/src/Tests/Giuru.IntegrationTests/Helpers/TestsHelper.cs
@@ -0,0 +1,62 @@
+﻿using Catalog.Api.v1.Products.RequestModels;
+using Foundation.ApiExtensions.Models.Response;
+using Foundation.GenericRepository.Paginations;
+using Giuru.IntegrationTests.Definitions;
+using Inventory.Api.v1.RequestModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Giuru.IntegrationTests.Helpers
+{
+    public static class TestsHelper
+    {
+        public static async Task<Guid?> CreateProductAndAddToStockAsync(ApiFixture apiFixture, ProductRequestModel product, string endpoint = null)
+        {
+            var newProdct = await apiFixture.SellerWebClient.PostAsync<ProductRequestModel, BaseResponseModel>(ApiEndpoints.ProductsApiEndpoint, product);
+
+            Assert.NotNull(newProdct);
+            Assert.NotEqual(Guid.Empty, newProdct.Id);
+
+            if (endpoint is not null)
+            {
+                var addToStock = await apiFixture.SellerWebClient.PostAsync<InventoryRequestModel, BaseResponseModel>(endpoint, new InventoryRequestModel
+                {
+                    ProductId = newProdct.Id,
+                    WarehouseId = Guid.Parse("00000000-0000-0000-0000-000000000001"),
+                    AvailableQuantity = Products.Quantities.AvailableQuantity,
+                    Quantity = Products.Quantities.Quantity,
+                });
+
+                Assert.NotNull(addToStock);
+                Assert.NotEqual(Guid.Empty, addToStock.Id);
+            }
+
+            return newProdct.Id;
+        }
+
+        public static async Task<PagedResults<IEnumerable<T>>> GetDataAsync<T>(
+            Func<Task<PagedResults<IEnumerable<T>>>> fetchData,
+            Func<T, bool> condition)
+        {
+            int timeoutInSeconds = Common.TimeoutInSeconds;
+            int elapsedSeconds = Common.ElapsedSeconds;
+
+            while (elapsedSeconds < timeoutInSeconds)
+            {
+                var getResults = await fetchData();
+
+                if (getResults.Data.Any(condition))
+                {
+                    return getResults;
+                }
+
+                await Task.Delay(1000);
+                elapsedSeconds++;
+            }
+
+            return null;
+        }
+    }
+}
