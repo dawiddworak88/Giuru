62a3b6f6db305bb4e3fc7cd96133360cfd5b8648
Piotr Zaranek <piotrek.zaranek@gmail.com>
2025-04-24 14:15:20 +0200

    Added definitions for inventories

---
+++ be/src/Tests/Giuru.IntegrationTests/Definitions/Inventories.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Giuru.IntegrationTests.Definitions
+{
+    public struct Inventories
+    {
+        public static readonly Guid WarehouseId = Guid.Parse("1b4a61fb-cdda-45b2-a4d6-92a27acdf833");
+
+        public struct Quantities
+        {
+            public const int Quantity = 2;
+            public const int AvailableQuantity = 2;
+        }
+    }
+}
