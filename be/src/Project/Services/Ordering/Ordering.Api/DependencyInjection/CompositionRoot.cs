ce4f08f699ddfde9ad7ca6eb31bd09b6169af3e4
Sebastian Kasperek <sebastian.kasperek@eltap.com>
2025-04-30 10:12:12 +0200

    Update CompositionRoot.cs

--- be/src/Project/Services/Ordering/Ordering.Api/DependencyInjection/CompositionRoot.cs
+++ be/src/Project/Services/Ordering/Ordering.Api/DependencyInjection/CompositionRoot.cs
@@ -25,7 +25,9 @@ namespace Ordering.Api.DependencyInjection
         {
             services.AddScoped<OrderingContext>();
 
-            services.AddDbContext<OrderingContext>(options => options.UseSqlServer(configuration["ConnectionString"], opt => opt.UseNetTopologySuite()));
+            services.AddDbContext<OrderingContext>(options => options.UseSqlServer(configuration["ConnectionString"], opt => opt.UseNetTopologySuite())
+                .EnableSensitiveDataLogging()
+                .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, LogLevel.Information));
         }
 
         public static void RegisterEventBus(this IServiceCollection services, IConfiguration configuration)
