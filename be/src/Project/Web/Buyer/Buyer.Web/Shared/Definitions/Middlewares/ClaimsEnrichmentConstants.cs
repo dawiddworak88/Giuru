2eb5c1163455638152eabf66255bbff8dbc1943c
Sebastian Kasperek <sebastian.kasperek@eltap.com>
2025-05-04 23:18:00 +0200

    Cleaned claims middleware

---
+++ be/src/Project/Web/Buyer/Buyer.Web/Shared/Definitions/Middlewares/ClaimsEnrichmentConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace Buyer.Web.Shared.Definitions.Middlewares
+{
+    public static class ClaimsEnrichmentConstants
+    {
+        public static readonly string PaletteLoadingClaimType = "PaletteLoading";
+        public static readonly string ExtraPackingClaimType = "ExtraPacking";
+        public static readonly string CountryClaimType = "Country";
+        public static readonly string ZipCodeClaimType = "ZipCode";
+        public static readonly string CurrencyClaimType = "Currency";
+    }
+}
--- be/src/Project/Web/Buyer/Buyer.Web/Shared/Middlewares/ClaimsEnrichmentMiddleware.cs
+++ be/src/Project/Web/Buyer/Buyer.Web/Shared/Middlewares/ClaimsEnrichmentMiddleware.cs
@@ -1,9 +1,11 @@
-﻿using Buyer.Web.Shared.Repositories.Clients;
+﻿using Buyer.Web.Shared.Configurations;
+using Buyer.Web.Shared.Definitions.Middlewares;
+using Buyer.Web.Shared.Repositories.Clients;
 using Buyer.Web.Shared.Repositories.Global;
 using Foundation.ApiExtensions.Definitions;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Http;
-using System.Globalization;
+using Microsoft.Extensions.Options;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -16,87 +18,100 @@ namespace Buyer.Web.Shared.Middlewares
         private readonly IClientAddressesRepository _clientAddressesRepository;
         private readonly IGlobalRepository _globalRepository;
         private readonly IClientFieldValuesRepository _clientFieldValuesRepository;
-        
+        private readonly IOptions<AppSettings> _options;
+
         public ClaimsEnrichmentMiddleware(
             IClientsRepository clientsRepository,
             IClientAddressesRepository clientAddressesRepository,
             IGlobalRepository globalRepository,
-            IClientFieldValuesRepository clientFieldValuesRepository)
+            IClientFieldValuesRepository clientFieldValuesRepository,
+            IOptions<AppSettings> options)
         {
             _clientsRepository = clientsRepository;
             _clientAddressesRepository = clientAddressesRepository;
             _globalRepository = globalRepository;
             _clientFieldValuesRepository = clientFieldValuesRepository;
+            _options = options;
         }
 
         public async Task InvokeAsync(HttpContext context, RequestDelegate next)
         {
-            if (context.User.Identity.IsAuthenticated)
+            if (context.User.Identity?.IsAuthenticated is false)
             {
-                var userEmail = context.User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Email)?.Value;
+                await next(context);
+                return;
+            }
 
-                if (string.IsNullOrWhiteSpace(userEmail))
+            var email = context.User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Email)?.Value;
+
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                await next(context);
+                return;
+            }
+
+            var token = await context.GetTokenAsync(ApiExtensionsConstants.TokenName);
+            var claimsIdentity = (ClaimsIdentity)context.User.Identity;
+
+            var client = await _clientsRepository.GetClientByEmailAsync(token, _options.Value.DefaultCulture, email);
+
+            if (client is null)
+            {
+                await next(context);
+                return;
+            }
+
+            if (client.PreferedCurrencyId.HasValue)
+            {
+                var currencies = await _globalRepository.GetCurrenciesAsync(token, _options.Value.DefaultCulture, null);
+
+                if (currencies.Any())
                 {
-                    return;
-                }
+                    var currency = currencies.FirstOrDefault(c => c.Id == client.PreferedCurrencyId);
 
-                var token = await context.Request.HttpContext.GetTokenAsync(ApiExtensionsConstants.TokenName);
-                var language = CultureInfo.CurrentUICulture.Name;
-                var client = await _clientsRepository.GetClientByEmailAsync(token, language, userEmail);
-
-                if (client is not null)
-                {
-                    var claimsIdentity = (ClaimsIdentity)context.User.Identity;
-
-                    var countries = await _globalRepository.GetCountriesAsync(token, language, null);
-                    
-                    var clientAddress = await _clientAddressesRepository.GetAsync(token, language, client.DefaultDeliveryAddressId);
-
-                    if (clientAddress is not null)
+                    if (currency is not null)
                     {
-                        var country = countries.FirstOrDefault(x => x.Id == clientAddress.CountryId);
-
-                        if (country is not null)
-                        {
-                            claimsIdentity.AddClaim(new Claim("ZipCode", $"{clientAddress.PostCode} ({clientAddress.City}, {country.Name})"));
-                            claimsIdentity.AddClaim(new Claim("Country", $"{country.Name}"));
-                        }
-                    }
-
-                    var clientFieldValues = await _clientFieldValuesRepository.GetAsync(token, language, client.Id);
-
-                    if (clientFieldValues.Any())
-                    {
-                        var extraPackingField = clientFieldValues.FirstOrDefault(x => x.FieldName == "Extra Packing");
-
-                        if (extraPackingField is not null)
-                        {
-                            claimsIdentity.AddClaim(new Claim("ExtraPacking", extraPackingField.FieldValue));
-                        }
-
-                        var paletteLoading = clientFieldValues.FirstOrDefault(x => x.FieldName == "Palette Loading");
-
-                        if (paletteLoading is not null)
-                        {
-                            claimsIdentity.AddClaim(new Claim("PaletteLoading", paletteLoading.FieldValue));
-                        }
-                    }
-
-                    var currencies = await _globalRepository.GetCurrenciesAsync(token, language, null);
-
-                    if (currencies.Any())
-                    {
-                        var currency = currencies.FirstOrDefault(x => x.Id == client.PreferedCurrencyId);
-
-                        if (currency is not null)
-                        {
-                            claimsIdentity.AddClaim(new Claim("Currency", currency.CurrencyCode));
-                        }
+                        claimsIdentity.AddClaim(new Claim(ClaimsEnrichmentConstants.CurrencyClaimType, currency.CurrencyCode));
                     }
                 }
             }
 
-            await next(context);
+            var address = await _clientAddressesRepository.GetAsync(token, _options.Value.DefaultCulture, client.DefaultDeliveryAddressId);
+
+            if (address is not null)
+            {
+                var countries = await _globalRepository.GetCountriesAsync(token, _options.Value.DefaultCulture, null);
+
+                if (countries.Any())
+                {
+                    var country = countries.FirstOrDefault(c => c.Id == address.CountryId);
+
+                    if (country != null)
+                    {
+                        claimsIdentity.AddClaim(new Claim(ClaimsEnrichmentConstants.ZipCodeClaimType, $"{address.PostCode} ({address.City}, {country.Name})"));
+                        claimsIdentity.AddClaim(new Claim(ClaimsEnrichmentConstants.CountryClaimType, country.Name));
+                    }
+                }
+            }
+
+            var clientFieldValues = await _clientFieldValuesRepository.GetAsync(token, _options.Value.DefaultCulture, client.Id);
+
+            if (clientFieldValues.Any())
+            {
+                var extraPackingField = clientFieldValues.FirstOrDefault(x => x.FieldName == "Extra Packing");
+
+                if (extraPackingField is not null)
+                {
+                    claimsIdentity.AddClaim(new Claim(ClaimsEnrichmentConstants.ExtraPackingClaimType, extraPackingField.FieldValue));
+                }
+
+                var paletteLoading = clientFieldValues.FirstOrDefault(x => x.FieldName == "Palette Loading");
+
+                if (paletteLoading is not null)
+                {
+                    claimsIdentity.AddClaim(new Claim(ClaimsEnrichmentConstants.PaletteLoadingClaimType, paletteLoading.FieldValue));
+                }
+            }
         }
     }
 }
