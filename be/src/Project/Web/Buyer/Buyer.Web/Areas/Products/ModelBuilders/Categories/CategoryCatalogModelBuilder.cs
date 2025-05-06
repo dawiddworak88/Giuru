279ba5ad6fb73bcc1eb70f757dbe15cb1f9b2181
Sebastian Kasperek <sebastian.kasperek@eltap.com>
2025-05-06 12:34:14 +0200

    Added displaying prices on category page

--- be/src/Project/Web/Buyer/Buyer.Web/Areas/Products/ModelBuilders/Categories/CategoryCatalogModelBuilder.cs
+++ be/src/Project/Web/Buyer/Buyer.Web/Areas/Products/ModelBuilders/Categories/CategoryCatalogModelBuilder.cs
@@ -11,6 +11,15 @@ using Foundation.PageContent.ComponentModels;
 using Buyer.Web.Shared.ViewModels.Sidebar;
 using Buyer.Web.Shared.ViewModels.Modals;
 using Buyer.Web.Areas.Products.DomainModels;
+using Buyer.Web.Shared.DomainModels.Prices;
+using System.Linq;
+using Buyer.Web.Shared.Services.Prices;
+using Microsoft.Extensions.Options;
+using Buyer.Web.Shared.Configurations;
+using System;
+using Buyer.Web.Areas.Products.ViewModels.Products;
+using Buyer.Web.Shared.ViewModels.Catalogs;
+using System.Collections.Generic;
 
 namespace Buyer.Web.Areas.Products.ModelBuilders.Categories
 {
@@ -21,19 +30,25 @@ namespace Buyer.Web.Areas.Products.ModelBuilders.Categories
         private readonly IAsyncComponentModelBuilder<ComponentModelBase, ModalViewModel> modalModelBuilder;
         private readonly IProductsService productsService;
         private readonly ICategoryRepository categoryRepository;
+        private readonly IOptions<AppSettings> _options;
+        private readonly IPriceService _priceService;
 
         public CategoryCatalogModelBuilder(
             ICatalogModelBuilder<SearchProductsComponentModel, CategoryCatalogViewModel> catalogModelBuilder,
             IAsyncComponentModelBuilder<ComponentModelBase, SidebarViewModel> sidebarModelBuilder,
             IAsyncComponentModelBuilder<ComponentModelBase, ModalViewModel> modalModelBuilder,
             IProductsService productsService,
-            ICategoryRepository categoryRepository)
+            ICategoryRepository categoryRepository,
+            IOptions<AppSettings> options,
+            IPriceService priceService)
         {
             this.catalogModelBuilder = catalogModelBuilder;
             this.sidebarModelBuilder = sidebarModelBuilder;
             this.productsService = productsService;
             this.categoryRepository = categoryRepository;
             this.modalModelBuilder = modalModelBuilder;
+            _options = options;
+            _priceService = priceService;
         }
 
         public async Task<CategoryCatalogViewModel> BuildModelAsync(SearchProductsComponentModel componentModel)
@@ -49,7 +64,8 @@ namespace Buyer.Web.Areas.Products.ModelBuilders.Categories
                 viewModel.Sidebar = await this.sidebarModelBuilder.BuildModelAsync(componentModel);
                 viewModel.Modal = await this.modalModelBuilder.BuildModelAsync(componentModel);
                 viewModel.OrderBy = nameof(Product.Name);
-                viewModel.PagedItems = await this.productsService.GetProductsAsync(
+
+                var products = await this.productsService.GetProductsAsync(
                     null,
                     componentModel.Id,
                     null,
@@ -59,6 +75,63 @@ namespace Buyer.Web.Areas.Products.ModelBuilders.Categories
                     PaginationConstants.DefaultPageIndex,
                     ProductConstants.ProductsCatalogPaginationPageSize,
                     componentModel.Token);
+
+                if (products is not null)
+                {
+                    var prices = Enumerable.Empty<Price>();
+
+                    if (string.IsNullOrWhiteSpace(_options.Value.GrulaAccessToken) is false)
+                    {
+                        prices = await _priceService.GetPrices(
+                            _options.Value.GrulaAccessToken,
+                            DateTime.UtcNow,
+                            products.Data.Select(x => new PriceProduct
+                            {
+                                PrimarySku = x.PrimaryProductSku,
+                                FabricsGroup = x.FabricsGroup,
+                                SleepAreaSize = x.SleepAreaSize,
+                                ExtraPacking = x.ExtraPacking
+                            }),
+                            new PriceClient
+                            {
+                                Name = componentModel.Name,
+                                CurrencyCode = componentModel.CurrencyCode,
+                                ExtraPacking = componentModel.ExtraPacking,
+                                PaletteLoading = componentModel.PaletteLoading,
+                                Country = componentModel.Country,
+                                DeliveryZipCode = componentModel.DeliveryZipCode
+                            });
+                    }
+
+                    for (int i = 0; i < products.Data.Count(); i++)
+                    {
+                        var product = products.Data.ElementAtOrDefault(i);
+
+                        if (product is null)
+                        {
+                            continue;
+                        }
+
+                        if (prices.Any())
+                        {
+                            var price = prices.ElementAtOrDefault(i);
+
+                            if (price is not null)
+                            {
+                                product.Price = new ProductPriceViewModel
+                                {
+                                    Current = price.Amount,
+                                    Currency = price.CurrencyCode
+                                };
+                            }
+                        }
+                    }
+
+                    viewModel.PagedItems = new PagedResults<IEnumerable<CatalogItemViewModel>>(products.Total, products.PageSize)
+                    {
+                        Data = products.Data.OrderBy(x => x.Title)
+                    };
+                }
             }
 
             return viewModel;
