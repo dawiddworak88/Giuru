091a639f97fd7ef6fe691c954b4961dcc21d25b9
Sebastian Kasperek <sebastian.kasperek@eltap.com>
2025-05-06 09:14:28 +0200

    Added new api endpoint to constants

--- be/src/Infrastructure/ApiExtensions/Foundation.ApiExtensions/Definitions/ApiConstants.cs
+++ be/src/Infrastructure/ApiExtensions/Foundation.ApiExtensions/Definitions/ApiConstants.cs
@@ -37,6 +37,7 @@
             public static readonly string ApplicationsApiEndpoint = "/api/v1/clientsapplications";
             public static readonly string ManagersApiEndpoint = "/api/v1/clientaccountmanagers";
             public static readonly string ClientsApiEndpoint = "/api/v1/clients";
+            public static readonly string ClientsByEmailApiEndpoint = "/api/v1/clients/email";
             public static readonly string AddressesApiEndpoint = "/api/v1/clientaddresses";
             public static readonly string FieldsApiEndpoint = "/api/v1/clientfields";
             public static readonly string FieldOptionsApiEndpoint = "/api/v1/clientfieldoptions";
