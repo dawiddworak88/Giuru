36897b350a4cfc779833b12d001d916d07c09dea
Sebastian Kasperek <sebastian.kasperek@eltap.com>
2025-05-08 12:21:12 +0200

    Added price formating

--- fe/projects/AspNetCore/src/shared/components/Price/Price.js
+++ fe/projects/AspNetCore/src/shared/components/Price/Price.js
@@ -7,17 +7,19 @@ const Price = ({
     currency,
     lowestPriceLabel
 }) => (
-    <div className="price">
-        <div className="price__container">
-            <h3 className="price__current">{current} {currency}</h3>
-            {old && (
-                <span className="price__old">{old} {currency}</span>
+    current && (
+        <div className="price">
+            <div className="price__container">
+                <h3 className="price__current">{new Intl.NumberFormat(null, { style: 'currency', currency }).format(current)} {currency}</h3>
+                {old && (
+                    <span className="price__old">{old} {currency}</span>
+                )}
+            </div>
+            {lowestPrice && lowestPriceLabel && (
+                <span className="price__lowest">{lowestPriceLabel} {lowestPrice} {currency}</span>
             )}
         </div>
-        {lowestPrice && lowestPriceLabel && (
-            <span className="price__lowest">{lowestPriceLabel} {lowestPrice} {currency}</span>
-        )}
-    </div>
+    )
 )
 
 export default Price;
