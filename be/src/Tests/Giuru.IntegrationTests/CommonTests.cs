451591833d157891e6e24b457ffb24c836136c71
Piotr Zaranek <112848485+PiotrZaranek@users.noreply.github.com>
2025-05-08 09:54:09 +0200

    Add integration tests for inventory and correct BuyerWebTest (#433)
    * Added InventoryApiImage
    
    * Added Inventory container and added url for buyer and seller
    
    * Added endpoint for inventories
    
    * Added products for inventories
    
    * Corrected asserts and condition
    
    * Implement test
    
    * Moved Quantites constants to another struct
    
    * Added constants for common
    
    * Added helper class for tests
    
    * Improved tests
    
    * Removed unnecessary environment variable
    
    * Changed class name
    
    * Removed static value
    
    * Removed Quantities struct
    
    * Added definitions for inventories
    
    * Corrected helper name and definition name
    
    * Changed class name and removed GetDataAsync
    
    * Added DataHelper
    
    * Corrected helper classes name

---
+++ be/src/Tests/Giuru.IntegrationTests/CommonTests.cs
@@ -0,0 +1,104 @@
+﻿using Buyer.Web.Areas.Orders.ApiRequestModels;
+using Buyer.Web.Areas.Orders.ApiResponseModels;
+using Buyer.Web.Areas.Orders.DomainModels;
+using Catalog.Api.v1.Products.RequestModels;
+using Foundation.ApiExtensions.Models.Response;
+using Foundation.GenericRepository.Definitions;
+using Foundation.GenericRepository.Paginations;
+using Giuru.IntegrationTests.Definitions;
+using Giuru.IntegrationTests.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Giuru.IntegrationTests
+{
+    [Collection(nameof(ApiCollection))]
+    public class CommonTests
+    {
+        private readonly ApiFixture _apiFixture;
+
+        public CommonTests(ApiFixture apiFixture)
+        {
+            _apiFixture = apiFixture;
+        }
+
+        [Fact]
+        public async Task CreateProducts_AddProductsToInventory_CheckoutOrder_Returns_Orders()
+        {
+            var onStockProduct = await InventoryHelper.CreateProductAndAddToStockAsync(_apiFixture, new ProductRequestModel
+            {
+                Sku = Products.Anton.Sku,
+                Name = Products.Anton.Name,
+                CategoryId = Products.Anton.CategoryId,
+                IsPublished = Products.Anton.IsPublished,
+                Ean = Products.Anton.Ean,
+            },
+            ApiEndpoints.InventoriesApiEndpoint);
+
+            var onOutletProduct = await InventoryHelper.CreateProductAndAddToStockAsync(_apiFixture, new ProductRequestModel
+            {
+                Sku = Products.Aga.Sku,
+                Name = Products.Aga.Name,
+                CategoryId = Products.Aga.CategoryId,
+                IsPublished = Products.Aga.IsPublished,
+                Ean = Products.Aga.Ean,
+            },
+            ApiEndpoints.OutletsApiEndpoint);
+
+            var basket = await _apiFixture.BuyerWebClient.PostAsync<SaveBasketRequestModel, BasketResponseModel>(ApiEndpoints.BasketApiEndpoint, new SaveBasketRequestModel
+            {
+                Items = new List<BasketItemRequestModel>()
+                {
+                    new BasketItemRequestModel
+                    {
+                        ProductId = onStockProduct,
+                        Sku = Products.Anton.Sku,
+                        Name = Products.Anton.Name,
+                        StockQuantity = Inventories.Quantities.AvailableQuantity
+                    },
+                    new BasketItemRequestModel
+                    {
+                        ProductId = onOutletProduct,
+                        Sku = Products.Aga.Sku,
+                        Name = Products.Aga.Name,
+                        OutletQuantity = Inventories.Quantities.AvailableQuantity
+                    }
+                }
+            });
+
+            Assert.NotNull(basket);
+            Assert.NotEqual(Guid.Empty, basket.Id);
+            Assert.NotNull(basket.Items);
+            Assert.NotNull(basket.Items.FirstOrDefault(x => x.ProductId == onStockProduct));
+            Assert.NotNull(basket.Items.FirstOrDefault(x => x.ProductId == onOutletProduct));
+
+            await _apiFixture.BuyerWebClient.PostAsync<CheckoutBasketRequestModel, BaseResponseModel>(ApiEndpoints.OrderCheckoutApiEndpoint, new CheckoutBasketRequestModel
+            {
+                BasketId = basket.Id,
+                ClientId = Clients.Id,
+                ClientName = Clients.Name
+            });
+
+            var getResults = await DataHelper.GetDataAsync(
+                () => _apiFixture.BuyerWebClient.GetAsync<PagedResults<IEnumerable<Order>>>($"{ApiEndpoints.GetOrdersApiEndpoint}?pageIndex={Constants.DefaultPageIndex}&itemsPerPage={Constants.DefaultItemsPerPage}"),
+                x => x.OrderItems.Any(y => y.ProductId == onStockProduct || y.ProductId == onOutletProduct));
+
+            var order = getResults.Data.FirstOrDefault(x => x.OrderItems.Any(x => x.ProductId == onStockProduct || x.ProductId == onOutletProduct));
+
+            Assert.NotNull(order);
+            Assert.NotEmpty(order.OrderItems);
+
+            var stockOrderItem = order.OrderItems.FirstOrDefault(x => x.ProductId == onStockProduct);
+
+            Assert.NotNull(stockOrderItem);
+            Assert.Equal(Inventories.Quantities.AvailableQuantity, stockOrderItem.StockQuantity);
+
+            var outletOrderItem = order.OrderItems.FirstOrDefault(x => x.ProductId == onOutletProduct);
+
+            Assert.NotNull(outletOrderItem);
+            Assert.Equal(Inventories.Quantities.AvailableQuantity, outletOrderItem.OutletQuantity);
+        }
+    }
+}
---
+++ be/src/Tests/Giuru.IntegrationTests/Images/InventoryApiImage.cs
@@ -0,0 +1,53 @@
+﻿using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Images;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Giuru.IntegrationTests.Images
+{
+    class InventoryApiImage : IImage
+    {
+        private readonly SemaphoreSlim _semaphoreSlim = new SemaphoreSlim(1, 1);
+
+        private readonly IImage _image = new DockerImage("testcontainers", "inventory-api", DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString());
+        
+        public async Task InitializeAsync()
+        {
+            await _semaphoreSlim.WaitAsync().ConfigureAwait(false);
+            try
+            {
+                await new ImageFromDockerfileBuilder()
+                    .WithName(this)
+                    .WithDockerfileDirectory(Path.Combine(CommonDirectoryPath.GetProjectDirectory().DirectoryPath, "../../"))
+                    .WithDockerfile("Project/Services/Inventory/Inventory.Api/Dockerfile")
+                    .WithDeleteIfExists(false)
+                    .Build()
+                    .CreateAsync()
+                    .ConfigureAwait(false);
+            }
+            finally
+            {
+                _semaphoreSlim.Release();
+            }
+        }
+
+        public Task DisposeAsync()
+        {
+            return Task.CompletedTask;
+        }
+        public string Repository => _image.Repository;
+
+        public string Name => _image.Name;
+
+        public string Tag => _image.Tag;
+
+        public string FullName => _image.FullName;
+
+        public string GetHostname()
+        {
+            return _image.GetHostname();
+        }
+    }
+}
