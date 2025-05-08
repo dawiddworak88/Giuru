44b601c4762dc8b8aaae905baaa6e7406acfcd41
Sebastian Kasperek <sebastian.kasperek@eltap.com>
2025-05-08 12:49:23 +0200

    Update Price.js

--- fe/projects/AspNetCore/src/shared/components/Price/Price.js
+++ fe/projects/AspNetCore/src/shared/components/Price/Price.js
@@ -7,19 +7,17 @@ const Price = ({
     currency,
     lowestPriceLabel
 }) => (
-    current && (
-        <div className="price">
-            <div className="price__container">
-                <h3 className="price__current">{new Intl.NumberFormat(null, { style: 'currency', currency }).format(current)} {currency}</h3>
-                {old && (
-                    <span className="price__old">{old} {currency}</span>
-                )}
-            </div>
-            {lowestPrice && lowestPriceLabel && (
-                <span className="price__lowest">{lowestPriceLabel} {lowestPrice} {currency}</span>
+    <div className="price">
+        <div className="price__container">
+            <h3 className="price__current">{new Intl.NumberFormat(undefined, { style: 'currency', currency }).format(current)} {currency}</h3>
+            {old && (
+                <span className="price__old">{old} {currency}</span>
             )}
         </div>
-    )
+        {lowestPrice && lowestPriceLabel && (
+            <span className="price__lowest">{lowestPriceLabel} {lowestPrice} {currency}</span>
+        )}
+    </div>
 )
 
 export default Price;
