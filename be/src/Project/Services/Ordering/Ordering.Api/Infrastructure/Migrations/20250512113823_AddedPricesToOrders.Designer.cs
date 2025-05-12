3cc53510a74992847b266df0f8bd7d0e4075338a
Sebastian Kasperek <sebastian.kasperek@eltap.com>
2025-05-12 13:47:55 +0200

    Added migration

--- be/src/Project/Services/Ordering/Ordering.Api/Infrastructure/Migrations/20250512113823_AddedPricesToOrders.Designer.cs
+++ be/src/Project/Services/Ordering/Ordering.Api/Infrastructure/Migrations/20250512113823_AddedPricesToOrders.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Ordering.Api.Infrastructure;
 
@@ -11,9 +12,11 @@ using Ordering.Api.Infrastructure;
 namespace Ordering.Api.Infrastructure.Migrations
 {
     [DbContext(typeof(OrderingContext))]
-    partial class OrderingContextModelSnapshot : ModelSnapshot
+    [Migration("20250512113823_AddedPricesToOrders")]
+    partial class AddedPricesToOrders
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
