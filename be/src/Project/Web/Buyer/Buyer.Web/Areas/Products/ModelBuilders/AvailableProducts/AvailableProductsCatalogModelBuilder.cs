0ebe1e6007f6e7e51a0751948e0da5d5384bf4d0
Sebastian Kasperek <sebastian.kasperek@eltap.com>
2025-06-10 07:40:08 +0200

    Added missing fixes

--- be/src/Project/Web/Buyer/Buyer.Web/Areas/Products/ModelBuilders/AvailableProducts/AvailableProductsCatalogModelBuilder.cs
+++ be/src/Project/Web/Buyer/Buyer.Web/Areas/Products/ModelBuilders/AvailableProducts/AvailableProductsCatalogModelBuilder.cs
@@ -101,7 +101,7 @@ namespace Buyer.Web.Areas.Products.ModelBuilders.AvailableProducts
                                 SleepAreaSize = x.SleepAreaSize,
                                 ExtraPacking = x.ExtraPacking,
                                 PaletteSize = x.PaletteSize,
-                                IsOutlet = outletItems.Data.Any(y => y.ProductId == x.Id).ToYesOrNo()
+                                IsOutlet = (outletItems.Data.FirstOrDefault(y => y.ProductId == x.Id)?.AvailableQuantity > 0).ToYesOrNo()
                             }),
                             new PriceClient
                             {
