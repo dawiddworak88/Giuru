987670a11e3b1e0175a218c9b78eac917910f47e
Piotr Zaranek <piotrek.zaranek@gmail.com>
2025-06-16 09:40:30 +0200

    Added React

--- fe/projects/AspNetCore/src/shared/components/Filters/FilterCollector.js
+++ fe/projects/AspNetCore/src/shared/components/Filters/FilterCollector.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { Button, Checkbox, Chip, Drawer, ListItemText, MenuItem, Select, Typography } from "@mui/material";
 import Arrow from "../../Icons/Arrow";
