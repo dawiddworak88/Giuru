6e59a13a38e6594049faebab035e020cf674d6b5
Piotr Zaranek <piotrek.zaranek@gmail.com>
2025-06-24 08:26:16 +0200

    Added constants file for sorting

---
+++ be/src/Project/Web/Buyer/Buyer.Web/Shared/Definitions/Filters/SortingConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace Buyer.Web.Shared.Definitions.Filters
+{
+    public static class SortingConstants
+    {
+        public static readonly string Default = "default";
+        public static readonly string Name = "name";
+        public static readonly string Newest = "new";
+    }
+}
