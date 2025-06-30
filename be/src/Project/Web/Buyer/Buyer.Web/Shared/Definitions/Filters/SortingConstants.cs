5dbb06e6d02c2e7097778cd17830415937214bbd
Piotr Zaranek <piotrek.zaranek@gmail.com>
2025-06-30 10:14:14 +0200

    Corrected value

--- be/src/Project/Web/Buyer/Buyer.Web/Shared/Definitions/Filters/SortingConstants.cs
+++ be/src/Project/Web/Buyer/Buyer.Web/Shared/Definitions/Filters/SortingConstants.cs
@@ -4,6 +4,6 @@
     {
         public static readonly string Default = "default";
         public static readonly string Name = "name";
-        public static readonly string Newest = "new";
+        public static readonly string Newest = "newest";
     }
 }
