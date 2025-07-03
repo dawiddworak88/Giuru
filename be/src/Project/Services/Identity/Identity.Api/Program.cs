75507a21a813845956e26a0ebc572e9228b06b87
Sebastian Kasperek <sebastian.kasperek@eltap.com>
2025-07-03 13:40:39 +0200

    Update Program.cs

--- be/src/Project/Services/Identity/Identity.Api/Program.cs
+++ be/src/Project/Services/Identity/Identity.Api/Program.cs
@@ -141,12 +141,12 @@ var app = builder.Build();
 
 IdentityModelEventSource.ShowPII = true;
 
-app.UseHttpsRedirection();
-
 app.UseForwardedHeaders(new ForwardedHeadersOptions
 {
     ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
 });
+
+app.UseHttpsRedirection();
 
 app.UseGeneralException();
 
@@ -219,8 +219,12 @@ app.MapHealthChecks("/liveness", new HealthCheckOptions
 
 app.Use(async (context, next) =>
 {
-    Console.WriteLine($"Scheme: {context.Request.Scheme}");
-    Console.WriteLine($"Headers: {string.Join(", ", context.Request.Headers.Select(h => $"{h.Key}: {h.Value}"))}");
+    if (context.Request.Path.StartsWithSegments("/signin-oidc"))
+    {
+        var xfp = context.Request.Headers["X-Forwarded-Proto"].ToString();
+        var scheme = context.Request.Scheme;
+        Console.WriteLine($"[Logowanie] X-Forwarded-Proto: {xfp}, Request.Scheme: {scheme}");
+    }
     await next();
 });
 
