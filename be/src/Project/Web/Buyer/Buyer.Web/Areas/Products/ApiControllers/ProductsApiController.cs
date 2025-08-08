6fcbece5b9837725c7693ff2fc47bb48aee9e97b
Sebastian Kasperek <sebastian.kasperek@eltap.com>
2025-08-08 12:03:51 +0200

    Revert "Removed redundant method"
    This reverts commit 9b3eebd14d6487e477e75ebfb2b96d55584a4f79.

--- be/src/Project/Web/Buyer/Buyer.Web/Areas/Products/ApiControllers/ProductsApiController.cs
+++ be/src/Project/Web/Buyer/Buyer.Web/Areas/Products/ApiControllers/ProductsApiController.cs
@@ -53,6 +53,7 @@ namespace Buyer.Web.Areas.Products.ApiControllers
         private readonly IOptions<AppSettings> _options;
         private readonly IPriceService _priceService;
         private readonly IProductColorsService _productColorsService;
+        private readonly ILogger<ProductsApiController> _logger;
 
         public ProductsApiController(
             IProductsService productsService,
@@ -65,7 +66,8 @@ namespace Buyer.Web.Areas.Products.ApiControllers
             IOptions<AppSettings> options,
             IPriceService priceService,
             LinkGenerator linkGenerator,
-            IProductColorsService productColorsService)
+            IProductColorsService productColorsService,
+            ILogger<ProductsApiController> logger)
         {
             _productsService = productsService;
             _productsRepository = productsRepository;
@@ -79,6 +81,7 @@ namespace Buyer.Web.Areas.Products.ApiControllers
             _options = options;
             _priceService = priceService;
             _productColorsService = productColorsService;
+            _logger = logger;
         }
 
         [HttpGet]
@@ -439,6 +442,71 @@ namespace Buyer.Web.Areas.Products.ApiControllers
             }
 
             return StatusCode((int)HttpStatusCode.OK, Enumerable.Empty<ProductQuantitiesResponseModel>());
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetPrice(string sku)
+        {
+            var token = await HttpContext.GetTokenAsync(ApiExtensionsConstants.TokenName);
+            var language = CultureInfo.CurrentUICulture.Name;
+
+            var product = await _productsRepository.GetProductAsync(sku, language, token);
+
+            if (string.IsNullOrWhiteSpace(_options.Value.GrulaAccessToken) is false)
+            {
+                var outletItem = await _outletRepository.GetOutletProductBySkuAsync(token, language, sku);
+
+                try
+                {
+                    var price = await _priceService.GetPrice(
+                    _options.Value.GrulaAccessToken,
+                    DateTime.UtcNow,
+                    new PriceProduct
+                    {
+                        PrimarySku = product.PrimaryProductSku,
+                        FabricsGroup = _productsService.GetFirstAvailableAttributeValue(product.ProductAttributes, _options.Value.PossiblePriceGroupAttributeKeys),
+                        ExtraPacking = _productsService.GetFirstAvailableAttributeValue(product.ProductAttributes, _options.Value.PossibleExtraPackingAttributeKeys),
+                        SleepAreaSize = _productsService.GetSleepAreaSize(product.ProductAttributes),
+                        PaletteSize = _productsService.GetFirstAvailableAttributeValue(product.ProductAttributes, _options.Value.PossiblePaletteSizeAttributeKeys),
+                        Size = _productsService.GetSize(product.ProductAttributes),
+                        PointsOfLight = _productsService.GetFirstAvailableAttributeValue(product.ProductAttributes, _options.Value.PossiblePointsOfLightAttributeKeys),
+                        LampshadeType = _productsService.GetFirstAvailableAttributeValue(product.ProductAttributes, _options.Value.PossibleLampshadeTypeAttributeKeys),
+                        LampshadeSize = _productsService.GetFirstAvailableAttributeValue(product.ProductAttributes, _options.Value.PossibleLampshadeSizeAttributeKeys),
+                        LinearLight = _productsService.GetFirstAvailableAttributeValue(product.ProductAttributes, _options.Value.PossibleLinearLightAttributeKeys).ToYesOrNo(),
+                        Mirror = _productsService.GetFirstAvailableAttributeValue(product.ProductAttributes, _options.Value.PossibleMirrorAttributeKeys).ToYesOrNo(),
+                        Shape = _productsService.GetFirstAvailableAttributeValue(product.ProductAttributes, _options.Value.PossibleShapeAttributeKeys),
+                        PrimaryColor = await _productColorsService.ToEnglishAsync(_productsService.GetFirstAvailableAttributeValue(product.ProductAttributes, _options.Value.PossiblePrimaryColorAttributeKeys)),
+                        SecondaryColor = await _productColorsService.ToEnglishAsync(_productsService.GetFirstAvailableAttributeValue(product.ProductAttributes, _options.Value.PossibleSecondaryColorAttributeKeys)),
+                        ShelfType = _productsService.GetFirstAvailableAttributeValue(product.ProductAttributes, _options.Value.PossibleShelfTypeAttributeKeys),
+                        IsOutlet = (outletItem?.AvailableQuantity > 0).ToYesOrNo()
+                    },
+                    new PriceClient
+                    {
+                        Id = string.IsNullOrWhiteSpace(User.FindFirst(ClaimsEnrichmentConstants.ClientIdClaimType)?.Value) ? null : Guid.Parse(User.FindFirst(ClaimsEnrichmentConstants.ClientIdClaimType)?.Value),
+                        Name = User.Identity?.Name,
+                        CurrencyCode = User.FindFirst(ClaimsEnrichmentConstants.CurrencyClaimType)?.Value,
+                        ExtraPacking = User.FindFirst(ClaimsEnrichmentConstants.ExtraPackingClaimType)?.Value,
+                        PaletteLoading = User.FindFirst(ClaimsEnrichmentConstants.PaletteLoadingClaimType)?.Value,
+                        Country = User.FindFirst(ClaimsEnrichmentConstants.CountryClaimType)?.Value,
+                        DeliveryZipCode = User.FindFirst(ClaimsEnrichmentConstants.ZipCodeClaimType)?.Value
+                    });
+
+                    if (price is not null)
+                    {
+                        return StatusCode((int)HttpStatusCode.OK, new PriceResponseModel
+                        {
+                            CurrencyCode = price.CurrencyCode,
+                            CurrentPrice = price.CurrentPrice
+                        });
+                    }
+                }
+                catch
+                {
+                    return StatusCode((int)HttpStatusCode.OK);
+                }
+            }
+
+            return StatusCode((int)HttpStatusCode.OK);
         }
     } 
 }
