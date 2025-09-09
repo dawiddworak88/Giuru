97b7f45e6072b89db86d0d63d424ddfd62548e61
Piotr Zaranek <piotrek.zaranek@gmail.com>
2025-09-09 08:09:33 +0200

    Added slidesToSlide properties for all devices

--- fe/projects/AspNetCore/src/shared/components/Carousel/CarouselConstants.js
+++ fe/projects/AspNetCore/src/shared/components/Carousel/CarouselConstants.js
@@ -3,19 +3,23 @@ export default class CarouselConstants {
         return {
             superLargeDesktop: {
                 breakpoint: { max: 4000, min: 3000 },
-                items: 7
+                items: 7,
+                slidesToSlide: 7
             },
             desktop: {
                 breakpoint: { max: 3000, min: 1024 },
-                items: 4
+                items: 4,
+                slidesToSlide: 4
             },
             tablet: {
                 breakpoint: { max: 1024, min: 464 },
-                items: 2
+                items: 2,
+                slidesToSlide: 2
             },
             mobile: {
                 breakpoint: { max: 464, min: 0 },
-                items: 1
+                items: 1,
+                slidesToSlide: 1
             }
         };
     }
