d309b87666e6c551478778959be487338adb2aff
Piotr Zaranek <piotrek.zaranek@gmail.com>
2025-09-11 15:21:45 +0200

    Added CopyButton component

---
+++ fe/projects/AspNetCore/src/shared/components/CopyButton/CopyButton.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { ContentCopy } from "@mui/icons-material";
+import { IconButton, Tooltip } from "@mui/material";
+import { toast } from "react-toastify";
+import CopyConstants from "../../constants/CopyConstants";
+
+const CopyButton = ({
+    copiedText,
+    copyTextError,
+    copyToClipboardText,
+    text
+}) => {
+    const [copied, setCopied] = useState(false);
+
+    const handleOnCopy = async () => {
+        try {
+            if (navigator.clipboard?.writeText && window.isSecureContext) {
+                await navigator.clipboard.writeText(text)
+                setCopied(true)
+                setTimeout(() => setCopied(false), CopyConstants.copyFeedbackDuration())
+                return
+            }
+        } catch (err) { 
+            toast.error(copyTextError)
+        }
+        fallbackCopyTextToClipboard()
+    };
+
+    const fallbackCopyTextToClipboard = () => {
+        const ta = document.createElement('textarea')
+        ta.value = text
+        ta.setAttribute('readonly', '')
+        ta.style.position = 'absolute'
+        ta.style.left = '-9999px'
+        document.body.appendChild(ta)
+        ta.select()
+        try {
+            document.execCommand('copy')
+            setCopied(true)
+            setTimeout(() => setCopied(false), CopyConstants.copyFeedbackDuration())
+        } finally {
+            document.body.removeChild(ta)
+        }
+    };
+
+    return (
+        <Tooltip title={copied ? copiedText : copyToClipboardText}>
+            <IconButton
+                size="small"
+                disableRipple
+                className="copy-button"
+                onClick={handleOnCopy}
+            >
+                <ContentCopy fontSize="1rem" />
+            </IconButton>
+        </Tooltip>
+    )
+}
+
+CopyButton.propTypes = {
+    copiedText: PropTypes.string.isRequired,
+    copyTextError: PropTypes.string.isRequired,
+    copyToClipboardText: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired
+}
+
+export default CopyButton
