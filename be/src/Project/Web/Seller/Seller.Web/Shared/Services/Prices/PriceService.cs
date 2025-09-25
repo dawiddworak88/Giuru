effd43ee63e77f63d3ba59e94a1da0f16f482583
Piotr Zaranek <piotrek.zaranek@gmail.com>
2025-09-25 08:13:03 +0200

    Added console write line for products

--- be/src/Project/Web/Seller/Seller.Web/Shared/Services/Prices/PriceService.cs
+++ be/src/Project/Web/Seller/Seller.Web/Shared/Services/Prices/PriceService.cs
@@ -75,6 +75,7 @@ namespace Seller.Web.Shared.Services.Prices
             IEnumerable<PriceProduct> products,
             PriceClient client)
         {
+            Console.WriteLine(JsonSerializer.Serialize(products));
             var productList = (products ?? Enumerable.Empty<PriceProduct>()).ToList();
             var result = new Price[productList.Count];
 
