31d978d5b242c2edae993bfaad3d3753f0fc4128
Piotr Zaranek <piotrek.zaranek@gmail.com>
2025-09-25 15:09:53 +0200

    Added getting prices

--- be/src/Project/Web/Seller/Seller.Web/Areas/Orders/ApiControllers/OrderFileApiController.cs
+++ be/src/Project/Web/Seller/Seller.Web/Areas/Orders/ApiControllers/OrderFileApiController.cs
@@ -9,6 +9,10 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Seller.Web.Areas.Clients.Repositories.DeliveryAddresses;
+using Seller.Web.Areas.Clients.Repositories.FieldValues;
+using Seller.Web.Areas.Global.Repositories;
 using Seller.Web.Areas.Media.ApiRequestModels;
 using Seller.Web.Areas.Orders.ApiResponseModels;
 using Seller.Web.Areas.Orders.Definitions;
@@ -18,9 +22,16 @@ using Seller.Web.Areas.Orders.Repositories.Orders;
 using Seller.Web.Areas.Orders.Services.OrderFiles;
 using Seller.Web.Areas.Shared.Repositories.Media;
 using Seller.Web.Areas.Shared.Repositories.Products;
+using Seller.Web.Shared.Configurations;
 using Seller.Web.Shared.Definitions;
 using Seller.Web.Shared.DomainModels.Media;
+using Seller.Web.Shared.DomainModels.Prices;
+using Seller.Web.Shared.Repositories.Clients;
 using Seller.Web.Shared.Repositories.Inventory;
+using Seller.Web.Shared.Services.Prices;
+using Seller.Web.Shared.Services.ProductColors;
+using Seller.Web.Shared.Services.Products;
+using Seller.Web.Areas.Global.DomainModels;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
@@ -33,16 +44,25 @@ namespace Seller.Web.Areas.Orders.ApiControllers
     [Area("Orders")]
     public class OrderFileApiController : BaseApiController
     {
-        private readonly IOrderFileService orderFileService;
-        private readonly IProductsRepository productsRepository;
-        private readonly IBasketRepository basketRepository;
-        private readonly LinkGenerator linkGenerator;
-        private readonly IMediaService mediaService;
-        private readonly ILogger<OrderFileApiController> logger;
-        private readonly IMediaItemsRepository mediaRepository;
-        private readonly IOrdersRepository ordersRepository;
-        private readonly IInventoryRepository inventoryRepository;
-        private readonly IStringLocalizer<OrderResources> orderLocalizer;
+        private readonly IOrderFileService _orderFileService;
+        private readonly IProductsRepository _productsRepository;
+        private readonly IBasketRepository _basketRepository;
+        private readonly LinkGenerator _linkGenerator;
+        private readonly IMediaService _mediaService;
+        private readonly ILogger<OrderFileApiController> _logger;
+        private readonly IMediaItemsRepository _mediaRepository;
+        private readonly IOrdersRepository _ordersRepository;
+        private readonly IInventoryRepository _inventoryRepository;
+        private readonly IStringLocalizer<OrderResources> _orderLocalizer;
+        private readonly IPriceService _priceService;
+        private readonly IProductsService _productsService;
+        private readonly IProductColorsService _productColorsService;
+        private readonly IOptions<AppSettings> _options;
+        private readonly IClientsRepository _clientsRepository;
+        private readonly ICountriesRepository _countriesRepository;
+        private readonly IClientFieldValuesRepository _clientFieldValuesRepository;
+        private readonly IClientAddressesRepository _clientAddressesRepository;
+        private readonly ICurrenciesRepository _currenciesRepository;
 
         public OrderFileApiController(
             IOrderFileService orderFileService,
@@ -54,35 +74,47 @@ namespace Seller.Web.Areas.Orders.ApiControllers
             IOrdersRepository ordersRepository,
             IInventoryRepository inventoryRepository,
             ILogger<OrderFileApiController> logger,
-            IStringLocalizer<OrderResources> orderLocalizer)
+            IStringLocalizer<OrderResources> orderLocalizer,
+            IOptions<AppSettings> options,
+            IClientsRepository clientsRepository,
+            ICountriesRepository countriesRepository,
+            IClientFieldValuesRepository clientFieldValuesRepository,
+            IClientAddressesRepository clientAddressesRepository,
+            ICurrenciesRepository currenciesRepository)
         {
-            this.orderFileService = orderFileService;
-            this.productsRepository = productsRepository;
-            this.basketRepository = basketRepository;
-            this.linkGenerator = linkGenerator;
-            this.mediaService = mediaService;
-            this.logger = logger;
-            this.mediaRepository = mediaRepository;
-            this.ordersRepository = ordersRepository;
-            this.inventoryRepository = inventoryRepository;
-            this.orderLocalizer = orderLocalizer;
+            _orderFileService = orderFileService;
+            _productsRepository = productsRepository;
+            _basketRepository = basketRepository;
+            _linkGenerator = linkGenerator;
+            _mediaService = mediaService;
+            _logger = logger;
+            _mediaRepository = mediaRepository;
+            _ordersRepository = ordersRepository;
+            _inventoryRepository = inventoryRepository;
+            _orderLocalizer = orderLocalizer;
+            _options = options;
+            _clientsRepository = clientsRepository;
+            _countriesRepository = countriesRepository;
+            _clientFieldValuesRepository = clientFieldValuesRepository;
+            _clientAddressesRepository = clientAddressesRepository;
+            _currenciesRepository = currenciesRepository;
         }
 
         [HttpPost]
         public async Task<IActionResult> Index([FromForm] UploadMediaRequestModel model)
         {
-            var importedOrderLines = this.orderFileService.ImportOrderLines(model.File);
+            var importedOrderLines = _orderFileService.ImportOrderLines(model.File);
 
             var skus = importedOrderLines.OrEmptyIfNull().Select(x => x.Sku).Distinct();
 
             var token = await HttpContext.GetTokenAsync(ApiExtensionsConstants.TokenName);
             var language = CultureInfo.CurrentUICulture.Name;
 
-            var products = await this.productsRepository.GetProductsBySkusAsync(token, language, skus);
+            var products = await _productsRepository.GetProductsBySkusAsync(token, language, skus);
 
             if (!products.OrEmptyIfNull().Any())
             {
-                return StatusCode((int)HttpStatusCode.BadRequest, new { Message = this.orderLocalizer.GetString("ProductsNotFound") });
+                return StatusCode((int)HttpStatusCode.BadRequest, new { Message = _orderLocalizer.GetString("ProductsNotFound") });
             }
 
             var productBySku = products
@@ -90,7 +122,7 @@ namespace Seller.Web.Areas.Orders.ApiControllers
                 .ToDictionary(g => g.Sku, g => g);
 
             var productIds = products.OrEmptyIfNull().Select(x => x.Id).Distinct().ToList();
-            var stockAvailableProducts = await this.inventoryRepository.GetAvailbleProductsByProductIdsAsync(token, language, productIds);
+            var stockAvailableProducts = await _inventoryRepository.GetAvailbleProductsByProductIdsAsync(token, language, productIds);
 
             var stockByProductId = stockAvailableProducts
                .OrEmptyIfNull()
@@ -98,35 +130,117 @@ namespace Seller.Web.Areas.Orders.ApiControllers
 
             var basketItems = new List<BasketItem>();
 
-            foreach (var orderLine in importedOrderLines.OrEmptyIfNull())
+            var prices = Enumerable.Empty<Price>();
+
+            if (string.IsNullOrWhiteSpace(_options.Value.GrulaAccessToken) is false)
+            {
+                var countries = await _countriesRepository.GetAsync(token, _options.Value.DefaultCulture, $"{nameof(Country.CreatedDate)} desc");
+
+                var client = await _clientsRepository.GetClientAsync(token, _options.Value.DefaultCulture, model.ClientId);
+
+                string clientCountryName = null;
+
+                if (client.CountryId.HasValue)
+                {
+                    clientCountryName = countries.FirstOrDefault(c => c.Id == client.CountryId)?.Name;
+                }
+
+                string deliveryZipCode = null;
+
+                if (client.DefaultDeliveryAddressId.HasValue)
+                {
+                    var clientAddress = await _clientAddressesRepository.GetAsync(token, _options.Value.DefaultCulture, client.DefaultDeliveryAddressId);
+
+                    if (clientAddress is not null)
+                    {
+                        var deliveryCountry = countries.FirstOrDefault(c => c.Id == clientAddress.CountryId);
+
+                        if (deliveryCountry is not null)
+                        {
+                            deliveryZipCode = $"{clientAddress.PostCode} ({clientAddress.City}, {deliveryCountry.Name})";
+                        }
+                    }
+                }
+
+                var clientFieldValues = await _clientFieldValuesRepository.GetAsync(token, _options.Value.DefaultCulture, model.ClientId);
+
+                var currency = await _currenciesRepository.GetAsync(token, _options.Value.DefaultCulture, client?.PreferedCurrencyId);
+
+                var priceProducts = products.Select(async x => new PriceProduct
+                {
+                    PrimarySku = x.PrimaryProductSku,
+                    FabricsGroup = _productsService.GetFirstAvailableAttributeValue(x.ProductAttributes, _options.Value.PossiblePriceGroupAttributeKeys),
+                    ExtraPacking = _productsService.GetFirstAvailableAttributeValue(x.ProductAttributes, _options.Value.PossibleExtraPackingAttributeKeys).ToYesOrNo(),
+                    SleepAreaSize = _productsService.GetSleepAreaSize(x.ProductAttributes),
+                    PaletteSize = _productsService.GetFirstAvailableAttributeValue(x.ProductAttributes, _options.Value.PossiblePaletteSizeAttributeKeys),
+                    Size = _productsService.GetSize(x.ProductAttributes),
+                    PointsOfLight = _productsService.GetFirstAvailableAttributeValue(x.ProductAttributes, _options.Value.PossiblePointsOfLightAttributeKeys),
+                    LampshadeType = _productsService.GetFirstAvailableAttributeValue(x.ProductAttributes, _options.Value.PossibleLampshadeTypeAttributeKeys),
+                    LampshadeSize = _productsService.GetFirstAvailableAttributeValue(x.ProductAttributes, _options.Value.PossibleLampshadeSizeAttributeKeys),
+                    LinearLight = _productsService.GetFirstAvailableAttributeValue(x.ProductAttributes, _options.Value.PossibleLinearLightAttributeKeys).ToYesOrNo(),
+                    Mirror = _productsService.GetFirstAvailableAttributeValue(x.ProductAttributes, _options.Value.PossibleMirrorAttributeKeys).ToYesOrNo(),
+                    Shape = _productsService.GetFirstAvailableAttributeValue(x.ProductAttributes, _options.Value.PossibleShapeAttributeKeys),
+                    PrimaryColor = await _productColorsService.ToEnglishAsync(_productsService.GetFirstAvailableAttributeValue(x.ProductAttributes, _options.Value.PossiblePrimaryColorAttributeKeys)),
+                    SecondaryColor = await _productColorsService.ToEnglishAsync(_productsService.GetFirstAvailableAttributeValue(x.ProductAttributes, _options.Value.PossibleSecondaryColorAttributeKeys)),
+                    ShelfType = _productsService.GetFirstAvailableAttributeValue(x.ProductAttributes, _options.Value.PossibleShelfTypeAttributeKeys)
+                });
+
+                prices = await _priceService.GetPrices(
+                    DateTime.UtcNow,
+                    await Task.WhenAll(priceProducts),
+                    new PriceClient
+                    {
+                        Name = client?.Name,
+                        CurrencyCode = currency?.CurrencyCode,
+                        ExtraPacking = clientFieldValues.FirstOrDefault(x => x.FieldName == ClaimsEnrichmentConstants.ExtraPackingClientFieldName)?.FieldValue.ToYesOrNo(),
+                        PaletteLoading = clientFieldValues.FirstOrDefault(x => x.FieldName == ClaimsEnrichmentConstants.PaletteLoadingClientFieldName)?.FieldValue.ToYesOrNo(),
+                        Country = clientCountryName,
+                        DeliveryZipCode = deliveryZipCode
+                    });
+            }
+
+            var linesWithPrices = importedOrderLines.OrEmptyIfNull().Zip(prices);
+
+            foreach (var (orderLine, price) in linesWithPrices)
             {
                 if (!productBySku.TryGetValue(orderLine.Sku, out var product) || product == null)
                 {
-                    this.logger.LogError($"Product for SKU {orderLine.Sku} and language {language} couldn't be found.");
+                    _logger.LogError($"Product for SKU {orderLine.Sku} and language {language} couldn't be found.");
                     continue;
                 }
 
-                var availableStock = stockByProductId.TryGetValue(product.Id, out var qty) ? qty : 0;
+                var availableStock = stockByProductId.GetValueOrDefault(product.Id);
 
                 var stockQuantity = Math.Min(orderLine.Quantity, (double)availableStock);
                 var quantity = orderLine.Quantity - stockQuantity;
+
+                var firstImage = product.Images.OrEmptyIfNull().FirstOrDefault();
+                var pictureUrl = firstImage != Guid.Empty
+                    ? _mediaService.GetMediaUrl(firstImage, OrdersConstants.Basket.BasketProductImageMaxWidth)
+                    : null;
+
+                var unitPrice = price?.CurrentPrice ?? 0m;
+                var totalPrice = unitPrice * (decimal)orderLine.Quantity;
 
                 var basketItem = new BasketItem
                 {
                     ProductId = product.Id,
                     ProductSku = product.Sku,
                     ProductName = product.Name,
-                    PictureUrl = product.Images.OrEmptyIfNull().Any() ? this.mediaService.GetMediaUrl(product.Images.First(), OrdersConstants.Basket.BasketProductImageMaxWidth) : null,
+                    PictureUrl = pictureUrl,
                     Quantity = quantity,
                     StockQuantity = stockQuantity,
                     ExternalReference = orderLine.ExternalReference,
-                    MoreInfo = orderLine.MoreInfo
+                    MoreInfo = orderLine.MoreInfo,
+                    UnitPrice = unitPrice,
+                    Price = totalPrice,
+                    Currency = price?.CurrencyCode ?? string.Empty,
                 };
 
                 basketItems.Add(basketItem);
             }
 
-            var basket = await this.basketRepository.SaveAsync(
+            var basket = await _basketRepository.SaveAsync(
                 await HttpContext.GetTokenAsync(ApiExtensionsConstants.TokenName),
                 CultureInfo.CurrentUICulture.Name,
                 model.Id,
@@ -142,7 +256,7 @@ namespace Seller.Web.Areas.Orders.ApiControllers
                 basketResponseModel.Items = basket.Items.OrEmptyIfNull().Select(x => new BasketItemResponseModel
                 {
                     ProductId = x.ProductId,
-                    ProductUrl = this.linkGenerator.GetPathByAction("Edit", "Product", new { Area = "Products", culture = CultureInfo.CurrentUICulture.Name, Id = x.ProductId }),
+                    ProductUrl = _linkGenerator.GetPathByAction("Edit", "Product", new { Area = "Products", culture = CultureInfo.CurrentUICulture.Name, Id = x.ProductId }),
                     Name = x.ProductName,
                     Sku = x.ProductSku,
                     Quantity = x.Quantity,
@@ -151,11 +265,14 @@ namespace Seller.Web.Areas.Orders.ApiControllers
                     ExternalReference = x.ExternalReference,
                     ImageSrc = x.PictureUrl,
                     ImageAlt = x.ProductName,
-                    MoreInfo = x.MoreInfo
+                    MoreInfo = x.MoreInfo,
+                    UnitPrice = x.UnitPrice,
+                    Price = x.Price,
+                    Currency = x.Currency
                 });
             }
 
-            return this.StatusCode((int)HttpStatusCode.OK, basketResponseModel);
+            return StatusCode((int)HttpStatusCode.OK, basketResponseModel);
         }
 
         [HttpGet]
@@ -164,14 +281,14 @@ namespace Seller.Web.Areas.Orders.ApiControllers
             var token = await HttpContext.GetTokenAsync(ApiExtensionsConstants.TokenName);
             var language = CultureInfo.CurrentUICulture.Name;
 
-            var productFiles = await this.ordersRepository.GetOrderFilesAsync(token, language, id, pageIndex, itemsPerPage, searchTerm, $"{nameof(OrderFile.CreatedDate)} desc");
+            var productFiles = await _ordersRepository.GetOrderFilesAsync(token, language, id, pageIndex, itemsPerPage, searchTerm, $"{nameof(OrderFile.CreatedDate)} desc");
 
             var filesModel = new List<FileItem>();
             var filesIds = productFiles.Data.Select(x => x.Id);
 
             if (productFiles is not null && filesIds.Any())
             {
-                var files = await this.mediaRepository.GetMediaItemsAsync(filesIds, language, FilesConstants.DefaultPageIndex, FilesConstants.DefaultPageSize, token);
+                var files = await _mediaRepository.GetMediaItemsAsync(filesIds, language, FilesConstants.DefaultPageIndex, FilesConstants.DefaultPageSize, token);
 
                 foreach (var file in files.Data.OrEmptyIfNull())
                 {
@@ -180,10 +297,10 @@ namespace Seller.Web.Areas.Orders.ApiControllers
                         Id = file.Id,
                         Name = file.Name,
                         Filename = file.Filename,
-                        Url = this.mediaService.GetNonCdnMediaUrl(file.Id),
+                        Url = _mediaService.GetNonCdnMediaUrl(file.Id),
                         Description = file.Description ?? "-",
                         IsProtected = file.IsProtected,
-                        Size = this.mediaService.ConvertToMB(file.Size),
+                        Size = _mediaService.ConvertToMB(file.Size),
                         LastModifiedDate = file.LastModifiedDate,
                         CreatedDate = file.CreatedDate
                     };
@@ -197,7 +314,7 @@ namespace Seller.Web.Areas.Orders.ApiControllers
                 Data = filesModel
             };
 
-            return this.StatusCode((int)HttpStatusCode.OK, pagedFiles);
+            return StatusCode((int)HttpStatusCode.OK, pagedFiles);
         }
     }
 }
