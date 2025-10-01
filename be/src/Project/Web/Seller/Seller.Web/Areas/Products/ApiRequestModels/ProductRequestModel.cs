7667f3e05c0847732b03677f2cae045abd932f4e
Piotr Zaranek <piotrek.zaranek@gmail.com>
2025-10-01 14:38:26 +0200

    Added ProductRequestModel

---
+++ be/src/Project/Web/Seller/Seller.Web/Areas/Products/ApiRequestModels/ProductRequestModel.cs
@@ -0,0 +1,10 @@
+﻿using Foundation.ApiExtensions.Models.Request;
+using System;
+
+namespace Seller.Web.Areas.Products.ApiRequestModels
+{
+    public class ProductRequestModel : RequestModelBase
+    {
+        public Guid? SellerId { get; set; }
+    }
+}
