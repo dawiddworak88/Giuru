da2f32ba55e247290b9c9a9b8461e4ff5f73138e
Piotr Zaranek <piotrek.zaranek@gmail.com>
2025-10-07 08:52:01 +0200

    Added specific domain exception

---
+++ be/src/Infrastructure/Extensions/Foundation.Extensions/Exceptions/BadRequestException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Foundation.Extensions.Exceptions
+{
+    public class BadRequestException : Exception
+    {
+        public BadRequestException(string message) : base(message) { }
+    }
+}
