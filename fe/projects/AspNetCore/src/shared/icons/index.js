908638b3869be17457836fa0c89d9fecdfad7262
Sebastian Kasperek <sebastian.kasperek@eltap.com>
2025-10-16 22:02:27 +0200

    test2

--- fe/projects/AspNetCore/src/shared/icons/index.js
+++ fe/projects/AspNetCore/src/shared/icons/index.js
@@ -2,5 +2,5 @@ export { default as DeliveryIcon } from "./Delivery";
 export { default as FiltersIcon } from "./Filters";
 export { default as RemoveIcon } from "./Remove";
 export { default as ArrowIcon } from "./Arrow";
-export { default as CheckboxIcon } from "./Checkbox";
+export { default as CheckboxIcon } from "./Checkbox2";
 export { default as CheckboxCheckedIcon } from "./CheckboxChecked";
