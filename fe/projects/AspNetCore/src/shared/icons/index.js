ac810592511867b97507a6aa5f1e06aae63e2cc0
Sebastian Kasperek <sebastian.kasperek@eltap.com>
2025-10-16 22:36:53 +0200

    Icon test

--- fe/projects/AspNetCore/src/shared/icons/index.js
+++ fe/projects/AspNetCore/src/shared/icons/index.js
@@ -3,4 +3,4 @@ export { default as FiltersIcon } from "./Filters";
 export { default as RemoveIcon } from "./Remove";
 export { default as ArrowIcon } from "./Arrow";
 export { default as CheckboxIcon } from "./CheckBox";
-export { default as CheckboxCheckedIcon } from "./CheckBoxChecked";
+export { default as CheckboxCheckedIcon } from "./CheckBoxChecked2";
