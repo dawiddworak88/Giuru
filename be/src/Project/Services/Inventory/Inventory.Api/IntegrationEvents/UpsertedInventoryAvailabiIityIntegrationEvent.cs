a6665c5aa2f71b5a9ac3953f7536fa3c1afaf0e7
Sebastian Kasperek <sebastian.kasperek@eltap.com>
2026-03-23 12:21:36 +0100

    Added additional events for lead times

---
+++ be/src/Project/Services/Inventory/Inventory.Api/IntegrationEvents/UpsertedInventoryAvailabiIityIntegrationEvent.cs
@@ -0,0 +1,9 @@
+﻿using Foundation.EventBus.Events;
+
+namespace Inventory.Api.IntegrationEvents
+{
+    public class UpsertedInventoryAvailabiIityIntegrationEvent : IntegrationEvent
+    {
+        public string[] ProductSkus { get; set; }
+    }
+}
