3c4e508fc4c96d1b01e1ac602a72abc0720b24c0
Sebastian Kasperek <sebastian.kasperek@eltap.com>
2026-03-26 13:54:33 +0100

    Added missing migrations

---
+++ be/src/Project/Services/Ordering/Ordering.Api/Infrastructure/Migrations/20260326125352_AddedExpectedLeadTime.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ordering.Api.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedExpectedLeadTime : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ExpectedLeadTime",
+                table: "OrderItems",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExpectedLeadTime",
+                table: "OrderItems");
+        }
+    }
+}
--- be/src/Project/Services/Ordering/Ordering.Api/Program.cs
+++ be/src/Project/Services/Ordering/Ordering.Api/Program.cs
@@ -79,14 +79,14 @@ if (!builder.Environment.IsDevelopment())
         true,
         true);
 }
-
+/*
 builder.Services.AddDataProtection().UseCryptographicAlgorithms(
     new AuthenticatedEncryptorConfiguration
     {
         EncryptionAlgorithm = EncryptionAlgorithm.AES_256_CBC,
         ValidationAlgorithm = ValidationAlgorithm.HMACSHA256
     }).PersistKeysToStackExchangeRedis(ConnectionMultiplexer.Connect(builder.Configuration["RedisUrl"]), $"{Assembly.GetExecutingAssembly().GetName().Name}-DataProtection-Keys");
-
+*/
 builder.Services.AddControllers(options =>
 {
     options.RespectBrowserAcceptHeader = true;
