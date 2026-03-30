e86dd5cd3a92279d0ec0f16eed9bf6810c85aedb
Sebastian Kasperek <sebastian.kasperek@eltap.com>
2026-03-30 22:37:06 +0200

    Fixed paging

--- be/src/Project/Web/Buyer/Buyer.Web/Areas/Products/ApiControllers/SearchProductsApiController.cs
+++ be/src/Project/Web/Buyer/Buyer.Web/Areas/Products/ApiControllers/SearchProductsApiController.cs
@@ -1,5 +1,6 @@
 ﻿using Buyer.Web.Areas.Products.Repositories;
 using Buyer.Web.Areas.Products.Repositories.Inventories;
+using Buyer.Web.Areas.Products.Services.DeliveryMessages;
 using Buyer.Web.Areas.Products.Services.Products;
 using Buyer.Web.Areas.Products.ViewModels.Products;
 using Buyer.Web.Shared.Configurations;
@@ -35,6 +36,7 @@ namespace Buyer.Web.Areas.Products.ApiControllers
         private readonly IOptions<AppSettings> _options;
         private readonly IPriceService _priceService;
         private readonly ILeadTimeRepository _leadTimeRepository;
+        private readonly IDeliveryMessageHelper _deliveryMessageHelper;
 
         public SearchProductsApiController(
             IProductsService productsService,
@@ -42,7 +44,8 @@ namespace Buyer.Web.Areas.Products.ApiControllers
             IInventoryRepository inventoryRepository,
             IOptions<AppSettings> options,
             IPriceService priceService,
-            ILeadTimeRepository leadTimeRepository)
+            ILeadTimeRepository leadTimeRepository,
+            IDeliveryMessageHelper deliveryMessageHelper)
         {
             _productsService = productsService;
             _inventoryRepository = inventoryRepository;
@@ -50,6 +53,7 @@ namespace Buyer.Web.Areas.Products.ApiControllers
             _options = options;
             _leadTimeRepository = leadTimeRepository;
             _priceService = priceService;
+            _deliveryMessageHelper = deliveryMessageHelper;
         }
 
         [HttpGet]
@@ -147,6 +151,8 @@ namespace Buyer.Web.Areas.Products.ApiControllers
                     }
 
                     product.LeadTimeDays = leadTimes?.Items?.FirstOrDefault(x => x.Sku == product.Sku)?.LeadTimeDays ?? 0;
+                    product.LeadTimeDeliveryMessage = _deliveryMessageHelper.GetDeliveryMessage(
+                        User.FindFirst(ClaimsEnrichmentConstants.DeliveryTypeClaimType)?.Value, product.InStock, product.ExpectedDelivery);
                     product.CanOrder = true;
                 }
 
