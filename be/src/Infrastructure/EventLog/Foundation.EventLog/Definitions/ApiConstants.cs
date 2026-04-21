ce1a436e544e84010db5fc65755e9526d47e9821
Marcin Kalinowski <marcin.kalinowski@eltap.com>
2026-04-21 13:53:33 +0200

    chore: remove unused Foundation.EventLog project (net6.0, not in solution)

--- be/src/Infrastructure/EventLog/Foundation.EventLog/Definitions/ApiConstants.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Foundation.EventLog.Definitions
-{
-    public static class ApiConstants
-    {
-        public static readonly string EventLoggingApiEndpoint = "/api/v1/eventlogging";
-    }
-}
