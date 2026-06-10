552a48b2397b23aa8687f77f58c75adb18669416
Sebastian Kasperek <sebastian.kasperek@eltap.com>
2026-06-10 13:25:15 +0200

    Add database indexes to fix catalog product indexing timeouts

--- be/src/Infrastructure/Catalog/Foundation.Catalog/Infrastructure/Products/Entities/ProductFile.cs
+++ be/src/Infrastructure/Catalog/Foundation.Catalog/Infrastructure/Products/Entities/ProductFile.cs
@@ -1,8 +1,10 @@
 ﻿using Foundation.GenericRepository.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 
 namespace Foundation.Catalog.Infrastructure.Products.Entities
 {
+    [Index(nameof(ProductId))]
     public class ProductFile : EntityMedia
     {
         public Guid ProductId { get; set; }
--- be/src/Infrastructure/Catalog/Foundation.Catalog/Infrastructure/Products/Entities/ProductImage.cs
+++ be/src/Infrastructure/Catalog/Foundation.Catalog/Infrastructure/Products/Entities/ProductImage.cs
@@ -1,8 +1,10 @@
 ﻿using Foundation.GenericRepository.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 
 namespace Foundation.Catalog.Infrastructure.Products.Entities
 {
+    [Index(nameof(ProductId))]
     public class ProductImage : EntityMedia
     {
         public Guid ProductId { get; set; }
