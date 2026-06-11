821f80314fd6fea61221f2c3c6be7114b7e029d9
Piotr Zaranek <piotrek.zaranek@gmail.com>
2026-06-11 16:17:15 +0200

    Improved validation method

--- be/src/Project/Web/Seller/Seller.Web/Areas/Orders/Services/Basket/BasketService.cs
+++ be/src/Project/Web/Seller/Seller.Web/Areas/Orders/Services/Basket/BasketService.cs
@@ -1,10 +1,13 @@
 ﻿using Foundation.Extensions.Exceptions;
+using Foundation.Extensions.ExtensionMethods;
 using Foundation.Localization;
 using Microsoft.Extensions.Localization;
+using Seller.Web.Areas.Inventory.DomainModels;
 using Seller.Web.Areas.Inventory.Repositories;
 using Seller.Web.Areas.Inventory.Repositories.Inventories;
 using Seller.Web.Areas.Orders.Repositories.Baskets;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
@@ -40,74 +43,96 @@ namespace Seller.Web.Areas.Orders.Services.Basket
         {
             var basket = await _basketRepository.GetBasketByIdAsync(token, language, basketId);
 
-            if (basket is null) throw new CustomException(_orderLocalizer.GetString("BasketNotFound"), (int)HttpStatusCode.NotFound);
-
-            if (basket.Items.Any())
+            if (basket?.Items == null || basket.Items.Any() is false)
             {
-                if (basket.Items.Any(x => x.StockQuantity > 0))
+                throw new CustomException(_orderLocalizer.GetString("BasketNotFound").Value, (int)HttpStatusCode.NotFound);
+            }
+
+            var items = basket.Items.OrEmptyIfNull().ToList();
+
+            if (items.Any(x => x.ProductId.HasValue is false))
+            {
+                throw new CustomException(_orderLocalizer.GetString("ProductsNotFound"), (int)HttpStatusCode.NotFound);
+            }
+
+            await ValidateStockTypeAsync<InventoryItem>(
+                token, language, items,
+                quantitySelector: x => x.StockQuantity,
+                fetchProducts: ids => _inventoryRepository.GetInventoryProductByProductIdsAsync(token, language, ids),
+                messageNotFound: _inventoryLocalizer.GetString("InventoryNotFound"),
+                messageQuantityError: _orderLocalizer.GetString("StockQuantityError").Value);
+
+            await ValidateStockTypeAsync<OutletItem>(
+                token, language, items,
+                quantitySelector: x => x.OutletQuantity,
+                fetchProducts: ids => _outletRepository.GetOutletProductsByProductsIdAsync(token, language, ids),
+                messageNotFound: _inventoryLocalizer.GetString("OutletNotFound"),
+                messageQuantityError: _orderLocalizer.GetString("OutletQuantityError").Value);
+        }
+
+        private async Task ValidateStockTypeAsync<T>(
+            string token,
+            string language,
+            IEnumerable<DomainModels.BasketItem> basketItems,
+            Func<DomainModels.BasketItem, double> quantitySelector,
+            Func<IEnumerable<Guid>, Task<IEnumerable<T>>> fetchProducts,
+            string messageNotFound,
+            string messageQuantityError) where T : StockItem
+        {
+            var filteredItems = basketItems
+                .Where(x => quantitySelector(x) > 0)
+                .Select(x => new BasketItem
                 {
-                    var stockItems = basket.Items.Where(x => x.StockQuantity > 0).ToList();
+                    ProductId = x.ProductId,
+                    ProductName = x.ProductName,
+                    ProductSku = x.ProductSku,
+                    Quantity = quantitySelector(x),
+                }).ToList();
 
-                    var inventoryProducts = await _inventoryRepository.GetInventoryProductByProductIdsAsync(token, language, stockItems.Select(x => x.ProductId.Value));
+            if (filteredItems.Any() is false)
+            {
+                return;
+            }
 
-                    var inventoryAvailableByProduct = inventoryProducts
-                        .GroupBy(x => x.ProductId)
-                        .ToDictionary(g => g.Key, g => g.Sum(x => x.AvailableQuantity));
+            var stockProducts = (await fetchProducts(filteredItems.Select(x => x.ProductId.Value))).OrEmptyIfNull();
 
-                    var stockByProduct = stockItems
-                        .GroupBy(x => x.ProductId.Value)
-                        .ToDictionary(g => g.Key, g => (
-                            StockQuantity: g.Sum(x => x.StockQuantity),
-                            g.First().ProductName,
-                            g.First().ProductSku
-                        ));
+            ValidateQuantities<T>(filteredItems, stockProducts, messageNotFound, messageQuantityError);
+        }
 
-                    foreach (var (productId, stockData) in stockByProduct)
-                    {
-                        if (!inventoryAvailableByProduct.TryGetValue(productId, out var inventoryProductAvailableQuantity))
-                        {
-                            throw new CustomException(_inventoryLocalizer.GetString("InventoryNotFound"), (int)HttpStatusCode.NotFound);
-                        }
+        private void ValidateQuantities<T>(IEnumerable<BasketItem> items, IEnumerable<T> stockProducts, string messageNotFound, string messageQuantityError) where T : StockItem
+        {
+            var stockItemsAvailableByProduct = stockProducts
+                .GroupBy(x => x.ProductId)
+                .ToDictionary(g => g.Key, g => g.Sum(x => x.AvailableQuantity));
 
-                        if (stockData.StockQuantity > inventoryProductAvailableQuantity)
-                        {
-                            throw new CustomException($"{_orderLocalizer.GetString("StockQuantityError").Value} {stockData.ProductName} ({stockData.ProductSku}) {_globalLocalizer.GetString("InBasket")} {stockData.StockQuantity} {_globalLocalizer.GetString("MaximalLabel")} {inventoryProductAvailableQuantity}", (int)HttpStatusCode.Conflict);
-                        }
-                    }
+            var itemsByProduct = items
+                .GroupBy(x => x.ProductId.Value)
+                .ToDictionary(g => g.Key, g => (
+                    Quantity: g.Sum(x => x.Quantity),
+                    g.First().ProductName,
+                    g.First().ProductSku
+                ));
+
+            foreach (var (productId, itemData) in itemsByProduct)
+            {
+                if (!stockItemsAvailableByProduct.TryGetValue(productId, out var stockItemAvailableQuantity))
+                {
+                    throw new CustomException(messageNotFound, (int)HttpStatusCode.NotFound);
                 }
 
-                if (basket.Items.Any(x => x.OutletQuantity > 0))
+                if (itemData.Quantity > stockItemAvailableQuantity)
                 {
-                    var outletItems = basket.Items.Where(x => x.OutletQuantity > 0).ToList();
-
-                    var outletProducts = await _outletRepository.GetOutletProductsByProductsIdAsync(token, language, outletItems.Select(x => x.ProductId.Value));
-
-                    var outletAvailableByProduct = outletProducts
-                        .GroupBy(x => x.ProductId)
-                        .ToDictionary(g => g.Key, g => g.Sum(x => x.AvailableQuantity));
-
-                    var outletByProduct = outletItems
-                        .GroupBy(x => x.ProductId.Value)
-                        .ToDictionary(g => g.Key, g => (
-                            OutletQuantity: g.Sum(x => x.OutletQuantity),
-                            g.First().ProductName,
-                            g.First().ProductSku
-                        ));
-
-                    foreach (var (productId, outletData) in outletByProduct)
-                    {
-                        if (!outletAvailableByProduct.TryGetValue(productId, out var outletProductAvailableQuantity))
-                        {
-                            throw new CustomException(_inventoryLocalizer.GetString("OutletNotFound"), (int)HttpStatusCode.NotFound);
-                        }
-
-                        if (outletData.OutletQuantity > outletProductAvailableQuantity)
-                        {
-                            throw new CustomException($"{_orderLocalizer.GetString("OutletQuantityError").Value} {outletData.ProductName} ({outletData.ProductSku}) {_globalLocalizer.GetString("InBasket")} {outletData.OutletQuantity} {_globalLocalizer.GetString("MaximalLabel")} {outletProductAvailableQuantity}", (int)HttpStatusCode.Conflict);
-                        }
-                    }
+                    throw new CustomException($"{messageQuantityError} {itemData.ProductName} ({itemData.ProductSku}) {_globalLocalizer.GetString("InBasket")} {itemData.Quantity} {_globalLocalizer.GetString("MaximalLabel")} {stockItemAvailableQuantity}", (int)HttpStatusCode.Conflict);
                 }
             }
+        }
+
+        private class BasketItem
+        {
+            public Guid? ProductId { get; set; }
+            public string ProductName { get; set; }
+            public string ProductSku { get; set; }
+            public double Quantity { get; set; }
         }
     }
 }
