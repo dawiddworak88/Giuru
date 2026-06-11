32d16a8b3aad68174c8fc3a728628aec59dd0ea7
Piotr Zaranek <piotrek.zaranek@gmail.com>
2026-06-11 16:16:05 +0200

    Added inheritance from StockItem class

--- be/src/Project/Web/Seller/Seller.Web/Areas/Inventory/DomainModels/OutletItem.cs
+++ be/src/Project/Web/Seller/Seller.Web/Areas/Inventory/DomainModels/OutletItem.cs
@@ -2,12 +2,11 @@
 
 namespace Seller.Web.Areas.Inventory.DomainModels
 {
-    public class OutletItem
+    public class OutletItem : StockItem
     {
         public Guid Id { get; set; }
         public Guid WarehouseId { get; set; }
         public string WarehouseName { get; set; }
-        public Guid ProductId { get; set; }
         public string ProductName { get; set; }
         public string ProductSku { get; set; }
         public Guid SellerId { get; set; }
@@ -15,7 +14,6 @@ namespace Seller.Web.Areas.Inventory.DomainModels
         public string Title { get; set; }
         public string Description { get; set; }
         public string Ean { get; set; }
-        public double AvailableQuantity { get; set; }
         public DateTime LastModifiedDate { get; set; }
         public DateTime CreatedDate { get; set; }
     }
