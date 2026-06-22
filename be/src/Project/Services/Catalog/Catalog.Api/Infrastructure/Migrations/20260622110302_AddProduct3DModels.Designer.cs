5cf8877c6ae9923ed98e20e38082cb5ff221a2f1
Piotr Zaranek <piotrek.zaranek@gmail.com>
2026-06-22 16:01:44 +0200

    Added new migration with new table

--- be/src/Project/Services/Catalog/Catalog.Api/Infrastructure/Migrations/20260622110302_AddProduct3DModels.Designer.cs
+++ be/src/Project/Services/Catalog/Catalog.Api/Infrastructure/Migrations/20260622110302_AddProduct3DModels.Designer.cs
@@ -4,6 +4,7 @@ using Foundation.Catalog.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Catalog.Api.Infrastructure.Migrations
 {
     [DbContext(typeof(CatalogContext))]
-    partial class CatalogContextModelSnapshot : ModelSnapshot
+    [Migration("20260622110302_AddProduct3DModels")]
+    partial class AddProduct3DModels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ be/src/Project/Services/Catalog/Catalog.Api/Infrastructure/Migrations/20260622110302_AddProduct3DModels.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Catalog.Api.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProduct3DModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Product3DModels",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ProductId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    RowVersion = table.Column<byte[]>(type: "rowversion", rowVersion: true, nullable: true),
+                    LastModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    MediaId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Order = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product3DModels", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product3DModels_ProductId",
+                table: "Product3DModels",
+                column: "ProductId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product3DModels");
+        }
+    }
+}
