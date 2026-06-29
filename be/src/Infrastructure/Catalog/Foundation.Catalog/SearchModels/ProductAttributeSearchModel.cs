b63b338e2d7f382ff604e97269b4e86a37bdfb00
Piotr Zaranek <piotrek.zaranek@gmail.com>
2026-06-29 07:46:51 +0200

    Added ProductAttributeSearchModel

---
+++ be/src/Infrastructure/Catalog/Foundation.Catalog/SearchModels/ProductAttributeSearchModel.cs
@@ -0,0 +1,15 @@
+﻿using Nest;
+
+namespace Foundation.Catalog.SearchModels
+{
+    public class ProductAttributeSearchModel
+    {
+        [Keyword] public string Key { get; set; }
+        [Keyword] public string Name { get; set; }
+        [Keyword] public string[] ValueIds { get; set; }
+        [Keyword] public string[] ValueKeywords { get; set; }
+        [Text] public string ValueText { get; set; }
+        public double? ValueNumeric { get; set; }
+        public bool? ValueBoolean { get; set; }
+    }
+}
