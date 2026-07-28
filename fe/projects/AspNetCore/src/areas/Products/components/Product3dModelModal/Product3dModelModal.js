92debaeca48e386b0bbf54efba1c30ce8e09af1e
Piotr Zaranek <piotrek.zaranek@gmail.com>
2026-07-28 07:32:32 +0200

    Added dynamic import for model-viewer package

--- fe/projects/AspNetCore/src/areas/Products/components/Product3dModelModal/Product3dModelModal.js
+++ fe/projects/AspNetCore/src/areas/Products/components/Product3dModelModal/Product3dModelModal.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { Button, Dialog } from "@mui/material";
-import '@google/model-viewer';
 import { Close } from "@mui/icons-material";
+import { useEffect } from 'react';
 
 function Product3dModelModal(props) {
   const { isOpen, handleClose, modelSrc, modelAlt } = props;
+
+  useEffect(() => {
+    import('@google/model-viewer');
+  }, [])
 
   return (
     <Dialog
