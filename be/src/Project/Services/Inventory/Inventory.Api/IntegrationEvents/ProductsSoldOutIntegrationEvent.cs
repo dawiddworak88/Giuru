0c176073f7f8cf99f9d0d8c03173b1b59be37819
Piotr Zaranek <piotrek.zaranek@gmail.com>
2026-08-04 13:17:46 +0200

    Added event when product sold out

---
+++ be/src/Project/Services/Inventory/Inventory.Api/IntegrationEvents/ProductsSoldOutIntegrationEvent.cs
@@ -0,0 +1,11 @@
+﻿using Foundation.EventBus.Events;
+using System;
+using System.Collections.Generic;
+
+namespace Inventory.Api.IntegrationEvents
+{
+    public class ProductsSoldOutIntegrationEvent : IntegrationEvent
+    {
+        public IEnumerable<Guid> SoldOutProductIds { get; set; }
+    }
+}
